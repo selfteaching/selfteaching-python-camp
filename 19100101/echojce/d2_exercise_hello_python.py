7e2741a0b0b9e1816bf6005800e667959526939e
echojce <48573815+echojce@users.noreply.github.com>
2019-03-19 15:16:24 +0800

    Update and rename 19100101/echojce/d2_exercise_hello_python.py to  19100101/ echojce/d2_exercise_hello_python.py

--- 19100101/echojce/d2_exercise_hello_python.py
+++
@@ -1,8 +0,0 @@
-Last login: Tue Mar 19 14:22:26 on ttys000
-localhost:~ yiwaidexingyun$ python
-Python 3.7.1 (default, Dec 14 2018, 13:28:58) 
-[Clang 4.0.1 (tags/RELEASE_401/final)] :: Anaconda, Inc. on darwin
-Type "help", "copyright", "credits" or "license" for more information.
->>> print('hello world')
-hello world
->>> 
