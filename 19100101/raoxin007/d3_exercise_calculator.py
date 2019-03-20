40e2416bdf42466205168a681f11a3b7fb66e103
饶欣 <1139535078@qq.com>
2019-03-20 21:49:09 +0800

    第三天的作业

--- 19100101/raoxin007/d3_exercise_calculator.py
+++ 19100101/raoxin007/d3_exercise_calculator.py
@@ -1,34 +1,34 @@
 #简易加减乘除计算器程序
 #定义加法函数
-def add(x,y)：
+def add(x,y):
     return x+y
 #定义减法函数
-def substract（x，y）：
+def substract（x，y):
     return x-y
 #定义乘法函数
-def multiply（x，y）：
+def multiply（x，y):
     return x*y
 #定义除法函数
-def divide（x，y）：
+def divide（x，y):
     return x/y
 #定义执行方式，当执行脚本本身，执行如下代码
 if __name__ == '__main__':               
-    print("******************************")
-    print("          计算器")
-    print("******************************")
-    chose = input("选择你要的计算方式，1/加法 ，2/减法，3/乘法，4/除法: ")
-    if chose == "1":
-        tmp = input("请输入你要计算的数字:")
-        add(tmp)
-    elif chose == '2':
-        tmp = input("请输入你要计算的数字:")
-        subtraction(tmp)
-    elif chose == '3':
-        tmp = input("请输入你要计算的数字:")
-        multiplication(tmp)
-    elif chose == '4':
-        tmp = input("请输入你要计算的数字:")
-        division(tmp)
-    else:
-        print("你输入的有误，请重新输入")
+print("选择运算")
+print("1、加")
+print("2、减")
+print("3、乘")
+print("4、除")
+choise = input("输入你的选择（1/2/3/4):")
+num1 = int(input("输入第一个数字: "))
+num2 = int(input("输入第二个数字: "))
+if choice == '1':
+   print(num1,"+",num2,"=",add(num1,num2))
+elif choice == '2':
+   print(num1,"-",num2,"=", subtract(num1,num2)) 
+elif choice == '3':
+   print(num1,"*",num2,"=", multiply(num1,num2))
+elif choice == '4':
+   print(num1,"/",num2,"=", divide(num1,num2))
+else:
+    print("你输入的有误，请重新输入")
 
