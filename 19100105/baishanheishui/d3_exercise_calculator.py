51058f9c6b02cc29f7eff9f9abd355873f031805
baishanheishui <15309888203@163.com>
2019-03-20 16:26:26 +0800

    0320

--- 19100105/baishanheishui/d3_exercise_calculator.py
+++ 19100105/baishanheishui/d3_exercise_calculator.py
@@ -1,4 +1,4 @@
-argv = input('请输入：X+Y，或X-Y,或X*Y，或X/Y')
+argv = input('请输入：X+Y，或X-Y,或X*Y，或X/Y') 
 while(argv):
     print(argv,'=:')
     if argv[1] == '+':
@@ -11,4 +11,4 @@ while(argv):
       print(int(argv[0]) / int(argv[2]))
     else:
       print("出错")
-    argv = input('请继续输入：X+Y，或X-Y,或X*Y，或X/Y')
+    argv = input('请继续输入：X+Y，或X-Y,或X*Y，或X/Y') 
