e63d6ba62588e449e7592dad3d7d3b407a65cbbf
fuzaifei <48574127+fuzaifei@users.noreply.github.com>
2019-03-20 19:52:12 +0800

    no message
    Signed-off-by: fuzaifei <48574127+fuzaifei@users.noreply.github.com>

--- 19100104/fuzaife i/d3_exercise_calculator.py
+++ 19100104/fuzaife i/d3_exercise_calculator.py
@@ -1,9 +1,38 @@
-print('尝试编写一个支持加、减、乘、除功能的计算器')
+def add(x, y):  #加法运算
+    return x + y
 
-def add(x, y, c):    
-    a = x * y      
-    b = y / c        
-    e = (a + b)/c
-    return e
+def subtract(x, y):   #减法运算
+    return x - y
 
-# 在运算的时候，给与X, Y, C不同的数值，就可以计算出e的结果。
+def multiply(x, y):   #乘法运算
+    return x * y
+
+def divide(x, y):  #除法运算
+    return x / y
+
+#用户输入
+print('选择运算：')
+print('1、相加')
+print('2、相减')
+print('3、相乘')
+print('4、相除')
+
+choice = input('输入你的选择（1/2/3/4):')
+
+x = int(input('输入第一个数字：'))
+y = int(input('输入第二个数字：'))
+
+if choice == '1':
+    print(x, '+', y, '=', add(x, y) )
+
+elif choice == '2':
+    print(x, '-', y, '=', subtract(x, y))
+
+elif choice == '3':
+    print(x, '*', y,'=', multiply(x, y) )
+
+elif choice == '4':
+    print(x, '/', y, '=', divide(x, y))
+
+else :
+    print('非法输入')
