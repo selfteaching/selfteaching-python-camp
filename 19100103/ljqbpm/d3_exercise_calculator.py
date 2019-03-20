8cc7263bedece54240a399d1414606e424983681
LiaoJiaQing <294217473@qq.com>
2019-03-20 21:34:30 +0800

    d3_exercise_calculator

--- 19100103/ljqbpm/d3_exercise_calculator.py
+++ 19100103/ljqbpm/d3_exercise_calculator.py
@@ -5,14 +5,18 @@ def get_number(i):#获取输入的数字
     a = float(input(' 个数字'))
     return a
 
-cal=input('请输入运算符（仅支持+、-、*、/）')
-if cal=='+':
-    print('运算结果为：',get_number(1)+get_number(2))
-elif cal=='-':
-    print('运算结果为：',get_number(1)-get_number(2))
-elif cal=='*':
-    print('运算结果为：',get_number(1)*get_number(2))
-elif cal=='/':
-    print('运算结果为：',get_number(1)/get_number(2))
-else:
-    print('输入错误')
+def cal():
+    cal=input('请输入运算符（仅支持+、-、*、/）')
+    if cal=='+':
+        print('运算结果为：',get_number(1)+get_number(2))
+    elif cal=='-':
+        print('运算结果为：',get_number(1)-get_number(2))
+    elif cal=='*':
+        print('运算结果为：',get_number(1)*get_number(2))
+    elif cal=='/':
+        print('运算结果为：',get_number(1)/get_number(2))
+    else:
+        print('输入错误')
+    pass
+
+cal()
