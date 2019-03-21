4cbad6bad37fdbf354c4ac29c955f74903f4a131
SundyYang <cyang@TF015313.local>
2019-03-21 21:47:54 +0800

    更正第四天作业

--- 19100101/sundyyang/d4_exercise_control_flow.py
+++ 19100101/sundyyang/d4_exercise_control_flow.py
@@ -1,23 +1,20 @@
 #output “Multiplication Table” by “for xx in"
 
-for a in range (1,10): #遍历1-10数字序列
-    for b in range (1,a+1): #遍历1-“i+1”数字序列
-        print(a,"*",b,"=",a*b,end = "\t") #打印
+for i in range(1,10):
+     for j in range(1,i+1):
+         print( i,"*", j,"=" ,i*j,end="\t")
+     print (end="\n")
 
-print("\n") #空行
-print("\n") #空行
+print (end="\n")
 
-a = 1 #赋值
-while a <= 9: #在<=9内循环
-    b = 1
-    a = a + 1
-    while b <= a:
-        if a % 2 != 0:
-            if b < a:
-                print(a,"*",b,"=",a*b,end = "\t")
-                b = b + 1
-            else:
-                print(a,"*",b,"=",a*b,end = "\t")
-                b = b + 1
-        else:
-            b = b + 1
+i=1
+while i<10:
+    j=1
+    if(i%2==1):
+        while j<i+1:
+            print( i,"*", j,"=" ,i*j,end="\t")
+            j=j+1
+        print (end="\n")
+        i=i+1
+    else:
+        i=i+1
