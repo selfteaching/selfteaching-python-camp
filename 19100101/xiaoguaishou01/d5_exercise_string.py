eed8de54700d3e522334a08168d0b3e88839a4db
燃烧的小怪兽 <xiaoguaishou0809@gmail.com>
2019-03-23 00:11:17 +0800

    对d5_exercise_string.py进行修改

--- 19100101/xiaoguaishou01/d5_exercise_string.py
+++ 19100101/xiaoguaishou01/d5_exercise_string.py
@@ -22,12 +22,18 @@ If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 '''
 #将字符串样本text里英文单词中包含ea的英文单词剔除
+symbol = ",.!-*"
+for str in symbol:
+    text = text.replace(str,'')
+print(text)
+
 list = text.split( )#将text的字符串以空格为分割符分割成字符串并赋值给变量list
 str1 = "ea"
 str2=[]
 for i in list:
     if i.find(str1) == -1:#判断单词如果不包含ea则加进新数组
        str2.append(i) #将不包含ea的单词加进新数组str2
+       #m = str2.translate(None,str2.punctuation)
 print(str2)
 
 #将better全部替换成worse
@@ -37,7 +43,7 @@ rep = ["worse" if x in pattern else x for x in str2]
 print(rep)
 
 #大写字母转成小写，小写字母转成大写
-print("大写字母转成小写")
+print("大写字母转成小写,小写字母转成大写")
 rep2 = text
 print(rep2.swapcase())
 
