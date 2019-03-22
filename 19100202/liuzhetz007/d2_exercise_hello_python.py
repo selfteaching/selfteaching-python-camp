ead9d7659cc50af169fccf505548b0bedc7c7363
liuzhetz007 <liuzhetz007@126.com>
2019-03-22 16:56:52 +0800

    python

---
+++ 19100202/liuzhetz007/d2_exercise_hello_python.py
@@ -0,0 +1,6 @@
+Python 3.7.2 (v3.7.2:9a3ffc0492, Dec 24 2018, 02:44:43) 
+[Clang 6.0 (clang-600.0.57)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print('Hwllo World!')
+Hwllo World!
+>>> 
