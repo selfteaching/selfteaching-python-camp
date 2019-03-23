4212a174dca62a9e8a0b1f55f7b042bba26a850c
Gdong24 <gdong.24@gmail.com>
2019-03-23 16:21:05 +1100

    Update d3_exercise_calculator_2.py

--- 19100202/Gdong24/d3_exercise_calculator_2.py
+++ 19100202/Gdong24/d3_exercise_calculator_2.py
@@ -1,16 +1,33 @@
-num1 = int(input("please enter a num1"))
-Op = input("operator")
-num2 = int(input("please enter a num2"))
+# Define calculate() function
+def calculate():
+    operation = input('''
+Please type in the math operation:
++ for addition
+- for subtraction
+* for multiplication
+/ for division
+''')
 
-if Op == "+":
-    print(eval(num1 + num2))
+    number_1 = int(input('Please enter the first number: '))
+    number_2 = int(input('Please enter the second number: '))
 
-elif Op == '-':
-    print(num1 - num2)
+    if operation == '+':
+        print('{} + {} = '.format(number_1, number_2))
+        print(number_1 + number_2)
 
-elif Op == "*":
-    print(num1 * num2)
+    elif operation == '-':
+        print('{} - {} = '.format(number_1, number_2))
+        print(number_1 - number_2)
 
-elif Op == "/":
-    print(num1 / num2)
+    elif operation == '*':
+        print('{} * {} = '.format(number_1, number_2))
+        print(number_1 * number_2)
 
+    elif operation == '/':
+        print('{} / {} = '.format(number_1, number_2))
+        print(number_1 / number_2)
+
+    else:
+        print('You have not typed a valid operator, please run the program again.')
+
+calculate()
