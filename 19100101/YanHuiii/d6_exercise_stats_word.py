e478543f769c51ff4c3f122e6a5cf8f35316cc18
YanHui <yhhero4016@sina.com>
2019-03-23 19:59:08 +0800

    第六天作业，第二次修改@realcaiying

--- 19100101/YanHuiii/d6_exercise_stats_word.py
+++ 19100101/YanHuiii/d6_exercise_stats_word.py
@@ -4,6 +4,8 @@
 
 text = '''
 The Zen of Python, by Tim Peters
+
+
 Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
@@ -23,6 +25,8 @@ Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
+
+Python是一种计算机程序设计语言。是一种动态的、面向对象的脚本语言，最初被设计用于编写自动化脚本(shell)，随着版本的不断更新和语言新功能的添加，越来越多被用于独立的、大型项目的开发。
 '''
 dict1 = {}
 dict2 = {}
@@ -33,6 +37,8 @@ dict4 = {}
 """创建一个名为stats_text_en的函数，它的功能是为统计英文词频"""
 def stats_text_en(text):
     import re
+    '''只保留英文'''
+    text = re.sub("[^A-Za-z]", " ", text.strip())
     '''将字符串text转换为列表list1,只保留单词为list1中的元素'''
     list1 = re.split(r"\W+",text)   
     '''删除list1中为空的列表元素'''
@@ -56,9 +62,7 @@ str = ''
 
 
 '''6.2创建一个名为stats_text_cn的函数，并用它实现统计汉字词频的功能'''
-text1 = '''自学营的体验真不错，以后一定要讲自学当做一种习惯.
-The experience of self-study camp is really good, in the future must talk about self-study as a habit.
-'''
+
 def histogram(s, old_d):
     d = old_d
     for c in s:
@@ -92,4 +96,4 @@ def stats_text_cn(text):
 
 #打印统计中文词频的结果
 print("统计中文词频的结果为:")
-print(stats_text_cn(text1))
+print(stats_text_cn(text))
