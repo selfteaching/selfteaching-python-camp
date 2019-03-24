e91a2b8a596d0e72764059e7af11a70f0d2969ed
贾长娥 <echojce@gmail.com>
2019-03-24 12:18:53 +0800

    修改

--- 19100101/echojce/d6_exercise_stats_word.py
+++ 19100101/echojce/d6_exercise_stats_word.py
@@ -1,8 +1,12 @@
-'''这是一个封装统计英文词频的函数，以及一个封装中文词频的函数
-6.1创建一个名为stats_text_en的函数，并用它封装d5_exercise_stats_text.py文件中的代码 '''
+# this is d6 excercise for defining functions
+# date : 2019.3.23
+# author by : qiming
 
-text = '''
+# 示例字符串
+string1 =  '''
 The Zen of Python, by Tim Peters
+
+
 Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
@@ -22,74 +26,61 @@ Although never is often better than *right* now.
 If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
+
 Python是一种计算机程序设计语言。是一种动态的、面向对象的脚本语言，最初被设计用于编写自动化脚本(shell)，随着版本的不断更新和语言新功能的添加，越来越多被用于独立的、大型项目的开发。
 '''
-dict1 = {}
-dict2 = {}
-dict3 = {}
-dict4 = {}
+
+import collections
+import re
+
+def stats_text_en(string_en):
+    ''' 统计英文词频
+    
+    第一步：过滤英文字符，并将string拆分为list。
+    第二步：清理*-等标点符号。
+    第三步：使用collections库中的Counter函数进行词频统计并输出统计结果。
+    '''
+    result = re.sub("[^A-Za-z]", " ", string_en.strip())
+    newList = result.split( )
+    i=0
+    for i in range(0,len(newList)):
+        newList[i]=newList[i].strip('*-,.?!')
+        if newList[i]==' ': 
+            newList[i].remove(' ')
+        else:
+            i=i+1
+    print('英文单词词频统计结果： ',collections.Counter(newList),'\n')
 
 
-"""创建一个名为stats_text_en的函数，它的功能是为统计英文词频"""
-def stats_text_en(text):
-    import re
-    '''只保留英文'''
-    text = re.sub("[^A-Za-z]", " ", text.strip())
-    '''将字符串text转换为列表list1,只保留单词为list1中的元素'''
-    list1 = re.split(r"\W+",text)   
-    '''删除list1中为空的列表元素'''
-    while '' in list1:   
-        list1.remove('')
-    """i属于list1中的元素，开始循环"""
-    for i in list1:   
-        """将列表中的单词及单词的出现次数，分别赋值给dict1的键和值"""
-        dict1.setdefault(i,list1.count(i))  
-    """将dict1按照value值从大到小排列，并将结果赋给元组tup1"""
-    tup1 = sorted(dict1.items(),key = lambda items:items[1],reverse = True)   
-    """遍历元组tup1"""
-    for tup1 in tup1:   
-            dict2[tup1[0]] = dict1[tup1[0]]  
-    return dict2
+def stats_text_cn(string_cn):
+    ''' 统计中文汉字字频
+    
+    第一步：过滤汉字字符，并定义频率统计函数 stats()。
+    第二步：清除文本中的标点字符,将非标点字符组成新列表 new_list。
+    第三步：遍历列表，将字符同上一次循环中频率统计结果作为形参传给统计函数stats()。
+    第四步：统计函数在上一次统计结果基础上得出本次统计结果，赋值给newDict。
+    第五步：new_list遍历结束，输出倒序排列的统计结果。
+    '''
+    result1 = re.findall(u'[\u4e00-\u9fff]+', string_cn)
+    newString = ''.join(result1)
 
-#打印统计英文词频的结果
-print("统计英文词频的结果为:")
-print(stats_text_en(text))
-str = ''
+    def stats(orgString, newDict) :
+        d = newDict
+        for m in orgString :
+            d[m] = d.get(m, 0) + 1
+        return d
+    
+    new_list = []
+    for char in newString :
+        cn = char.strip('-*、。，：？！……')
+        new_list.append(cn)
+    
+    words = dict()
+    for n in range(0,len(new_list)) :
+        words = stats(new_list[n],words)
+    newWords = sorted(words.items(), key=lambda item: item[1], reverse=True) 
+    print('中文汉字字频统计结果： ',dict(newWords))
 
-
-'''6.2创建一个名为stats_text_cn的函数，并用它实现统计汉字词频的功能'''
-
-def histogram(s, old_d):
-    d = old_d
-    for c in s:
-        d[c] = d.get(c, 0) + 1
-    return d
-"""创建一个名为stats_text_cn的函数，它的功能是为统计中文词频"""
-def stats_text_cn(text):
-    import re
-    """去掉text中的英文和数字"""
-    text = re.sub("[A-Za-z0-9]", "", text)
-    '''将字符串text转换为列表list1,只保留单词为list1中的元素'''
-    list1 = re.split(r"\W+",text)   
-
-    '''删除list1中为空的列表元素'''
-    while '' in list1:   
-        list1.remove('')
-
-    ''' 把dict3的行拆成单字，拆成字典格式的'''
-    dict3 = dict()
-    '''给dict3赋值'''
-    for i in range(len(list1)):
-        dict3 = histogram(list1[i], dict3)
-
-    """将dict3按照value值从大到小排列，并将结果赋给元组tup1"""
-    tup1 = sorted(dict3.items(),key = lambda items:items[1],reverse = True)  
-
-    """遍历元组tup1"""
-    for tup1 in tup1:   
-            dict4[tup1[0]] = dict3[tup1[0]]  
-    return dict4
-
-#打印统计中文词频的结果
-print("统计中文词频的结果为:")
-print(stats_text_cn(text))
+# 调用函数
+stats_text_en(string1)
+stats_text_cn(string1)
