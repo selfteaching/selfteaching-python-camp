3a257edbc029ba6dade387da5f25734e9a3640d4
hpreborn <penghoufly@gmail.com>
2019-03-24 16:43:48 +0800

    19100102 d7 hpreborn homework update

--- 19100102/hpreborn/mymodule/stats_word.py
+++ 19100102/hpreborn/mymodule/stats_word.py
@@ -1,3 +1,9 @@
+import re
+
+#英文和中文字符匹配规则
+en_pattern = re.compile(r'[a-zA-Z]+[\'\-]?[a-zA-Z]+')
+cn_pattern = re.compile(r'[\u4e00-\u9fa5]')
+
 #字符串示例
 template = '''
 The Zen of Python, by Tim Peters
@@ -22,36 +28,6 @@ Although never is often better than *right* now.
 Namespaces are one honking great idea -- 让我们继续为之努力!
 '''
 
-#判断一个字符串是否是英文字符串的函数
-def judge_pure_english(keyword):
-        return all(ord(c) < 128 for c in keyword)
-
-#清理字符串中的非中文
-def is_ustr(in_str):
-    out_str=''
-    for i in range(len(in_str)):
-        if is_uchar(in_str[i]):
-            out_str=out_str+in_str[i]
-        else:
-            out_str=out_str+' '
-    return out_str
-
-def is_uchar(uchar):
-    """判断一个unicode是否是汉字"""
-    if uchar >= u'\u4e00' and uchar<=u'\u9fa5':
-            return True
-    """判断一个unicode是否是数字"""
-    if uchar >= u'\u0030' and uchar<=u'\u0039':
-            return False        
-    """判断一个unicode是否是英文字母"""
-    if (uchar >= u'\u0041' and uchar<=u'\u005a') or (uchar >= u'\u0061' and uchar<=u'\u007a'):
-            return False
-    if uchar in ('-',',','，','。','.','>','?'):
-            return False
-    return False
-
-
-
 #创建一个名为stats_text_en的函数，封装day5中任务2的代码到刚这个函数下，同时用文档字符串为stats_text_en添加注释说明
 def stats_text_en(text):
     ''' 
@@ -62,14 +38,12 @@ def stats_text_en(text):
     # 在这里写具体操作
     mydict={}
     mylist=[]
-    mylist=text.split(" ")
+    mylist=re.findall(en_pattern,text)
     for mylinum in mylist:
-        if judge_pure_english(mylinum) and mylinum != "":
-            if mylinum in mydict:
-                mydict[mylinum]=int(mydict[mylinum])+1
-            else:
-                mydict[mylinum]=1
-    #mydict=dict(sorted(stats_text_en(template).items(), key=lambda d: d[1],reverse=True))  #排序后返回
+        if mylinum in mydict:
+            mydict[mylinum]=int(mydict[mylinum])+1
+        else:
+            mydict[mylinum]=1
     return mydict
 #print(sorted(stats_text_en(template).items(), key=lambda d: d[1],reverse=True))
 
@@ -82,14 +56,13 @@ def stats_text_cn(text):
     '''
     # 在这里写具体操作
     mydict={}
-    text=is_ustr(text).replace(" ","")
-    for mylinum in text:
-        if mylinum != "":
-            if mylinum in mydict:
-                mydict[mylinum]=int(mydict[mylinum])+1
-            else:
-                mydict[mylinum]=1
-    
+    mylist=[]
+    mylist=re.findall(cn_pattern,text)
+    for mylinum in mylist:
+        if mylinum in mydict:
+            mydict[mylinum]=int(mydict[mylinum])+1
+        else:
+            mydict[mylinum]=1
     return mydict
 #print(sorted(stats_text_cn(template).items(), key=lambda d: d[1],reverse=True))
 
@@ -100,9 +73,6 @@ def stats_text(text):
     :param text:string
     :return dict:中文和英文单词词频统计结果
     '''
-    #对字符串做清洗操作，清洗掉换行、逗号，点，叹号，星号和破折号
-    text=text.replace("\n"," ").replace(","," ").replace("."," ").replace("*"," ").replace("--"," ")
-    
     return dict(stats_text_en(text),**stats_text_cn(text))
 
 
@@ -114,9 +84,3 @@ def main():
 
 if __name__ == '__main__':
     main()
-
-
-
-
-
-
