d6a975478ef8abc4c769ae46e22945ac68a9d902
xiaopenglee <1246054892@qq.com>
2019-03-24 23:55:07 +0800

    day5

--- 19100103/xiaopenglee/mymodule/stats_word.py
+++ 19100103/xiaopenglee/mymodule/stats_word.py
@@ -1,62 +1,41 @@
-text = '''
-The Zen of Python, by Tim Peters
-美丽 is better than 丑陋.
-清楚 is better than 含糊.
-简单 is better than 复杂.
-复杂 is better than 难懂.
-单一 is better than 嵌套.
-稀疏 is better than 稠密.
-可读性计数.
-Special cases aren't special enough to 打破规则.
-即使练习会使得不再纯粹.
-但错误不应该用沉默来掩盖.
-Unless explicitly silenced.
-面对起义，拒绝猜的诱惑.
-有且只有一个办法.
-Although that way may not be obvious at first unless you're Dutch.
-尝试总比从未试过要强.
-Although never is often better than *right* now.
-如果执行很难被解释，那将是一个很糟的想法.
-如果执行很容易解释，这会是一个好点子.
-Namespaces are one honking great idea！
-'''
-#先将text里的标点符号去除
-symbol = ",.!-*"
-for str in symbol:
-    text = text.replace(str,'')
+#-*- coding:utf-8 -*-
+    
+#定义注释函数
+def annotation(string) -> '''This is a Word frequency searcher''':     #用文档字符串进行注释
+    print("Annotation:",annotation.__annotations__) 
 
-#创建一个名为stats_text_en的函数
-#使用字典（dict）统计字符串样本text中各个英文单词出现的次数
-import re
-def stats_text_en(text):
-    '''使用字典（dict)统计text中每个英文单词出现的次数.'''
-    result = re.sub("[^A-Za-z]", " ", text.strip())
-    d = {}    
-    for x in result.split( ):
-        if not x in d:
-            d[x] = 1
-        else:
-            d[x] = d[x]+1
-    return d
-print("统计text中每个英文单词出现的次数")
-print(stats_text_en(text))
-frequency = stats_text_en(text)
-import re
-def stats_text_cn(str):
-    '''统计每个中文汉字出现的次数'''
-    result = re.findall(u'[\u4e00-\u9fff]+', str)#\u是unincode编码，u4e00是十六进制表达值
-    rep = ''.join(result)
-    resoult = {}
-    for i in rep:
-        resoult[i] = rep.count(i)
-    return resoult
-stats_text_cn(str = "")
-print("统计text中中文汉字出现的次数")
-print(stats_text_cn(text))
 
-print("统计text中英文单词和中文汉字出现的次数")
-stats_text_cn(str = "")
-a=stats_text_cn(text)
-b=stats_text_en(text)
-c=dict(a,**b)
-print(c)#输出合并词频统计结果
+#定义中文检查器，同d6                      
+def stats_text_cn(checkstr):
+    cndict={}
+    for i in checkstr:
+        if u'\u4e00' <= i <= u'\u9fff':
+            cndict[i] = checkstr.count(i)
+    cndict=sorted(cndict.items(),key=lambda item:item[1],reverse=True)
+    print(cndict)
+    return cndict
+
+#定义英文检查器，增加了查找英文字符的功能
+def stats_text_en(checkstr):
+    endict={}               #新建一个字典
+    entext=""               #新建一个空字符串
+    checkstr=checkstr.replace(',',' ').replace('.',' ').replace('--',' ').replace('!',' ').replace('*',' ')  #去标点
+
+    #遍历原始字符串
+    for i in checkstr: 
+        if i.isascii():         #去掉中文或者说非英文字符
+            entext=entext+i     #将英文字符放入新建的字符串中
+
+    entext=entext.split( )      #分隔字符串
+
+    for i in entext:            #遍历新字符串
+        endict[i]=entext.count(i)        #创建字典
+    endict=sorted(endict.items(),key=lambda item:item[1],reverse=True) #按照值排序，从小到大
+    print(endict)
+    return endict
+
+#定义stats_text函数
+def stats_text(str):
+    stats_text_cn(str)          #导入stats_text_cn函数
+    stats_text_en(str)          #导入stats_text_en函数
+    annotation(str)             #加入注释功能
