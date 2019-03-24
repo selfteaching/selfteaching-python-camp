c2ea4fc6133ad2f5d2575a135a579afab23ba000
junyanwang77 <junyanwang77@gmail.com>
2019-03-24 20:11:56 +0800

    Create d4_exercise_control_flow_3.py

---
+++ 19100202/junyanwang77/d4_exercise_control_flow_3.py
@@ -0,0 +1,11 @@
+i = 1
+while i < 10:
+    j = 1
+    while j<=i:
+        if i%2 == 0:
+           break
+        print('{}*{}={}'.format(i,j,i*j),end='\t')
+        j += 1
+    print()
+    i += 1
+    
