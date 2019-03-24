a8ad5743c669c7a1e4bb5ee9756e9ee4cae1c919
luodongrui <843435679@qq.com>
2019-03-24 20:31:50 +0800

    19100201 day4

--- 19100201/luodongrui/d4-exercise_control_flow.py
+++ 19100201/luodongrui/d4-exercise_control_flow.py
@@ -1,17 +1,19 @@
 # 九九乘法表
+
 for i in range(1,10):
     for j in range(1,i+1):
         d = i * j
         print('%d*%d=%-2d'%(i,j,d),end = ' ' )
     print()
 
-# 除去偶数的九九乘法表
-i=1
+# 除去偶数列的九九乘法表
 
-while i<=9:
-    j=[2,4,6,8]
-    while j<=i:
-        print('%d*%d=%-2d '%(j,i,i*j),end='')
-        j+=1
-    print('\n')
-    i+=1
+i = 1
+while i <= 9:
+    j = 1
+    while j <= i:
+        if i%2==1:
+            print('%d X %d = %d' % (j,i,i*j),end = '  ') 
+        j += 1
+    print('')
+    i += 1
