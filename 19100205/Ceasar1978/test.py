0744cf3a0f424e5adaed5832466f880460de5d94
Ceasar1978 <48576987+Ceasar1978@users.noreply.github.com>
2019-03-25 20:55:30 +0800

    3.25张凯山作业

--- 19100205/Ceasar1978/test.py
+++ 19100205/Ceasar1978/test.py
@@ -1,24 +1,9 @@
-text = '''
-The Zen of Python, by Tim Peters
-Beautiful is better than ugly.
-Explicit is better than implicit.
-Simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested.
-Sparse is better than dense.
-Readability counts.
-Special cases aren't special enough to break the rules.
-Although practicality beats purity.
-Errors should never pass silently.
-Unless explicitly silenced.
-In the face of ambxiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to do it.
-Although that way may not be obvious at first unless you're Dutch.
-Now is better than never.
-Although never is often better than *right* now.
-If the implementation is hard to explain, it's a bad idea.
-If the implementation is easy to explain, it may be a good idea.
-Namespaces are one honking great idea -- let's do more of those!
-'''
-text = text.replace('.', '')
-print(text)
+
+seq4 = [1, 2, 3]
+seq5 = []
+for i in range(len(seq4)):
+    seq5.append(str(seq4[i]))
+s6 = ''.join(seq5)
+print (s6)
+s7 = ''.join(str(i) for i in seq5)
+print (s7)
