54a09d74f89953025bcdb04be2df0825dec0143a
lipeer <2695025702@qq.com>
2019-03-25 14:36:04 +0800

    Update D5_exercise_string.py

---
+++ 19100102/lipeer/D5_exercise_string.py
@@ -0,0 +1,22 @@
+# ------- 1 -------
+import re
+ 
+f = open('word.txt'ea'r')
+word = f.read()
+f.close()
+ 
+
+#-----2----
+text=text.replace('better','worse')
+print(text)
+
+
+ 
+# ------- 3-------
+print(str.lower())
+print(str.upper())
+
+#------4----
+list = [a...z] 
+list.sort() 
+print list
