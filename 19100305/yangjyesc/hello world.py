ac69c703774ebef084a9e4dbf88e2d55e73af9f7
yangjyesc <48853971+yangjyesc@users.noreply.github.com>
2019-03-25 14:57:30 +0800

    hello world.py

---
+++ 19100305/yangjyesc/hello world.py
@@ -0,0 +1,5 @@
+Python 3.7.2 (tags/v3.7.2:9a3ffc0492, Dec 23 2018, 22:20:52) [MSC v.1916 32 bit (Intel)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> print ('hello world')
+hello world
+>>> 
