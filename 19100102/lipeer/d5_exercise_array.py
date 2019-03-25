71434b519c9bdc60df69756905e9ca13e400edbc
lipeer <2695025702@qq.com>
2019-03-25 15:22:52 +0800

    Create d5_exercise_array.py

---
+++ 19100102/lipeer/d5_exercise_array.py
@@ -0,0 +1,35 @@
+
+sStr2 = ",".join(map(str, [ele for sub in sStr1 for ele in sub]))
+ 
+
+sStr2 = ",".join(map(str, sStr1))
+
+
+arr=["a","b"]
+str=","join(arr)
+print Str1
+
+print " "[3：8]
+
+
+str="3:8"
+arr=str.split(",")
+print a
+
+
+
+int('101',2)
+int('101',8)
+int('101',16)
+
+
+In [7]: bin(10)
+Out[7]: '0b1010'
+
+In [9]: oct(10)
+Out[9]: '012'
+
+In [8]: hex(10)
+Out[8]: '0xa'
+
+
