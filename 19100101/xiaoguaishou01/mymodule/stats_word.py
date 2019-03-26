285ad2f5eb36ada0091c508561613a4a747ab716
燃烧的小怪兽 <xiaoguaishou0809@gmail.com>
2019-03-26 21:16:04 +0800

    day9 标准库

--- 19100101/xiaoguaishou01/mymodule/stats_word.py
+++ 19100101/xiaoguaishou01/mymodule/stats_word.py
@@ -5,7 +5,6 @@ Beautiful is better than ugly.
 Explicit is better than implicit.我们只中每个英文单词出现的次数'''
 import re
 import collections
-count = 5
 def stats_text_en(text,count):
     '''1、使用字典（dict)统计text中每个英文单词出现的次数.
     2、添加类型检查，如果不是字符串类型为异常'''
@@ -16,7 +15,6 @@ def stats_text_en(text,count):
 
     result = re.sub("[^A-Za-z]", " ", text.strip())
     return collections.Counter(result.split()).most_common(count)
-#print(stats_text_en(text,count))
 
 def stats_text_cn(text,count):
     '''1、统计每个中文汉字出现的次数
@@ -27,10 +25,7 @@ def stats_text_cn(text,count):
         raise ValueError ("This is not an str type")
     result = re.findall(u'[\u4e00-\u9fff]+', text)#\u是unincode编码，u4e00是十六进制表达值
     rep = ''.join(result)
-    return collections.Counter(rep).most_common(count)
-#print(stats_text_cn(text,count))
-
-        
+    return collections.Counter(rep).most_common(count)   
 
 #定义stats_word函数，分别调用stats_text_en,stats_text_cn,输出合并词频统计结果
 
@@ -46,5 +41,4 @@ def stats_text(text_bn,count):
     str1["en"] = stats_text_en(text_bn,count)
     str1["cn"] = stats_text_cn(text_bn,count)
     return str1 
-#print(stats_text(text))
 
