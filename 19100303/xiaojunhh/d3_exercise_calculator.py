482743e694bb37defe6a3e284b1da6cc13a5130c
xiaojunhh <48578523+xiaojunhh@users.noreply.github.com>
2019-03-26 23:02:45 +0800

    计算

--- 19100303/xiaojunhh/d3_exercise_calculator.py
+++ 19100303/xiaojunhh/d3_exercise_calculator.py
@@ -2,18 +2,24 @@
 #定义函数
 def add(x, y):
     """相加"""
+
     return x + y
-    def subtract(x, y):
+
+def subtract(x, y):
         """相减"""
-return x - y
+
+    return x - y
 def multiply(x, y):
 """相乘"""
+
 return x * y
+
 def divide(x, y):
     """相除"""
+
     return x / y
     
-    #输入
+#输入
 print("选择云算")
 print("1、相加")
 print("2、相减")
