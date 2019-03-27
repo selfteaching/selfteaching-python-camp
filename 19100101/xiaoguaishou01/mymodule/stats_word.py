dc1ff7c447ab45d6d428f06d6c9c667aba40a3d3
燃烧的小怪兽 <xiaoguaishou0809@gmail.com>
2019-03-27 15:38:25 +0800

    day10 第三方库

--- 19100101/xiaoguaishou01/mymodule/stats_word.py
+++ 19100101/xiaoguaishou01/mymodule/stats_word.py
@@ -2,9 +2,10 @@
 #使用字典（dict）统计字符串样本text中各个英文单词出现的次数
 text ='''The Zen of Python, by Tim Peters
 Beautiful is better than ugly.
-Explicit is better than implicit.我们只中每个英文单词出现的次数'''
+Explicit is better than implicit.字符串类型为异常类型就抛出异常抛出我们只中每个英文单词出现的次数如果不是字符串类型为异常'''
 import re
 import collections
+import jieba
 def stats_text_en(text,count):
     '''1、使用字典（dict)统计text中每个英文单词出现的次数.
     2、添加类型检查，如果不是字符串类型为异常'''
@@ -24,8 +25,13 @@ def stats_text_cn(text,count):
     elif type(count) != int:
         raise ValueError ("This is not an str type")
     result = re.findall(u'[\u4e00-\u9fff]+', text)#\u是unincode编码，u4e00是十六进制表达值
-    rep = ''.join(result)
-    return collections.Counter(rep).most_common(count)   
+    seg_list = jieba.cut(' '.join(result))#使用jieba精确模式对字符串进行分词
+    d = []
+    for i in seg_list: #使用for循环
+        if len(i) >= 2: #判断如果大于等于2的字符串就加进d数组
+            d.append(i) 
+    return collections.Counter(d).most_common(count)
+#print(stats_text_cn(text,3))   
 
 
 #定义stats_word函数，分别调用stats_text_en,stats_text_cn,输出合并词频统计结果
