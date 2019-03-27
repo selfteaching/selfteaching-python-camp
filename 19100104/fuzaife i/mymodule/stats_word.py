73ffd23207fcffd6e486b57d7606d28826e15697
fuzaifei <48574127+fuzaifei@users.noreply.github.com>
2019-03-27 21:07:10 +0800

    no message
    Signed-off-by: fuzaifei <48574127+fuzaifei@users.noreply.github.com>

--- 19100104/fuzaife i/mymodule/stats_word.py
+++ 19100104/fuzaife i/mymodule/stats_word.py
@@ -1,75 +1,43 @@
+# 这是一个单独统计英文单词和单独统计中文单字以及统计中文字和英文词词频（字频）
+
 import re
-from collections import Counter
+import collections
+import jieba
 
-def stats_text_en(text):
-    if isinstance(text, str):    
-        list1 = []    
-        text_list = re.split(r'[\s,.!\-*]', text)
-        for i in text_list:        
-            if not u'\u4e00' <= i <= u'\u9fff': 
-                list1.append(i)
-        return Counter(list1).most_common()    
+count = int()
+
+''' 创建一个名为stats_text_en的函数，它的功能是为统计英文词频'''
+def stats_text_en(text, count):
+    '''判断输入的是字符，执行以下语句'''
+    if type(text) == str:
+        '''只保留字母，并用空格作为分隔符'''
+        text_en = re.sub('[^A-Za-z]','', text.strip())
+        '''以空格以及拆行为分隔，将每个给分隔的内容作为一个列表元素，并将所有列表元素合在一起组成一个列表'''
+        list_en = text_en.split()
+        '''利用colections中的counter为list_en中的每个出现的英文单词计数，返回前count个词频最高的词以及出现的次数'''
+        return collections.Counter(list_en).most_common(count)
     else:
-        raise ValueError("This is not a string!")
+        raise ValueError('输入的类型不为字符串')
 
+'''创建一个名为stats_text_cn的函数，它的功能是为统计中文词频'''
+def stats_text_cn(text, count):
+    '''判断输入的是字符，执行以下属于'''
+    if type(text) == str:
+        '''保留所有匹配的汉字Unicode码的字符'''
+        list_cn = re.findall(u'[\u4e00-\u9fff]+', text.strip())
+        '''将list_cn不用任何分隔符链接起来'''
+        str_list = ''.join(list_cn)
+        '''day_10作业：通过jieba的精确模式将字符串str_cn进行分词，只要前长度大于2的词'''
+        seg_list = [ x for x in jieba.cut_for_search(str_cn)if len(x) >= 2]
 
-def stats_text_cn(checkstr, top_n=None):
-    if isinstance(checkstr, str):
-        cndic = {}
-        for i in checkstr:        
-            if u'\u4e00' <= i <= u'\u9fff':   
-                cndic[i] = checkstr.count(i)   
-        return Counter(cndic).most_common(top_n)
+        '''利用colections中的counter为list_cn中的每个出现的英文单词计数，返回count个词频最高的词及出现的次数'''
+        return collections.Counter(seg_list).most_common(count)
     else:
-        raise ValueError("This is not a string!")
+        raise ValueError('输入的类型不为字符串')
 
-
-def stats_text(text_an):
-    if isinstance(text_an, str):
-        str1 = {}
-        str1["en"] =stats_text_en(text_an)
-        print("""---以下是英文字符输出结果---""")
-        print(str1["en"])
-        str1["cn"] =stats_text_cn(text_an)
-        print("""---以下是中文字符输出结果---""")
-        print(str1["cn"])
-        return str1
-    else:
-        raise ValueError("This is not a string!")
-
-
-
-text_an = '''
-The Zen of Python, by Tim Peters
-美丽 is better than 丑陋.
-清楚 is better than 含糊.
-简单 is better than 复杂.
-复杂 is better than 难懂.
-单一 is better than 嵌套.
-稀疏 is better than 稠密.
-可读性计数.
-Special cases aren't special enough to 打破规则.
-即使练习会使得不再纯粹.
-但错误不应该用沉默来掩盖.
-Unless explicitly silenced.
-面对起义，拒绝猜的诱惑.
-有且只有一个办法.
-Although that way may not be obvious at first unless you're Dutch.
-尝试总比从未试过要强.
-Although never is often better than *right* now.
-如果执行很难被解释，那将是一个很糟的想法.
-如果执行很容易解释，这会是一个好点子.
-Namespaces are one honking great idea -- 让我们继续为之努力!
-'''
-
-
-stats_text(text_an)
-
-
-with open("tang300.json", 'r', encoding='utf-8') as file_object:
-    contents = file_object.read()
-    # print(contents)
-
-result = stats_text_cn(contents, top_n=100)
-print("""---唐诗300首中词频最高的前100个字---""")
-print(result)
+'''创建一个名为stats_text的函数，它的功能是为统计中文字和英文词频，并返回词频最高的前count个单词'''
+def stats_text(text, count):
+    '''利用colections中的counter为list1中的每个出现的英文单词计数，
+    并利用collections.ordereddict将stats_text_en(text)和stats_text_cn(text)中返回的结果合二为一,
+    返回前count个词频最高的词即出现的次数（其中MAX表示返回所有汉字和英文字的词频）'''
+    return  collections.OrderedDict(collections.Counter(stats_text_en(text, max)+stats_text_cn(text, max)).most_common(count))
