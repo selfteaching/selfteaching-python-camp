e187b0546b2703d4ecd620d4234902e8591e8080
cftqbxl <1245034052@qq.com>
2019-03-28 01:20:52 +0800

    Update d4_exeurcise_cobntrol_flow.py

--- 19100305/cftqbxl/d4_exeurcise_cobntrol_flow.py
+++ 19100305/cftqbxl/d4_exeurcise_cobntrol_flow.py
@@ -1,6 +1,15 @@
+for i in range(1,10):
+    for j in range(1,i+1):
+        print('%s*%s=%s'%(i,j,i*j),end='')
+        print()
+
+
+
+print()
 for i in range (1,10):
     for j in range (1,i+1):
         while  i%2!=0:
-            print ("%s*%s=%s"%(i,j,i*j),end='\t')
+            print ("%s*%s=%s\t"%(i,j,i*j),end='')
             break
-print ()
+        if i%2==0:
+            print
