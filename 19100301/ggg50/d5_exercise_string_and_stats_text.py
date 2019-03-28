d9f326dd2c998635649d3f89542ebe3244fd950a
kalagala <xjfletitgo@gmail.com>
2019-03-28 23:35:33 +0800

    remove duplicate

--- 19100301/ggg50/d5_exercise_string_and_stats_text.py
+++ 19100301/ggg50/d5_exercise_string_and_stats_text.py
@@ -1,7 +1,5 @@
 import re
 
-# d5_exercise_string
-
 text = '''
 The Zen of Python, by Tim Peters
 Beautiful is better than ugly.
@@ -24,6 +22,11 @@ If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 '''
+
+
+# d5_exercise_string
+
+
 # change "better" into "worse", and "\n" into " ", then remove "ea"
 T = text.replace("better", "worse").replace("ea", "").replace("\n", " ").strip()
 
@@ -40,18 +43,21 @@ text_swapcase = [x.swapcase() for x in text_to_list]
 
 # sort
 text_swapcase.sort()
-# for t in text_swapcase:
-#     print(t, end=" ")
-# print()
+for t in text_swapcase:
+    print(t, end=" ")
+print()
 
 # A A AMBXIGUITY AND ARE ARENT AT BAD BE BE BE BRK BTS BY CASES COMPLEX COMPLICATED COUNTS DENSE DO DO ENOUGH EXPLAIN EXPLAIN EXPLICITLY FACE FIRST GOOD GRT GUESS HARD HONKING ID ID ID IMPLEMENTATION IMPLEMENTATION IMPLICIT IS IS IS IS IS IS IS IS IS IS IT IT ITS LETS MAY MAY MORE NESTED NEVER NEVER NEVER NOT NOW OBVIOUS OBVIOUS OF OF OF OFTEN ONE ONE ONE ONLY PASS PRACTICALITY PREFERABLY PURITY REFUSE RIGHT RULES SHOULD SHOULD SILENCED SILENTLY SPECIAL SY TEMPTATION THAN THAN THAN THAN THAN THAN THAN THAN THAT THE THE THE THE THE THOSE TO TO TO TO TO UGLY UNLESS WAY WAY WORSE WORSE WORSE WORSE WORSE WORSE WORSE WORSE YOURE aLTHOUGH aLTHOUGH aLTHOUGH bUTIFUL cOMPLEX dUTCH eRRORS eXPLICIT fLAT iF iF iN nAMESPACES nOW pETERS pYTHON rDABILITY sIMPLE sPARSE sPECIAL tHE tHERE tIM uNLESS zEN
+
+
 
 # d5_exercise_stats_text.py
 
 # create dictionary
-
 sort_list = [(x, text_to_list.count(x)) for x in set(text_to_list)]
 sort_dict = dict(sort_list)
 
 print(sort_dict) # {'do': 2, 'good': 1, 'Butiful': 1, 'Tim': 1, 'silenced': 1, ...
 
+# #$%$%@$#%!#@#&*…… 
+print(sorted(sort_dict.items(), key=lambda x: x[1], reverse=True))
