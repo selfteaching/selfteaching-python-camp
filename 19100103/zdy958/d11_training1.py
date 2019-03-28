8017e76d477b3a3bfbfaf13868e5dde9cb581a5d
zdy958 <38367232+zdy958@users.noreply.github.com>
2019-03-28 23:56:29 +0800

    1900103 day 11
    @srvz

--- 19100103/zdy958/d11_training1.py
+++ 19100103/zdy958/d11_training1.py
@@ -14,7 +14,7 @@ content = document('p').text()
 
 
 '''用正则去掉文本中的数字，字母和符号'''
-content = re.sub('[a-zA-Z0-9’!"%&\'^\s+|\s+$()*+,-./:;，。?、…？“”‘’！]+',"",content)
+content = re.sub(r'[a-zA-Z0-9’!"%&\'^\s+|\s+$()*+,-./:;，。?、…？“”‘’！]+',"",content)
 '''用jieba将文本进行全模式切分'''
 seg_list = jieba.cut(content, cut_all=True)
 '''用counter统计词频'''
@@ -28,5 +28,5 @@ mystr = str(mydic)
 
 '''连接服务器，用yagmail发送邮件，此处的password为邮箱的授权码，非邮箱登录密码'''
 sendSmpt = yagmail.SMTP(user = input('请输入发件人邮箱:'),
-password=getpass.getpass('请输入发件人邮箱密码(可复制粘贴):'),host='smtp.163.com')  
+password=getpass.getpass('请输入发件人邮箱密码(可复制粘贴):'),host='smtp.126.com')  
 sendSmpt.send(to = input('请输入收件人邮箱:'),subject="19100103 zdy958",contents=mystr)
