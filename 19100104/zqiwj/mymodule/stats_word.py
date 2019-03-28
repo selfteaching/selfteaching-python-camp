4c45a208e53a1d5b21fe1fde48a17afe5e308f17
zqiwj <isn0969@dingtalk.com>
2019-03-28 23:47:25 +0800

    day11

--- 19100104/zqiwj/mymodule/stats_word.py
+++ 19100104/zqiwj/mymodule/stats_word.py
@@ -1,45 +1,46 @@
-text_en = 1
+def stats_text_cn(text, count):  # 统计中文词频，增加变量count控制输出
+    """Count the chinese words in the text """  # 使用文档字符串说明
+    from collections import Counter  # 调用Counter计数器
+    import jieba
+    if not isinstance(text, str):  # 如果不是字符串类型触发异常
+        raise ValueError("input data is not string type")
+    text = [x for x in jieba.cut_for_search(text) if len(x) >= 2]  #使用精确模式分词
+    countcn = {}
+    count = int(count)
+    for i in text:
+        if u'\u4e00' <= i <= u'\u9fff':
+            countcn[i] = text.count(i)
 
-import collections
-import re
-import jieba
-
-def stats_text_en(text_en):
-    if not isinstance(text_en,str):
-        raise ValueError('不是字符串类型(string)!')
-    result = re.sub("[^A-Za-z]", " ", text_en.strip())
-    newList = result.split( )
-    # i=0
-    for i in range(0,len(newList)):
-        newList[i]=newList[i].strip('*-,.?!')
-        if newList[i]==' ': 
-            newList[i].remove(' ')
-        else:
-            i=i+1
-    print('英文单词词频统计结果： ',collections.Counter(newList).most_common(10) )
+    countcn = Counter(countcn).most_common(count)  # 按词频排序并用count控制输出
+    return countcn
 
 
-def stats_text_cn(text_cn):
-    if not isinstance(text_cn,str):
-        raise ValueError('不是字符串类型(string)!')
-    result1 = re.findall(u'[\u4e00-\u9fff]+',text_cn)
-    newString = ''.join(result1)
-    seg_list = jieba.cut(newString, cut_all=False)
-    f = list(seg_list)
-    a = []
-    for i in range(len(f)):
-        if len(f[i]) > 1:
-            a.append(f[i])
-        else:
-            continue
-    print('中文汉字字频统计结果： ', collections.Counter(a).most_common(20))
+def stats_text_en(text, count):  # 统计英文词频，增加变量count控制输出
+    """Count the english words in the text"""  # 使用文档字符串说明
+    from collections import Counter  # 调用Counter计数器
+    import re
+    if not isinstance(text, str):  # 如果不是字符串类型，触发异常
+        raise ValueError("input data is not string type")
+    text = re.sub("[^A-Za-z]", " ", text.strip())  #确保是英文
+    text = text.replace(',', ' ').replace('.', ' ').replace(
+        '--', ' ').replace('!', ' ').replace('*', ' ')  # 去除特殊字符
+    text = text.lower()
+    text = text.split()
+    count = int(count)
+    a = {}
+    for i in text:
+        quantity = text.count(i)
+        b = {i: quantity}
+        a.update(b)
+    a = Counter(a).most_common(count)  # 按词频排序并用count控制输出
+    return a
 
-def stats_text(text):
-    if not isinstance(text,str):
-        raise ValueError('不是字符串类型(string)!')
-    stats_text_en(text)
-    stats_text_cn(text)
 
-# 以下为调试函数用
-# stats_text_en(text)
-# stats_text_cn(text)
+def stats_text(text,count):  # 合并中英文词频统计
+        """combine the stats_text_cn and stats_text_en."""
+        if not isinstance(text, str):    #如果不是字符串类型，触发异常
+            raise ValueError("input data is not string type")
+        a = stats_text_en(text,count)
+        b = stats_text_cn(text,count)
+        c = a+b
+        return c
