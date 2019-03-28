4f0ebd08261687dfc4b6b7b6b0e807b82a7ce666
silver <c15122132185@vip.163.com>
2019-03-29 05:06:31 +0800

    19100304 day5
     *字符串的基本处理
     *统计字符串样本中英文单词出现的次数
     *数组操作，进制转换

--- 19100304/excaliburyy/d5_exercise_stats_text.py
+++ 19100304/excaliburyy/d5_exercise_stats_text.py
@@ -1,7 +1,13 @@
-import collections            
-import operator
+# 19100304 day5 银零
+#【统计字符串样本中英文单词出现的次数】
+# 1.使用字典统计样本中各个英文单词出现的次数
+# 2.按照出现次数从大到小输出所有的单词及出现的次数
+# 3.只统计英文单词，不包括非英文字符的其他任何符号，如连接符号、空白字符等
+
+
+# 样本原文
 text = '''
-The Zen of Python, by Tim Peters.
+The Zen of Python, by Tim Peters
 Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
@@ -22,9 +28,16 @@ If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 '''
-text_0 = text.replace(',','').replace('.','').replace('--','')\
-    .replace('!','').replace('*','').replace('\n','')
-text_1 = text_0.split(' ')                         
-print(text_1)
-text_2 = dict(collections.Counter(text_1))              
-print(sorted(text_2.items(),key=lambda x:x[1],reverse=True))   
+
+# 1.使用字典统计样本中各个英文单词出现的次数
+import re
+list1 = re.split(r'\W+',text)                   #将字符串text转换为列表list1，只保留单词为list1中的元素
+while '' in list1:                              #删除list1中为空的元素
+    list1.remove('')
+dict1 = {}
+for i in list1:                                 #i属于list1中的元素，开始循环
+    dict1.setdefault(i,list1.count(i))          #将列表中的单词及单词的出现次数，分别赋值给dict1的键和值
+
+# 2.按照出现次数从大到小输出所有的单词及出现的次数
+tup1 = sorted(dict1.items(),key = lambda items:items[1],reverse = True)      #将dict1按照value值从大到小排列，并将结果赋值给元祖tup1
+print(tup1)
--- 19100304/excaliburyy/d5_exercise_string.py
+++ 19100304/excaliburyy/d5_exercise_string.py
@@ -1,5 +1,15 @@
+# 19100304 day5 银零
+#【字符串的基本处理】
+# 1.将字符串样本text里的better全部替换成worse
+# 2.从第二步的结果里，将单词中包含ea的单词剔除
+# 3.将第三步的结果里的字母进行大小写翻转
+# 4.将第四步的结果里所有的单词按a...z升序排列
+# 5.输出结果
+
+
+# 原始文本
 text = '''
-The Zen of Python, by Tim Peters.
+The Zen of Python, by Tim Peters
 Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
@@ -20,11 +30,28 @@ If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 '''
-print(text.replace("better","worse")) 
-text_0 = text.replace("better","worse")
-print(text_0.replace("ea",""))        
-text_1 = text_0.replace("ea","")
-print(text_1.swapcase())            
-text_2 = text_1.swapcase()
-text_3 = text_2.split(' ')           
-print(sorted(text_3))                 
+
+# 1.将字符串样本text里的better全部替换成worse
+txt1 = text.replace('better','worse')
+
+# 2.从第二步的结果里，将单词中包含ea的单词剔除
+import re
+list1 = re.split(r'(\W+)',text)     #将text字符串（string）转换为列表（list）,每一个单词和其他字符（空格，换行，‘.’，‘,’，‘*’），都成为list1列表中的一个元素
+for i in list1:                     #当i属于list1列表当中的元素时，开始循环
+    if 'ea' in i:                   #判断当'ea'属于i时，执行语句
+        list1.remove(i)             #列表list1移除i  
+txt2 = ''.join(list1)
+
+# 3.将第三步的结果里的字母进行大小写翻转
+txt3 = txt2.swapcase()
+
+# 4.将第四步的结果里所有的单词按a...z升序排列
+import re
+list2 = re.split(r'\W+',txt3)       #将text3字符串（string）转换为列表list2,只保留单词为list2中的元素
+list2.sort()
+while '' in list2:                  #删除list2中为空的列表元素
+    list2.remove('')
+
+# 5.输出结果
+txt4 = ' '.join(list2)
+print(txt4)
