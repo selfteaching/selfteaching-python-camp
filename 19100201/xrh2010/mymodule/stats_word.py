761a1b92af1259485b6840927d2b0bc563399847
xrh <xu@xudeMacBook.local>
2019-03-28 22:14:38 +0800

    D7
    越来越乱。

--- 19100201/xrh2010/mymodule/stats_word.py
+++ 19100201/xrh2010/mymodule/stats_word.py
@@ -55,34 +55,63 @@ mighty gods to carry the mountains away.
 
 
 
-#判断是否包含英文
-def stats_text_en(checkstr):
-	endic = {}
-	for ch in checkstr:
-		if u'a' <= ch <= u'z' or u'A' <= ch <= u'Z':
-			endic[ch] = checkstr.count(ch)
-	return endic
 
+dict1 = {}
+dict2 = {}
+dict3 = {}
+dict4 = {}
+  
+def stats_text_en(text):                           # 定义一个名为stats_text_en的函数
+    import re 
+    text = re.sub("[^A-Za-z]", " ", text.strip())  # 使用正则表达式"[a-zA-Z]+"，将非英文字母替换成空字符
+    list1 = re.split(r"\W+",text)                  # 将字符串text转换为列表list1,只保留单词为list1中的元素
+    while '' in list1:                             # 删除list1中为空的列表元素
+        list1.remove('')
+    for i in list1:                                # i属于list1中的元素，开始循环
+        dict1.setdefault(i,list1.count(i))         # 将列表中的单词及单词的出现次数，分别赋值给dict1的键和值
+    # 将dict1按照value值倒序排列，并将结果赋给元组tup1
+    tup1 = sorted(dict1.items(),key = lambda items:items[1],reverse = True)   
+    for tup1 in tup1:   
+            dict2[tup1[0]] = dict1[tup1[0]]  
+    return dict2
 
-def stats_text_cn(checkstr):    #定义检索中文函数
-	cndic = {}
-	for i in checkstr:
-		if u'\u4e00' <= i <= u'\u9fff':
-			cndic[i] = checkstr.count(i)
-	return cndic
+#打印统计英文词频的结果
+print("统计英文词频的结果为:")
+print(stats_text_en(text))
+str = ''
 
+def feng_zhuang(s, old_d):                            # 封装函数
+    d = old_d
+    print(s,d)
+    for c in s:
+        d[c] = d.get(c, 0) + 1
+    return d
+    
 
-def stats_text():
-	cndic = stats_text_cn(text)             #调用检索中文频次的函数
+def stats_text_cn(text):                             # 定义一个名为stats_text_cn的函数
+    import re
+    text = re.sub("[A-Za-z0-9]", "", text)           # 使用正则表达式"[A-Za-z0-9]"，将非英文字母替换成空字符
+    list1 = re.split(r"\W+",text)                    # 将字符串text转换为列表list1,只保留单词为list1中的元素
+    while '' in list1:                               
+        list1.remove('')                             # 删除list1中为空的列表元素
+    dict3 = dict()                                   # 把dict3的行拆成字典
+    for i in range(len(list1)):
+        dict3 = feng_zhuang(list1[i], dict3)         # 调用函数，引用传递参数
 
-	cndic=sorted(cndic.items(),key=lambda item:item[1],reverse = True)      #为了阅读方便，检索完毕后对字典进行按值从大到小排序
-	print(cndic)                            #打印汉字
+    # 将dict3按照value值倒序排列，并将结果赋给元组tup1
+    tup1 = sorted(dict3.items(),key = lambda items:items[1],reverse = True)  
+    for tup1 in tup1:   
+            dict4[tup1[0]] = dict3[tup1[0]]  
+    return dict4
 
-	endic = stats_text_en(text)             #调用检索中文频次的函数
+#打印统计中文词频的结果
+print("统计中文词频的结果为:")
+print(stats_text_cn(text))
 
-	endic=sorted(endic.items(),key=lambda item:item[1],reverse = True)      #为了阅读方便，检索完毕后对字典进行按值从大到小排序
-	print(endic)                            #打印汉字
-
-
-if __name__ == '__main__':
-	stats_text()
+#定义stats_text函数，合并输出
+def stats_text(text):
+    #用isinstance（变量名，类型）判断字符串类型是否正确，如果不正确抛出ValueError
+    if isinstance(text,str) != True:
+        raise ValueError
+    else:
+        return (stats_text_en(text),stats_text_cn(text))
