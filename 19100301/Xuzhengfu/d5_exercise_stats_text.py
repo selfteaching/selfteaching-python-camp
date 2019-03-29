fdd7594ce0c83f9ed74aaa900169f2c77bfd54ac
Zhengfu <xuzhengfu.g@gmail.com>
2019-03-29 13:19:17 +0800

    19100301 day5

--- 19100301/Xuzhengfu/d5_exercise_stats_text.py
+++ 19100301/Xuzhengfu/d5_exercise_stats_text.py
@@ -25,15 +25,13 @@ Namespaces are one honking great idea -- let's do more of those!
 # 二、统计字符串样本中英文单词出现的次数
 # 1、创建一个名为 d5_exercise_stats_text.py 的文件
 # 2、使用字典（dict）统计字符串样本 text 中各个英文单词出现的次数。
-symbol_deleting = [',','.','!','-','*']  # 将非词类字符均替换为空格
+symbol_deleting = [',','.','!','-','*']                  # 将非词类字符均替换为空格
 for x in symbol_deleting:
    text = text.replace(x,'')
-text = text.lower()                      # 将所有单词改为小写
-text = text.split()                      # 以空格为分隔符，分隔字符串text，使之成为列表text
-stats = {}
-for i in text:
-   times = text.count(i)
-   key_times = {i:times}
-   stats.update(key_times)
+text = text.lower()                                      # 将所有单词改为小写
+text = text.split()                                      # 以空格为分隔符，分隔字符串text，使之成为列表text
+stats = dict([(word,text.count(word)) for word in text]) # 使用list comprehension和dict()生成词典
 
 # 3、按照出现次数从大到小输出所有的单词及出现的次数
+stats = sorted(stats.items(),key=lambda item:item[1],reverse=1)
+print(stats)
