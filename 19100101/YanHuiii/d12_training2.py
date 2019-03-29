ca8d514ba182e376a59999cdf54dc209562da985
YanHui <yhhero4016@sina.com>
2019-03-29 23:29:31 +0800

    day12 19100101 Second modification

--- 19100101/YanHuiii/d12_training2.py
+++ 19100101/YanHuiii/d12_training2.py
@@ -7,16 +7,13 @@ from wxpy import *
 '''登录微信'''
 bot = Bot()
 
-'''选取特定的对象小明'''
 object1 = bot.friends()
-
-
 
 @bot.register(object1)
 def SHARING_Msg (msg) :
 	if msg.type == 'Sharing' :
 	    url = msg.url
-	    object1.send(training1.stats_meg(url))
+	    msg.reply(training1.stats_meg(url))
 	else :
 	    print("您的好友还没发给您关于分享类型的信息")
 
