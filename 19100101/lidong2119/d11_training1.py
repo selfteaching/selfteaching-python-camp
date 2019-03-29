fb10a603d4c2669865312807319edc7fe7bb8055
李栋 <470650363@qq.com>
2019-03-30 00:29:32 +0800

    19100101 day12 again

--- 19100101/lidong2119/d11_training1.py
+++ 19100101/lidong2119/d11_training1.py
@@ -8,15 +8,6 @@ def stats(url):
     document = PyQuery(response.text)
     content = document('#js_content').text()
 
-
-
-
     statList = stats_word.stats_text(content,100)
     statString = ''.join(str(i) for i in statList)
-
-<<<<<<< HEAD
     return statString
-=======
-yag = yagmail.SMTP(sender,password,smtp)
-yag.send(recipient,'lidong2119 d11',statString)
->>>>>>> cf86c537e790ffb7f78df7821ac287627dc4bbc6
