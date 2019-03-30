967a1345a8aa04ab8dd30fd4fe0af997c274845e
wiltonwung <wiltonwung@gmail.com>
2019-03-30 22:35:38 +0800

    19100201 day10

--- 19100201/wiltonwung/mymodule/stats_word.py
+++ 19100201/wiltonwung/mymodule/stats_word.py
@@ -1,5 +1,13 @@
+import jieba
 import collections
+
 from collections import Counter
+
+
+
+
+
+
 count = int()
 def stats_text_en(text,count):
     import re
@@ -10,7 +18,17 @@ def stats_text_cn(text,count):
     import re
     text_cn = re.findall(u'[\u4e00-\u9fff]+', text.strip())
     str_cn = ''.join(text_cn)
-    return collections.Counter(str_cn)
+    seg_list = jieba.cut(str_cn)
+    nlist = []
+    for i in seg_list:
+        if len(i)>=2:
+            nlist.append[i]
+        else:
+            pass
+
+    return collections.Counter(nlist).most_common(count)
+
 def stats_text(text,count):
     return collections.Counter(stats_text_en(text,count)+stats_text_cn(text,count)).most_common(count)
+
 
