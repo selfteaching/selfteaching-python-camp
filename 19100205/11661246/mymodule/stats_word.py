ef6e7d13a6a1834bb72372a4c1779e60e1415f17
ZoroYa <48607383+11661246@users.noreply.github.com>
2019-03-30 22:56:05 +0800

    19100205 10day

--- 19100205/11661246/mymodule/stats_word.py
+++ 19100205/11661246/mymodule/stats_word.py
@@ -1,3 +1,7 @@
+# encoding=utf 
+import jieba #day10
+import collections
+
 # 函数1：统计输入文本中英文单词的词频：
 def stats_text_en(text):
     if not isinstance(text,str):
@@ -17,14 +21,19 @@ def stats_text_en(text):
 def stats_text_cn(text):  
     if not isinstance(text,str):
         raise ValueError('输入的不是文本格式，请重新输入：') # 第8天作业要求，添加参数类型检查
-    dic = {}
+    
+    text1 = []
     for i in text:  # 这个循环有效，说明一串汉字也是一个字符串，每个汉字就是其中的一个元素，可以用for in 来遍历，其中i代表了每个汉字的unicode编码
-        if u'\u4e00' <= i <= u'\u9fff':     # 挑选出中文字
-            dic[i] = text.count(i)      # 用.count()函数/方法来对每个元素（这里是汉字）进行计数，形成一个字典
-    import collections
+        if u'\u4e00' > i > u'\u9fff':     # 挑选出非中文字
+            text=text.split(i,"") # 将非中文字符替换为空格
+    seg_list = jieba.cut(text,cut_all =False)
+    
+    for j in seg_list:
+             if len(j) >= 2 : #只统计长度大于等于2的词
+              text1.append(j)
     count = int(input('请输入要限制输出的元素个数：'))
-    dic = collections.Counter(dic).most_common(count)  #按出现次数从大到小排列
-    return dic
+    text1 = collections.Counter(text1).most_common(count)  #按出现次数从大到小排列
+    return text1
 
 
 # 函数3：统计中英文混合词频：
