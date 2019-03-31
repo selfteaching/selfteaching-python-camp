38de41de4429542e8a80c48b5630fb477106e08b
Gdong24 <gdong.24@gmail.com>
2019-03-31 19:19:58 +1100

    Update d11_training1.py

--- 19100202/Gdong24/d11/d11_training1.py
+++ 19100202/Gdong24/d11/d11_training1.py
@@ -6,16 +6,16 @@ import stats_word
 from pyquery import PyQuery
 
 
-# 设置发件人、登录密码、收件人
+#设置发件人、登录密码、收件人
 sender = input('请输入发件人邮箱地址：')
 psw = input('请输入发件人邮箱登录密码：')
 recipient = input('请输入收件人邮箱地址：')
 smtp = 'smtp.qq.com'
 
-# 获取微信公众号文章   
+#获取微信公众号文章   
 response = requests.get('https://mp.weixin.qq.com/s/pLmuGoc4bZrMNl7MSoWgiA')
 
-# 提取微信公众号正文
+#提取微信公众号正文
 document = PyQuery (response.text)
 content = document ('#js_content').text() 
 #print(content)
@@ -28,6 +28,11 @@ try:
 except:
     print("对象不是字符串类型！")
 
+# 统计前100词频
+statList = stats_word.stats_text_cn(content)
+statString = ''.join(str(i) for i in statList)
+print(statString)
 
-
+#将统计结果发送到 
+yagmail.SMTP(sender,psw,smtp).send(recipient,'19100202 gdong',statString)  
 
