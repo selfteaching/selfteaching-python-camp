3279c44cb4c8083a6ef29a83825e14539594c767
luodongrui <843435679@qq.com>
2019-04-01 21:30:11 +0800

    19100201 day12

--- 19100201/luodongrui/d12_training2.py
+++ 19100201/luodongrui/d12_training2.py
@@ -1,5 +1,5 @@
 # 导入模块
-import wxpy
+from wxpy import *
 from pyquery import PyQuery
 import requests
 import stats_word
@@ -7,17 +7,18 @@ import stats_word
 bot = Bot()
 
 # 监听好友消息
+my_friend = bot.friends()
 @bot.register(my_friend)
-def get_msg (msg):  
+def reply_my_friend(msg):
 # 如果消息为 分享(SHARING)类型，则获取消息的网页链接(msg.ugl)
-    if msg.type == 'Sharing' :
-        target_url = msg.url
-        stats_msg = d12_training1.stats(target_url)
-# 将获取的链接使用 实战项目1中的方式进行处理
-url = target_url 
-response = requests.get(url)
-document = PyQuery(response.text) 
-content = document('#js_content').text()
-string = ','.join(str(i) for i in stats_word.stats_text_cn(content))
-# 将处理结果返回给发送消息过来的好友
-my_friend.send(stats_msg)
+    if msg.type =='Sharing':    
+        response = requests.get(msg.url)  
+        document = PyQuery(response.text)   
+        content = document('#js_content').text()            
+        c= str(stats_word.stats_text(content, 100))
+        # 将获得的信息回复好友
+        msg.reply(c)
+    else:
+        pass
+    
+embed()
