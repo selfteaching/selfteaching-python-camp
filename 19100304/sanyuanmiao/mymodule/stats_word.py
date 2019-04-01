a52e7035c2ae97df3f504dadc3a0ca16e67f0c70
sanyuanmiao <48642271+sanyuanmiao@users.noreply.github.com>
2019-04-01 20:27:06 +0800

    Update stats_word.py

--- 19100304/sanyuanmiao/mymodule/stats_word.py
+++ 19100304/sanyuanmiao/mymodule/stats_word.py
@@ -1,73 +1,28 @@
-# 一、将统计中文字频和英文词频的函数封装为一个模块
-# 1、添加一个名为 stats_text 的函数，实现功能：分别调用 stats_text_en, stats_text_cn，输出合并词频统计结果
+import collections
+import re#导入正则表达式模块
 
-# 1.1 封装统计英文单词词频的函数
-def stats_text_en(string):
-   """
-      0/ check parameter type
-      1/ Counts the number of occurrences of each word in the parameter;
-      2/ Returns an array in descending order of word frequency.
-   """
-   if type(string) == str:
-      symbol_deleting_en = [',','.','!','-','*']
-      for x in symbol_deleting_en:
-         string = string.replace(x,'')
-      string = string.lower()
-      string = string.split()
-      stats = dict([(word,string.count(word)) for word in string])
-      stats = sorted(stats.items(),key=lambda item:item[1],reverse=1)
-      return stats
-   else:
-      raise ValueError(string)
+def stats_text_en(text):
+    i=int (input("英文词频最高的前i个词，请输入i："))
+    if not isinstance(text,str):#判断参数类型
+        raise ValueError("text非字符串参数")#抛出异常类型
+    a= text.lower()
+    a=re.sub("[^\\u0061-\u007a]", " ", a)#小写字母unicode范围，筛选英文
+    a=a.split()#指定分隔符对字符串进行切片
+    print(collections.Counter(a).most_common(i))
+ 
+#统计中文词频
 
-# 1.2 封装统计中文汉字字频的函数
-def stats_text_cn(string):
-   """
-      0/ check parameter type
-      1/ Counts the number of occurrences of each character in the parameter;
-      2/ Returns an array in descending order of character frequency.
-   """
-   if type(string) == str:
-      symbol_deleting_cn = ['~','！',"?","…",'，','。','：',"—","”","“"," ","「","」","\n"]
-      for x in symbol_deleting_cn:
-         string = string.replace(x,'')
-      string = [character for character in string]
-      stats = dict([(character,string.count(character)) for character in string])
-      stats = sorted(stats.items(),key=lambda item:item[1],reverse=1)
-      return stats
-   else:
-      raise ValueError(string)      
-
-# 1.3 对中英文混杂的字符串进行再分类
-def Reclassify_cn(string):
-   """ Pick out Chinese characters in the string ”text“. """
-   if type(string) == str:
-      string_cn = string[:string.find("How")]
-      return string_cn
-   else:
-      raise ValueError(string)
-
-def Reclassify_en(string):
-   """ Pick out English words in the string ”text“. """
-   if type(string) == str:
-      string_en = string[string.find("How"):]
-      return string_en
-   else:
-      raise ValueError(string)
+def stats_text_cn(text):
+    j=int (input("中文字频最高的前j个词，请输入j："))
+    if not isinstance(text,str):#判断参数类型
+        raise ValueError("text非字符串参数")#抛出异常类型
+    #提取中文字符串
+    text= re.sub("[^\u4e00-\u9fa5]", "", text)#中文汉字unicode范围
+    print(collections.Counter(text).most_common(j))
 
 
-# 1.4 将统计中文字频和英文词频的函数封装为一个函数
-def stats_text(string):
-   """
-      1/ Reclassify the string that mix Chinese characters and English words;
-      2/ Counts the number of occurrences of each English word and each Chinese character in the parameter;
-      3/ Returns an array in descending order of their frequency.
-   """
-   if type(string) == str:
-      string_cn = Reclassify_cn(string)
-      string_en = Reclassify_en(string)
-      result_cn = stats_text_cn(string_cn)
-      result_en = stats_text_en(string_en)
-      print(result_en,"\n","\n","\n",result_cn)
-   else:
-      raise ValueError(string)
+#调用stats_text_en , stats_text_cn 函数，合并统计结果
+
+def stats_text(text):
+    stats_text_en(text)
+    stats_text_cn(text)
