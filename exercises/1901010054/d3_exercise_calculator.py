4805d152f925c30f2428cbafc3b2e5be7705ab15
Evan <li_2281838166@gmail.com>
2019-04-02 21:12:22 +0800

    Create d3_exercise_calculator.py

---
+++ exercises/1901010054/d3_exercise_calculator.py
@@ -0,0 +1,18 @@
+#长方形长宽差及周长
+length = 6
+width = 4
+difference = length - width
+print(difference)   
+#长度与宽度之差
+
+perimeter = length *2 + width *2
+print(perimeter)  
+#长方形周长
+
+#比值保留小数点两位
+a = 57
+b = 13
+c = a / b
+round(c,2) 
+#结果保留小数点后两位
+
