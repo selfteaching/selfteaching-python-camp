129506605d4bca4bf08deddf5176b28e84b21955
sanyuanmiao <48642271+sanyuanmiao@users.noreply.github.com>
2019-04-02 22:21:11 +0800

    Update stats_word.py

--- 19100304/sanyuanmiao/mymodule/stats_word.py
+++ 19100304/sanyuanmiao/mymodule/stats_word.py
@@ -1,79 +1,47 @@
-import re
+# encoding=utf 
+# 调用jieba
+import jieba #day10
+#调用counter
 import collections
+import re
 
-dict1 = {}
-dict2 = {}
-str1 = ''
-count = int()
+# 函数1：统计输入文本中英文单词的词频：
+def stats_text_en(text,count):
+    if not isinstance(text,str):
+        raise ValueError('输入的不是文本格式，请重新输入：') # 第8天作业要求，添加参数类型检查
+    text = text.replace('.', '').replace('!', '').replace('--', '').replace('*', '').replace(',', '').replace('(', '').replace(')', '').replace(';', '').replace(':', '').replace('\'', '').replace('?', '').replace('_', '').replace('-', '').replace('/', '') .replace('[', '') .replace(']', '') .replace('\\', '') .replace('\"', '').replace('{', '').replace('}', '').replace('\t', '').replace('\n', '').replace('\r\n', '')    
+        
+    # 以上替换去除各种标点符号
+    list_text = text.split() # 将字符串转换为列表
+    import collections
+    dic = collections.Counter(list_text).most_common(count)
+    return dic
 
-"""创建一个名为stats_text_en的函数，它的功能是为统计英文词频"""
-def stats_text_en(text):
+
+
+# 函数2：统计输入文本中中文字的词频：
+def stats_text_cn(text,count):  
+    if not isinstance(text,str):
+        raise ValueError('输入的不是文本格式，请重新输入：') # 第8天作业要求，添加参数类型检查
     
-    '''只保留英文'''
-    text = re.sub("[^A-Za-z]", " ", text.strip())
-    '''将字符串text转换为列表list1,只保留单词为list1中的元素'''
-    list1 = re.split(r"\W+",text)   
-    '''删除list1中为空的列表元素'''
-    while '' in list1:   
-        list1.remove('')
+    text1 = []
+    for i in text:  # 这个循环有效，说明一串汉字也是一个字符串，每个汉字就是其中的一个元素，可以用for in 来遍历，其中i代表了每个汉字的unicode编码
+        if u'\u4e00' > i > u'\u9fff':     # 挑选出非中文字
+            text=text.split(i,"") # 将非中文字符替换
+    seg_list = jieba.cut(text,cut_all =False) 
     
-    '''利用colections中的Counter为list1中的每个出现的英文单词计数，并以字典的形式返回'''
-    return collections.Counter(list1)
+    for j in seg_list:
+             if len(j) >= 2 : #只统计长度大于等于2的词
+              text1.append(j)
+    text1 = collections.Counter(text1).most_common(count)  #按出现次数从大到小排列
+    return text1
 
 
-'''创建一个名为stats_text_cn的函数，并用它实现统计汉字词频的功能'''
-
-
-"""创建一个名为stats_text_cn的函数，它的功能是为统计中文词频"""
-def stats_text_cn(text):
-    
-    """去掉text中的英文和数字"""
-    text = re.sub("[A-Za-z0-9]", "", text)
-    '''将字符串text转换为列表list1,只保留单词为list1中的元素'''
-    list1 = re.split(r"\W+",text)   
-
-    '''删除list1中为空的列表元素'''
-    while '' in list1:   
-        list1.remove('')
-    
-    str1 = ''.join(list1)
-    
-    return collections.Counter(str1)
-
-#第九天的代码
-"""创建一个名为stats_text_count的函数，它的功能是为统计中文字和英文词频,并返回词频最高的前count个单词"""
-
-def stats_text_count(text,count):
-    '''利用colections中的Counter为list1中的每个出现的英文单词计数，并利用collections.OrderedDict将stats_text_en(text)和stats_text_cn(text)的返回结果合二为一，返回前count个词频最高的词及出现的次数'''
-    return collections.OrderedDict(collections.Counter(stats_text_en(text)+stats_text_cn(text)).most_common(count))
-
-"""创建一个名为stats_text_en_count的函数，它的功能是为统计英文词频,并返回词频最高的前count个单词"""
-def stats_text_en_count(text,count):
-    
-    '''只保留英文'''
-    text = re.sub("[^A-Za-z]", " ", text.strip())
-    '''将字符串text转换为列表list1,只保留单词为list1中的元素'''
-    list1 = re.split(r"\W+",text)   
-    '''删除list1中为空的列表元素'''
-    while '' in list1:   
-        list1.remove('')
-    
-    '''利用colections中的Counter为list1中的每个出现的英文单词计数，返回前count个词频最高的词及出现的次数'''
-    return collections.Counter(list1).most_common(count)
-
-"""创建一个名为stats_text_cn_count的函数，它的功能是为统计中文字频,并打印字频最高的前count个单词"""
-def stats_text_cn_count(text,count):
-    
-    """去掉text中的英文和数字"""
-    text = re.sub("[A-Za-z0-9]", "", text)
-    '''将字符串text转换为列表list1,只保留单词为list1中的元素'''
-    list1 = re.split(r"\W+",text)   
-
-    '''删除list1中为空的列表元素'''
-    while '' in list1:   
-        list1.remove('')
-    
-    str1 = ''.join(list1)
-    
-    '''利用colections中的Counter为list1中的每个出现的英文单词计数，返回前count个字频最高的字及出现的次数'''
-    return collections.Counter(str1).most_common(count)
+# 函数3：统计中英文混合词频：
+def stats_text(text,count) :
+    '''合并中英文词频'''
+    '''参数类型检查，如果输入参数不为字符串抛出ValueError'''
+    if  not  isinstance(text, str) :
+        return(stats_text_en(text,count) + stats_text_cn(text,count))
+    else:
+        raise ValueError("输入的不是字符串")
