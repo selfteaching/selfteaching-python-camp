57728a7405dec564547ad3b727e5e9407539d278
kalagala <xjfletitgo@gmail.com>
2019-04-02 23:37:18 +0800

    Day_10

--- 19100301/ggg50/main_day9.py
+++ 19100301/ggg50/main_day9.py
@@ -1,4 +1,4 @@
-from mymodule import stats_word_day10
+from mymodule import stats_word_day9
 import json
 
 
@@ -6,4 +6,4 @@ with open("tang300.json", "r", encoding='utf-8') as f:
     text_tang = f.read()
 
 # input text and define output word numbers
-stats_word_day10.stats_text_cn(text_tang, 100)
+stats_word_day9.stats_text_cn(text_tang, 100)
