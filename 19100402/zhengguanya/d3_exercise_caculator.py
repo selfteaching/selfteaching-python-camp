4c852315ab979e0c45017be0cf6801ac2b582273
ZhengGuanya <48589364+ZhengGuanya@users.noreply.github.com>
2019-04-02 19:28:18 +0800

    Update d3_exercise_caculator.py
    final！

--- 19100402/zhengguanya/d3_exercise_caculator.py
+++ 19100402/zhengguanya/d3_exercise_caculator.py
@@ -1,68 +1,38 @@
-def caculator():
-    formula00 = input(('请输入公式'))
-    formula01 = ''
-    for i in formula00:
-        if i.isspace():
-            pass
-        else:
-            formula01 += i
-    num01 = ''
-    num_list = [] #用来存储单独数字
-    symbol = [] #用来存储运算符号
-    count = 0 #计数器，判断是否连续出现运算符
-    for i in str(formula01):
-        if count > 1:
-            print('输入有误，请重新输入')
-            break
-        elif i.isdigit() or i == '.':
-            num01 += i   #判断单个字符，对于连续的数字字符，将会组合在一起，即'789' 变为数字
-            count = 0     #出现数字符号，计数器重置为0
-        elif i == '*' or i == '/' or i == '+' or i == '-':
-            num_list.append(float(num01))
-            num01 = ''
-            symbol.append(i)
-            count += 1 #出现运算符，计数器加1，连续出现，count>1，第一个if条件生效
-        else:
-            print('输入有误，请重新输入')
-            break
-    num_list.append(float(num01))  #在公式最后不会出现运算符，所以需要将最后一个存入列表
+def add(x, y):
+    return x + y
 
-    #对num_list和symbol两个列表进行处理，进行运算
-    while symbol:
-        while "*" in symbol or "/" in symbol: #先寻找*和/运算
-            for i in symbol:
-                if i == "*":
-                    #找到运算符对应位置的symbol列表下标，对num_list里面对应位置的两个相邻数据进行运算，
-                    # 并用结果覆盖原数据，同时删除symbol列表里的运算符
-                    j = symbol.index("*")
-                    result = num_list[j] * num_list[j+1]
-                    del num_list[j:j+2]
-                    num_list.insert(j,result)
-                    symbol.remove("*")
-                    break
-                if i == "/":
-                    j = symbol.index("/")
-                    result = num_list[j] / num_list[j + 1]
-                    del num_list[j:j + 2]
-                    num_list.insert(j, result)
-                    symbol.remove("/")
-                    break
 
-        while "+" in symbol or "-" in symbol:
-            for i in symbol:
-                if i == "+":
-                    j = symbol.index("+")
-                    result = num_list[j] + num_list[j+1]
-                    del num_list[j:j + 2]
-                    num_list.insert(j, result)
-                    symbol.remove("+")
-                    break
-                if i == "-":
-                    j = symbol.index("-")
-                    result = num_list[j] - num_list[j + 1]
-                    del num_list[j:j + 2]
-                    num_list.insert(j, result)
-                    symbol.remove("-")
-                    break
-    return num_list
-print(caculator())
+def subtract(x, y):
+    return x - y
+
+
+def multiply(x, y):
+    return x * y
+
+
+def divide(x, y):
+    return x / y
+
+
+# 用户输入
+print("选择运算：")
+print("1、相加")
+print("2、相减")
+print("3、相乘")
+print("4、相除")
+choice = input(("输入你的选择（1/2/3/4):"))
+num1 = int(input("x="))
+num2 = int(input("y="))
+
+if choice == '1':
+    print(num1, "+", num2, "=", add(num1, num2))
+elif choice == '2':
+    print(num1, "-", num2, "=", subtract(num1, num2))
+elif choice == '3':
+    print(num1, "*", num2, "=", multiply(num1, num2))
+elif choice == '4':
+    print(num1, "/", num2, "=", divide(num1, num2))
+elif choice == '4':
+    print(num1, "//", num2, "=", divide(num1, num2))
+else:
+    print("非法输入")
