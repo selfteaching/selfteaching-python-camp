50abf2bb8252fcf3bf0f1ce7ec8c5fa6e200df14
andyzou <649687383@qq.com>
2019-04-03 00:04:41 +0800

    d3_exercice_calculator.py
    day 3

---
+++ 19100401/zoushui/d3_exercice_calculator.py
@@ -0,0 +1,17 @@
+del cal():
+    numA = float(raw_input('please enter a number: '))
+    op = raw_input('please enter a operator: ')
+    numB = float(raw_input('please enter another number: '))
+    if op == '+':
+        print "result: ",numA+numB
+    elif op == '-'
+        print "result: ",numA-numB
+    elif op == '*'
+        print "result: ",numA*numB
+    elif op == '/'
+        print "result: ",numA/numB
+        
+    else:
+        print "Unknown opertor ",opcal()
+
+    
