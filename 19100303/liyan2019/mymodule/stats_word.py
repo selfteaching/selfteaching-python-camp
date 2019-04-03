4f2207394271bd606aa710577127ebd212d8eecb
liyan2019 <muzi6120000@163.com>
2019-04-03 16:56:16 +0800

    day7 module

--- 19100303/liyan2019/mymodule/stats_word.py
+++ 19100303/liyan2019/mymodule/stats_word.py
@@ -28,3 +28,18 @@ def stats_text_en(text):
     # 上述if……else，与“wordcount=split_text.count(i)” 有什么区别？                
     wordcount=sorted(wordcount.items(),key=lambda x:x[1],reverse=True)
     return wordcount
+
+
+# 添加stats_text()函数，以调用 stats_text_cn(text) 和stats_text_en(text)，输出合并词频统计结果。
+
+## ?? 如何调用两个函数 【原来就是两个函数相加这么简单。】
+# 但是英文单词词频函数中包括了中文的情况--只要中文字中间没有空格，会将相连的中文字认定为一个单词，如何处理？】
+def stats_text(text):  # 合并中英文词频统计
+        """combine the stats_text_cn and stats_text_en,count english words and chinese characters"""
+        a = stats_text_en(text)
+        b = stats_text_cn(text)
+        c = a+b
+        print(c) #与前述a、b、c对齐，如果不对齐，而与 def 对齐则会出错。
+
+text='''合并中英文词频中英文。 python useful and interesting, go and run python.'''
+print(stats_text(text))
