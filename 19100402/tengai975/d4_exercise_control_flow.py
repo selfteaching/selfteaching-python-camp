ba65b0fc7d6d2bd2eabfff8ea04d4b773e8a0880
tengai975 <84120483@qq.com>
2019-04-04 05:10:12 +0800

    D4 flow99乘法表
    I copied it from the Internet,but I still don't fully understand it

---
+++ 19100402/tengai975/d4_exercise_control_flow.py
@@ -0,0 +1,26 @@
+for i in range (1,10):
+    for j in range(1,10):
+        print(j,"x",i,"=",i*j,"\t",end="")
+        if i==j:
+            print("")
+            break
+
+
+i=0
+j=0
+while i<9:
+    i+=1
+    while j<9:
+        j+=1
+        print(j,"x",i,"=",i*j,"\t",end="")
+        if i==j:
+            j=0
+            print("")
+            break
+
+
+
+
+
+
+
