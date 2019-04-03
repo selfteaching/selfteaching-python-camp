f8b3d8f93be33237f9efc9ba8ff227544578a842
yangxueximao <15031928600@139.com>
2019-04-03 18:40:55 +0800

    Update d4_exercise_control_flow.py
    修改

--- 19100401/yangxueximao/d4_exercise_control_flow.py
+++ 19100401/yangxueximao/d4_exercise_control_flow.py
@@ -1,7 +1,7 @@
 #for...in打印九九乘法表
 for i in range (1,10):
     for j in range(1,10):
-        print(i,"*",j,"=","\t",end="")
+        print(i,"*",j,"=",i*j,"\t",end="")
         if i==j:
             print("")
             break
