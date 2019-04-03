e604936dd6c8911d84eeda4b327e756a6813bc83
xiaojunhh <48578523+xiaojunhh@users.noreply.github.com>
2019-04-03 23:09:37 +0800

    Day 7.1修改

---
+++ 19100303/xiaojunhh/modules/geometry/main.py
@@ -0,0 +1,4 @@
+
+import geometry.line
+result=geometry.line.slope(1,1,3,3)
+print("斜率"，result)
---
+++ 19100303/xiaojunhh/modules/modules/geometry/line.py
@@ -0,0 +1,4 @@
+def len(x1,y1,x2,y2):
+    return (((x2-x1)**2)+((y2-y1)**2))**0.5
+def slope(x1,y1,x2,y2):
+    return (y2-y1)/(x2-x1)
