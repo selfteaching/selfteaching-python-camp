1fc7fd8a1436ee750955b2cbf0d7c145de0b44e3
lunbixiaozi <47010339+lunbixiaozi@users.noreply.github.com>
2019-04-03 15:19:34 -0400

    second merge for day 11

--- 19100303/lunbixiaozi/d11_training1.py
+++ 19100303/lunbixiaozi/d11_training1.py
@@ -1,36 +1,31 @@
 from pyquery import PyQuery
 import yagmail
-
+import requests
 import getpass
+import mymodule.stats_word
+
+Inscription = '''
+Best regards
+Zheng Zhang
+MBA, Class of 2019, Stephen M. Ross School of Business
+Tauber Institute for Global Operations
+University of Michigan
+
+'''
 
 r=requests.get('https://mp.weixin.qq.com/s/pLmuGoc4bZrMNl7MSoWgiA',auth=('user','pass'))
-document=pyquery.PyQuery(r.text)
+document=PyQuery(r.text)
 content=document('#js_content').text()
 
-mdict=[]
-strResult=''
-try:
-    mdict=stats_word.stats_text_cn(text)
-except:
-    print("error:",sys.exc_info()[0])
+counter_cn_dict = mymodule.stats_word.stats_text_cn(content, 100)
+counter_cn_string = str(counter_cn_dict)
 
-strResult=','.join([str(x) for x in mdict])
+mail_body = 'Hi \nHere are the most common 100 words in the speech: \n' + counter_cn_string + '\n\n' + Inscription
+print (mail_body)
 
+sender = input('输入发件人邮箱: ')
+password = getpass.getpass('输入发件人邮箱密码: ')
+recipients = input('输入收件人邮箱: ')
 
-# document = PyQuery(response.text)
-# content = document('#js_content').text()
-
-
-sender = input('输入发件人邮箱')
-password = getpass.getpass('输入发件人邮箱密码:')
-recipients = input('输入收件人邮箱:')
-
-yag = yagmail.SMTP('zzfuture1983', 'password')
-yag.send('recipients', 'test yagmail', 'This is the body')
-
-# sender = input('Please input your email address:')
-# password = getpass.getpass('Please input your password:')
-# recipients = input('Please input the recipients:')
-# title = input('Please input the title:')
-# yag = yagmail.SMTP(sender,password)
-# yag.send(recipients,'19100303 张正', 'oh just testing.')
+yag = yagmail.SMTP('zhz@umich.edu', password)
+yag.send(recipients, '19100303 lunbixiaozi', mail_body)
