d02daf1e9bc30d5d11b13d9db0f9ad5bf4b0884b
rock zhang <rockzhang666@gmail.com>
2019-04-04 15:45:36 +0800

    Update 1001S01E05_string.py

--- 19100402/zhangshuzhong/1001S01E05_string.py
+++ 19100402/zhangshuzhong/1001S01E05_string.py
@@ -22,17 +22,25 @@ Namespaces are one honking great idea -- let's do more of those!
 '''
 text1=text.replace('better','worse')#使用str.replace()函数，主要用于字符串中元素的替换，用后一个元素替换前一个元素
 print(text1)
-text2=text1.replace('ea','')#删除'ea'，相当于用于个空元素替换'ea‘
-print(text2)
-text3=text2.swapcase()#str.swapcase()函数用于将字符串中的字母进行大小写替换
-print(text3)
 
 #将字符串中的标点符号去掉
 symbol = ",.!-*"
 for str in symbol:
-    text3= text3.replace(str,'')
-print(text3)
+    text1= text1.replace(str,'')
+print(text1)
 
 #split()函数，针对制定分割符号将字符串进行切片，并返回分割后的字符串列表
-text4=text3.split()#这里制定的分割符号是''，这里与上面替换标点符号时候所替换的符号相对
-print(sorted(text4))#列表可以直接用sorted()函数排序
+text2=text1.split()#这里制定的分割符号是''，这里与上面替换标点符号时候所替换的符号相对
+
+
+text3=[]#新建一个空的列表
+for i in text2:
+    if i.find('ea')<0:#逐一寻找“ea”,如果该单词中包含ea那么find()函数会输出其位置，如果没有那么结果会为-1（及小于0）
+        text3.append(i)#如果结果小于0，那么就将这个单词添加到新的列表中，开始下一个循环
+print(text3)
+
+str1=" ".join(text3)#join()是将列表转换为字符串，每个单词之间我使用空格作为间隔
+str2=str1.swapcase()#str.swapcase()函数用于将字符串中的字母进行大小写替换
+print(str2)
+
+print(sorted(str2.split( )))#先使用split()函数将字符串转化为列表，这里的分割符号是空格。然后再使用sorted()函数对列表排序
