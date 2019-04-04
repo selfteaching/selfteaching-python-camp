5574aedd8ef15a01e81e1bbf463dde3e49e90292
jiahaolawyer <94697734@qq.com>
2019-04-04 22:48:24 +0800

    Update d11_training1.py

--- 19100301/jiahaolawyer/d11_training1.py
+++ 19100301/jiahaolawyer/d11_training1.py
@@ -1,31 +1,29 @@
-setup(name='jieba',
-      version='0.39',
-      description='Chinese Words Segementation Utilities',
-      long_description=LONGDOC,
-      author='Sun, Junyi',
-      author_email='ccnusjy@gmail.com',
-      url='https://github.com/fxsjy/jieba',
-      license="MIT",
-      classifiers=[
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Natural Language :: Chinese (Simplified)',
-        'Natural Language :: Chinese (Traditional)',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.2',
-        'Programming Language :: Python :: 3.3',
-        'Programming Language :: Python :: 3.4',
-        'Topic :: Text Processing',
-        'Topic :: Text Processing :: Indexing',
-        'Topic :: Text Processing :: Linguistic',
-      ],
-      keywords='NLP,tokenizing,Chinese word segementation',
-      packages=['jieba'],
-      package_dir={'jieba':'jieba'},
-      package_data={'jieba':['*.*','finalseg/*','analyse/*','posseg/*']}
-)
+import requests
+from pyquery import PyQuery
+from mymodule import stats_word
+
+
+# 定义stats函数，通过url获取文本并分析
+def stats (url,num) :
+    response = requests.get(url)
+    # 提取微信公众号正文
+    document = PyQuery (response.text)
+    content = document ('#js_content').text() 
+    # 统计前100词频
+    statList = stats_word.stats_text(content,num)
+#    statString = ''.join(str(i) for i in statList)
+    statDict = dict(statList)
+    return statDict
+
+# 发送邮件功能的实现方法（暂不需要）
+# import yagmail
+# import getpass
+
+# 设置发件人、登录密码、收件人
+# sender = input('请输入发件人邮箱地址：')
+# psw = input('请输入发件人邮箱登录密码：')
+# recipient = input('请输入收件人邮箱地址：')
+# smtp = 'smtp.163.com'
+
+# 将统计结果发送到 pythoncamp@163.com，title: 19100301 jiahaolawyer
+# yagmail.SMTP(sender,psw,smtp).send(recipient,'19100301 jiahaolawyer',statString)    
