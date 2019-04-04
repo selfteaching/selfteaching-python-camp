1fa04ef36583b54ff8b22309d90fc774daa8b5e1
lunbixiaozi <47010339+lunbixiaozi@users.noreply.github.com>
2019-04-04 12:28:27 -0400

    Update d12_training1.py

--- 19100303/lunbixiaozi/d12_training1.py
+++ 19100303/lunbixiaozi/d12_training1.py
@@ -6,33 +6,39 @@ import getpass
 import mymodule.stats_word
 
 
-def main():
-    bot = Bot() #扫描二维码登录微信
-    my_friend = bot.friends() #回复对象为所有好友
+#def main():
+    # bot = Bot() #扫描二维码登录微信
+    # my_friend = bot.friends() #回复对象为所有好友
 
-    @bot.register(None,None) #接受分享类消息
-    def auto_reply(msg):
-        # response = requests.get(msg.url) #msg.url为分享的网址
-        # document = PyQuery(response.text)
-        # content = document('#js_content').text()
-        #处理文本
-        result = 'lala' #stats_word.stats_text_cn(content,100)
-        return result #将结果返回给好友
+    # @bot.register(None,None) #接受分享类消息
+    # def auto_reply(msg):
+    #     # response = requests.get(msg.url) #msg.url为分享的网址
+    #     # document = PyQuery(response.text)
+    #     # content = document('#js_content').text()
+    #     #处理文本
+    #     result = 'lala' #stats_word.stats_text_cn(content,100)
+    #     return result #将结果返回给好友
 
-    embed() #堵塞线程，保持监听状态
+    # embed() #堵塞线程，保持监听状态
 
-if __name__ == '__main__':
-    main() 
-# bot = Bot()
-# myself = bot.self
-# bot.file_helper.send('Hello from wxpy!')
-# bot.join()
-# @bot.register(None,None, False, True, True )
-# def print_messages(msg):
-#     print(msg)
+# if __name__ == '__main__':
+#     main() 
 
+bot = Bot()
+myself = bot.self
+bot.file_helper.send('Hello from wxpy!')
 
-# embed()
+@bot.register()
+def print_messages(msg):
+    if msg.type == SHARING:
+        r=requests.get(msg.url,auth=('user','pass')) 
+        document=PyQuery(r.text)
+        content=document('#js_content').text()
+        counter_cn_dict = mymodule.stats_word.stats_text_cn(content, 100)
+        counter_cn_string = str(counter_cn_dict)
+        msg.reply(counter_cn_string)
+
+embed()
 # Inscription = '''
 # Best regards
 # Zheng Zhang
