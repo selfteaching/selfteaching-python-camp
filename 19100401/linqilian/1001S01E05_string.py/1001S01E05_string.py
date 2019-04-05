f73e851ce160d17b3dfe2df2aac6803e75ce8250
linqilian <49056960+linqilian@users.noreply.github.com>
2019-04-05 13:29:13 +0800

    Update 1001S01E05_string.py

--- 19100401/linqilian/1001S01E05_string.py/1001S01E05_string.py
+++ 19100401/linqilian/1001S01E05_string.py/1001S01E05_string.py
@@ -22,18 +22,31 @@ If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let't do more of those!
 '''
 
-replacedText = text.replace("better", "worse")
-#print(replacedText)
+print('****************************************\n第一步：better替换成worse:\n****************************************') 
+ text=text.replace('better','worse') 
+ print(text) 
+ 
+ 
+ 
+ 
+ print('****************************************\n第二步：将字符串样本text里英文单词中包含ea的英文单词剔除:\n****************************************') 
+ new_list_1 = text.split() 
+ str1="ea" 
+ str2=[] 
+ for i in new_list_1: 
+     if i.find(str1) < 0: 
+         str2.append(i) 
+ print(str2) 
+ 
+ 
+ print('****************************************\n第三步：大小写字母转换:\n****************************************') 
+ text=text.swapcase() 
+ print(text) 
+ 
+ 
+ print('****************************************\n第四步：将所有单词按a，...z升序排列:\n****************************************') 
+ new_list_2=text.split() 
+ new_list_2.sort() 
+ print(new_list_2) 
 
-splitReplacedText = replacedText.split()
-removedEaText = []
-for i in splitReplacedText:
-     letters=range(ord('A'),ord('Z')):
-     if ord(i[0]) not in range(ord('A'),ord('Z')):
-          print("Not a word:%s"%(i))
-          
-     if ord(i[lent(i)-1]) not in range(ord('A'),ord('Z')):
-          print("Has dangle:%s"%(i))
-          i.strip(i[len(i)-1])
-          print("no dangle：%s"%(i))
           
