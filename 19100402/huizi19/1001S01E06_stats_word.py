77c4b0c0806b1af2e08a9886620fdbb7b722c6cc
huizi2019 <913784067@qq.com>
2019-04-05 23:22:50 +0800

    Update 1001S01E06_stats_word.py

--- 19100402/huizi19/1001S01E06_stats_word.py
+++ 19100402/huizi19/1001S01E06_stats_word.py
@@ -13,18 +13,15 @@ def  stats_text_en(text):
   
 
 
-#中文词频统计函数
-import jieba
+#中文频数统计函数
 
 def  stats_text_cn(text):
     p =  re.compile(r'[\u4e00-\u9fa5]')  # 中文的编码范围是：\u4e00到\u9fa5
     res = re.findall(p, text)   #取中文
-    result = ''.join(res)       #中文拼接
-    cn=list(jieba.cut(result))   #结巴分词
     wordcount={}                 #词频统计字典初始化
-    for i in cn:             
-        wordcount[i]=cn.count(i)     #词频统计词典设置
+    for i in res:             
+        wordcount[i]=res.count(i)     #词频统计词典设置
     wordcount=sorted(wordcount.items(),key=lambda x:x[1],reverse=True)    #词频统计，按词频降序排列
-    print(wordcount)  #输出中文分词后的词频统计结果
+    print(wordcount)  #输出每个中文汉字频数统计结果
     
 
