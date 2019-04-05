bf6e44af93022304a3cd4750e6243d9d67cb8edf
Congboqiu <32408593+congboqiu@users.noreply.github.com>
2019-04-05 23:36:11 +0800

    Update 1001S01E06_stats_word.py

--- 19100401/congboqiu/1001S01E06_stats_word.py
+++ 19100401/congboqiu/1001S01E06_stats_word.py
@@ -1,3 +1,5 @@
+'输入任何一段英文文档，得到降序排列的词频数组'
+':'
 def stats_text_en(text):
     """统计参数中每个英文单词出现的次数，最后返还一个按词频降序排列的数组"""
     textlist1 = text.split()#形成单词列表
@@ -29,9 +31,16 @@ def stats_text_cn(a):
     for i in a:
         if u'\u4e00' <= i <= u'\u9fff': #判断一个unicode是否是汉字
             dict1[i] = a.count(i)
-    dict1 = sorted(dict1.items(),key=lambda item:item[1],reverse=True)  #创建数组，并降序排列
+        
+    dict1 = sorted(dict1.items(),key=lambda item:item[1],reverse=True)  #创建以键值对为元素的元组
     dict1 = dict(dict1)#转化为字典
     return dict1                      
+<<<<<<< HEAD
     print(dict1)               
 a='morining我的个to省啊mingh妈the妈啊，你usa的良心komi大大的花了啊,很ah好,我爱ao you are my borther你time，你是我的nkkal爱人啊'
+=======
+    print(dict1)            
+    
+a = input('morining我的个to省啊mingh妈the妈啊，你usa的良心komi大大的花了啊,很ah好，我爱ao you are my borther你time，你是我的nkkal爱人啊')
+>>>>>>> parent of 87738a28... 111
 stats_text_cn(a)
