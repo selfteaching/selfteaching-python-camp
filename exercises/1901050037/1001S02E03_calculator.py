df48d41df8c7730046d96a3969161890fc474945
hx-137461197 <hanxue137461197@163.com>
2019-04-06 19:59:28 +0800

    Update 1001S02E03_calculator.py
    修改完成

--- exercises/1901050037/1001S02E03_calculator.py
+++ exercises/1901050037/1001S02E03_calculator.py
@@ -1,20 +1,21 @@
-def add(x,y):
-    result=x+y
-    return result
-def minus(x,y):
-    result=x-y
-    return result
-def multiply(x,y):
-    result=x*y
-    return result
-def divide(x,y):
-    if y==0 :
-        print("error")
-    else:
-        result=x/y
-        return result
 
-add(1,2)  
-minus(2,3)
-multiply(3,5)
-divide(2,2)
+print("1.加法","2.减法","3.乘法","4.除法",sep="***",end='\n')
+
+choice = input("(1/*/2/*/3/*/4)请输入对应功能序号:")
+n1 = eval(input("请输入第一个数字:"))
+n2 = eval(input("请输入第二个数字:"))
+
+if choice == '1':
+    print(n1,"+",n2,"=",n1+n2)
+
+elif choice == '2':
+    print(n1,"-",n2,"=",n1-n2)
+
+elif choice == '3':
+    print(n1,"*",n2,"=",n1*n2)
+
+elif choice == '4':
+    print(n1,"/",n2,"=",n1/n2)
+else:
+    print("输入错误，请重试")
+pass
