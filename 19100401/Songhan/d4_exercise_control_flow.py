e454951ad83669ada984e7a3ad84e55d441fb8fc
Song han <1595840783@qq.com>
2019-04-06 12:08:29 +0800

    Update d4_exercise_control_flow.py

--- 19100401/Songhan/d4_exercise_control_flow.py
+++ 19100401/Songhan/d4_exercise_control_flow.py
@@ -1,6 +1,6 @@
 #使用for...in循环打印九九乘法表
-for i in range(1:10):
-    for j in range(1:i+1):
+for i in range(1,10):
+    for j in range(1,i+1):
         print(i,'*',j,'=',i*j,end='\t')
             #print默认是打印一行，然后默认换行，'\t'代表制表符，end=说明打印后跟空格，即下一个打印接着后面输出
         if i==j:
