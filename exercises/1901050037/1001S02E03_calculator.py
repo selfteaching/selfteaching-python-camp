be04812da30aebe8366f06a40caae9f4c3c199a5
hx-137461197 <hanxue137461197@163.com>
2019-04-06 15:44:14 +0800

    Create 1001S02E03_calculator.py
    编写一个支持加、减、乘、除的计算器，支持输入、输出

---
+++ exercises/1901050037/1001S02E03_calculator.py
@@ -0,0 +1,20 @@
+def add(x,y):
+    result=x+y
+    return result
+def minus(x,y):
+    result=x-y
+    return result
+def multiply(x,y):
+    result=x*y
+    return result
+def divide(x,y):
+    if y==0 :
+        print("error")
+    else:
+        result=x/y
+        return result
+
+add(1,2)  
+minus(2,3)
+multiply(3,5)
+divide(2,2)
