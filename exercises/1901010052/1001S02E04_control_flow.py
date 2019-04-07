3008b4b9077f164ef2ae0e2ef897a04e8810cc91
luckylisa379 <luckylisa379@163.com>
2019-04-07 21:36:14 +0800

    Update 1001S02E04_control_flow.py

--- exercises/1901010052/1001S02E04_control_flow.py
+++ exercises/1901010052/1001S02E04_control_flow.py
@@ -1,7 +1,7 @@
 # 九九乘法表
 for i in range(1,10):
     for j in range(1,i+1):
-        print('{}x{}={}\t'.format(j, i, i*j), end='') # 格式
+        print('{}x{}={}\t'.format(j, i, i*j), end='') 
     print()
 
 
@@ -10,7 +10,7 @@ i = 1
 while i <= 9:
     j = 1
     while j <= i and i % 2!=0:
-        print('{}*{}={}'.format(i,j,i*j),end = " ")  # \t往右边移动一个制表符位
+        print('{}*{}={}'.format(i,j,i*j),end = " ")  
         j += 1
     print()
     i += 1
