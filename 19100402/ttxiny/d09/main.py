061acb80c40095e5a0d3e4a48d43df42707f0769
ttxiny <fanspooh@163.com>
2019-04-08 22:31:06 +0800

    d9 update

--- 19100402/ttxiny/d09/main.py
+++ 19100402/ttxiny/d09/main.py
@@ -2,7 +2,7 @@ from mymodule import stats_word
 import json
 with open('tang300.json','r',encoding='UTF-8') as f:
     text = f.read()
-f.closed
+f.close
 try:
     stats_word.stats_text_cn(text,100)
 except ValueError:
