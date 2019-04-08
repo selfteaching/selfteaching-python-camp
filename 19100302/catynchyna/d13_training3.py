4d377af1592074e5fae08213fe7882171a7304d1
caiying <i@caiying.me>
2019-04-08 16:06:57 +0800

    update cat homework

--- 19100302/catynchyna/d13_training3.py
+++ 19100302/catynchyna/d13_training3.py
@@ -1,49 +1,19 @@
-# day13 20190405-- copied from @wangrui0802 @wengyadong @ss412231878
-#question: if my d12 file is not in the same level of files used today, will there be bugged?
-#my plan A is to input all the codes included in d12
-
-#from mymodule import stats_word as sw
-#import requests
-#from pyquery import PyQuery
-##from wxpy import * error notification resolved by issue #1169
-#from wxpy import Bot,Message,embed
-
-
-#def main():
-#    bot = Bot(cache_path=True)#扫描二维码登陆微信 #issue1195 avoiding scanning again shortly
-#    my_friend = bot.friends()#回复对象为所有好友
-#    @bot.register(my_friend)
-#    @bot.register(msg_types='Sharing')#监听好友分享的消息 why 'Sharing'expression? SHARING got errors why oh why? input distinguish problems again?
-#    def auto_reply(msg):
-#        response = requests.get(msg.url)#msg.url为分享的网址
-#        document = PyQuery(response.text)
-#        content = document('#js_content').text()
-#        #像d11一样处理文本
-#        result = sw.stats_text_cn(content,count=100)
-#        wechat_word = ''.join(str(i)for i in result) # seems better str this type in case causing trouble in reading
-#        return wechat_word#将结果返回给好友
-
-#    embed()#d堵塞线程，保持监听
-
-#if __name__=='__main__':
-#    main()
-##I am not so sure but feel this should not be the same as day 12, thus I wanna try some revision below day13's codes:
-
-# day13 20190405-- copied from @wangrui0802(101) @wengyadong(102) @ss412231878(205) @slona-song(105)
-# with Cat's thoughts and queries listed for further exploration later. try as hard as you could for now...
+#!/usr/bin/python
+# -*- coding: UTF-8 -*-
+import matplotlib as mpl
+mpl.use('TkAgg')
 import matplotlib.pyplot as plt
 import matplotlib.font_manager as fm
 import numpy as np
-# np.random.seed(19680801)
-#### 105 @slona-song found through issue#1284 and her codes revealed her thoughts clearly and need to be explored !!! a golden mine 
 
 plt.rcdefaults()
 fig,ax = plt.subplots()
 # 设置中文字体  # issue#1692 provided another solution on font setting problems pyplotz: pip install
-cnfont = fm.FontProperties(fname='/Users/cat/Library/Fonts/SimHei.ttf')
+cnfont = fm.FontProperties(fname='/System/Library/Fonts/PingFang.ttc')
 
 # 定义绘制图标函数并保存为图片
 def chartImg(data={}) :
+    print('process img start')
     group_x = list(data.values())
     group_y = list(data.keys())
     # 自定义表格标签及标题样式
@@ -55,6 +25,7 @@ def chartImg(data={}) :
     ax.barh(group_y,group_x)
     # 保存绘制文件
     plt.savefig('chart.png')
+    print('process img done')
     return 'chart.png'
 
 
@@ -62,29 +33,28 @@ from mymodule import stats_word as sw
 import requests
 from pyquery import PyQuery
 #from wxpy import * error notification resolved by issue #1169
-from wxpy import Bot,Message,embed
+from wxpy import *
 
+bot = Bot(cache_path=True)
 
-def main():
-    bot = Bot(cache_path=True)#扫描二维码登陆微信 #issue1195 avoiding scanning again shortly
-    my_friend = bot.friends()#回复对象为所有好友
-    @bot.register(my_friend)
-    @bot.register(msg_types='Sharing')#监听好友分享的消息 why 'Sharing'expression? SHARING got errors why oh why? input distinguish problems again?
-    
-    def reply_my_friend(msg):
-         
-        wechat = requests.get(msg.url)
-        document = PyQuery(wechat.text)
+@bot.register()
+def auto_reply(msg):
+    if msg.type == SHARING:
+        response = requests.get(msg.url)  # msg.url为分享的网址
+        document = PyQuery(response.text)
         content = document('#js_content').text()
-#        result = sw.stats_text_cn(content,count=100)
-#        wechat_word = ''.join(str(i)for i in result) # seems better str this type in case causing trouble in reading
-#        return
-        list_a = sw.stats_text_cn(content,100)#接收发来的分享链接的文字，并处理成词频统计结果
-           
-        msg.reply_image(chartImg(list_a)) #回复刚才保存的图片给好友        
-        return
+        # 像d11一样处理文本
+        result = dict(sw.stats_text_cn(content, count=10))
+        print('process text done')
+        msg.reply_image(chartImg(result)) #回复刚才保存的图片给好友      
 
-    embed()#让程序执行到这里后返回，以便于重新执行
+embed()
+# bot.join()
 
-if __name__=='__main__':#这条语句能够帮助windows正常系统执行上面部分代码
-    main()
+# url  = 'https://mp.weixin.qq.com/s/pLmuGoc4bZrMNl7MSoWgiA'
+# response = requests.get(url)  # msg.url为分享的网址
+# document = PyQuery(response.text)
+# content = document('#js_content').text()
+# result = sw.stats_text_cn(content, count=10)
+
+# chartImg(dict(result))
--- 19100302/catynchyna/d5_exercise_string.py
+++ 19100302/catynchyna/d5_exercise_string.py
@@ -24,7 +24,7 @@ text = '''
   '''
 
 # 2-assign a new variable "changeWords" value using replace to change some words in "text"
-changeWords =text.replace('better','worse')
+changeWords = text.replace('better', 'worse')
 # print(changeWords) #checking upon my output value
 
 # 3-goal:delete those words including "ea"
@@ -33,9 +33,9 @@ changeWords =text.replace('better','worse')
 # question1: can this be proceed by string method? or have to be changed into list?
 # explored on campmates' homeworks got confused on why split and why the sequences(flow of control)
 # 3.1 question2: the assignment tasks including the procedures, are those sequences indicated the hint/
-# guidance on flow of control? I deemed the answer yes and take my movements. 
-# AS CLOSELY STICKED TO MY QUESTION1 AS POSSIBLE: 
-# I found one thinking process quite enlightened (19100101 echojce) I copied this works and 
+# guidance on flow of control? I deemed the answer yes and take my movements.
+# AS CLOSELY STICKED TO MY QUESTION1 AS POSSIBLE:
+# I found one thinking process quite enlightened (19100101 echojce) I copied this works and
 # tried some of my experiments. APPERICIATED A LOOOO0T @echojce.
 
 # let's assign a variable "text" the value as follows
@@ -63,10 +63,10 @@ Namespaces are one honking great idea -- let's do more of those!
 '''
 
 # 1.1 let's replace better by worse
-text1 = text.replace('better','worse')
+text1 = text.replace('better', 'worse')
 
 # let's convert it to a list
-list1 = text1.split( )
+list1 = text1.split()
 
 # using print(list1) to check whether this worked -- checked
 
@@ -75,10 +75,10 @@ list1 = text1.split( )
 str1 = 'ea'
 list2 = []
 for i in list1:
-    if i.find(str1) == -1 : #if not find ea in i, put this i into list2 
+    if i.find(str1) == -1:  # if not find ea in i, put this i into list2
         list2.append(i)
     else:
-        continue # not break because we should keep looking for qualified i to put into list2
+        continue  # not break because we should keep looking for qualified i to put into list2
 # use: print(' '.join(list2)) to check whether # this showed a text? str?
 
 # 1.3 let's try to use string method --swapcase
@@ -94,14 +94,15 @@ text2 = str2.swapcase()
 # 1.4 let's try to delete those - . * , ! and sort
 # we have to convert to a list?[to be thought later]
 
-list3 = text2.split() # let's try on list using split method
-i = 0 # this one is used to eliminate the first " ".but how?[to be thought later]
-for i in range(0,len(list3)):
-    list3[i]=list3[i].strip('*-,.!') # list[i] used to index the position.
-    if list3[i]==' ': 
+list3 = text2.split()  # let's try on list using split method
+# this one is used to eliminate the first " ".but how?[to be thought later]
+i = 0
+for i in range(0, len(list3)):
+    list3[i] = list3[i].strip('*-,.!')  # list[i] used to index the position.
+    if list3[i] == ' ':
         list3[i].remove(' ')
     else:
-        i=i+1 # still not so clarified. oh my poor math mindset more practice more!
+        i = i + 1  # still not so clarified. oh my poor math mindset more practice more!
 list4 = sorted(list3)
 
 print(' '.join(list4))
