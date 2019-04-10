5b0ebf6ebe9696b516386ed57b39dc2b589c0d56
cjh20040088 <41349874+cjh20040088@users.noreply.github.com>
2019-04-10 15:50:10 +0800

    19100402 day11

--- 19100402/cjh20040088/d11/mymodule/stats_word.py
+++ 19100402/cjh20040088/d11/mymodule/stats_word.py
@@ -14,7 +14,7 @@ def stats_text_cn(txt,count):
     if type(txt)==str:
         txt=re.sub('[^\u4e00-\u9fa5]','',txt)
         txt=txt.strip()
-        txt=[x for x in jieba.cut_for_search(txt) if len(x)>=2]
+        txt=[x for x in jieba.cut_for_search(txt)]
         print(collections.Counter(txt).most_common(count))
     else:
         raise ValueError
