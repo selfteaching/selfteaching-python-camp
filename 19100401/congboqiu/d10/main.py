01b1a6dfda5a6f11a4994cc5e3456c39e1a06fc9
Congboqiu <32408593+congboqiu@users.noreply.github.com>
2019-04-10 19:13:46 +0800

    day11

--- 19100401/congboqiu/d10/main.py
+++ 19100401/congboqiu/d10/main.py
@@ -1,24 +1,11 @@
 import re
-import sys
 import jieba
-import getpass
-import yagmail
 from mymodule import stats_word
-#import json
-#with open('tang300.json','r',encoding='UTF-8') as f:
-   # text=f.read()
-
-#print(stats_word.stats_text_cn(text, 20))
-import requests
-from pyquery import PyQuery
-from mymodule import stats_word
+import json
 
 
-response = requests.get("https://mp.weixin.qq.com/s/pLmuGoc4bZrMNl7MSoWgiA")
-document = PyQuery(response.text)
-content = document('#js_content').text()
-print(stats_word.stats_text(content,100))
+with open('tang300.json','r',encoding='UTF-8') as f:
+    text=f.read()
 
-sender = input('输入发件人邮箱:')
-password = getpass.getpass('输入发件人邮箱密码:')
-recipients = input('输入收件人邮箱:')
+
+print(stats_word.stats_text_cn(text, 20))
