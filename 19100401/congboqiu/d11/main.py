59d5a73f1eb2b7e83450427013e5b598507b5e61
Congboqiu <32408593+congboqiu@users.noreply.github.com>
2019-04-11 18:27:25 +0800

    day11

--- 19100401/congboqiu/d11/main.py
+++ 19100401/congboqiu/d11/main.py
@@ -8,7 +8,7 @@ from mymodule import stats_word
 #with open('tang300.json','r',encoding='UTF-8') as f:
    # text=f.read()
 
-#print(stats_word.stats_text_cn(text, 20))
+#print(stats_word.stats_text_cn(text,20))
 import requests
 from pyquery import PyQuery
 from mymodule import stats_word
@@ -17,8 +17,6 @@ from mymodule import stats_word
 response = requests.get("https://mp.weixin.qq.com/s/pLmuGoc4bZrMNl7MSoWgiA")
 document = PyQuery(response.text)
 content = document('#js_content').text()
+return stats_word.stats_text_cn(content,100)
 print(stats_word.stats_text(content,100))
 
-sender = input('输入发件人邮箱:')
-password = getpass.getpass('输入发件人邮箱密码:')
-recipients = input('输入收件人邮箱:')
