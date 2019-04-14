370f4b5086848fa6e69fcffd5b359d7b81842ebc
zxinyu16 <48579552+zxinyu16@users.noreply.github.com>
2019-04-15 00:29:27 +0800

    Update 1001S02E04_control_flow.py

--- exercises/1901010055/1001S02E04_control_flow.py
+++ exercises/1901010055/1001S02E04_control_flow.py
@@ -5,16 +5,20 @@ def mul_table():
         for n in range(1,10):
             if n<=i:
                 print(str(i),'*',n,'=',i*n,end='\t')
-        print(' ')
+        print()
 mul_table()
 def odd_table():
-    for i in range(1,10):
-        for n in range(1,10):
-            if n<=i:
-               while n*i%2!=0:
-                     print(str(i),'*',n,'=',i*n,end='\t')
-                     break
-        print(' ') 
+    i=1
+    while i<10:
+        j=1
+        if(i%2==1):
+            while j<=i:
+                print('{}*{}={}'.format(i,j,i*j),end='\t')
+                j+=1
+            print()
+            i+=1
+        else:
+            i+=1 
 odd_table()
 
 
