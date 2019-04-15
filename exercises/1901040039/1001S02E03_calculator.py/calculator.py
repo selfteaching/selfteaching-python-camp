88f3cd58f78ff3378c1d076e4a186e58acee9a9a
hanwuji1 <763654937@qq.com>
2019-04-15 22:48:49 +0800

    Update calculator.py

--- exercises/1901040039/1001S02E03_calculator.py/calculator.py
+++ exercises/1901040039/1001S02E03_calculator.py/calculator.py
@@ -6,22 +6,22 @@ caculation = input ("Please enter operator: ")
 
 if caculation == "+":
 
-    print(number_1+number_2)
+        print(number_1+number_2)
 
 elif caculation =="-":
-     
-     print(number_1-number_2)
+                    
+        print(number_1-number_2)
 
 elif caculation =="*":
 
-    print(number_1*number_2)
+        print(number_1*number_2)
 
 elif caculation =="/":
 
         print(number_1 / number_2)
-
+        
 else:
 
-            print("wrong type")
+        print("wrong type")
 
         
