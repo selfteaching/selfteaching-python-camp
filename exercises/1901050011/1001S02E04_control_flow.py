72f37901b0b2def7c4d51cf1a2a21a47a1647edc
wdy1888 <763590877@qq.com>
2019-04-18 22:36:19 +0800

    Update 1001S02E04_control_flow.py

--- exercises/1901050011/1001S02E04_control_flow.py
+++ exercises/1901050011/1001S02E04_control_flow.py
@@ -4,7 +4,7 @@ Select python environment
 +  #九九乘法表
 +  for i in range（1，10）：
 +      for  j  in range（1，i+1）：
-+             print（j，‘*’，i，‘=’，i*j，‘\t‘，end='')
++             print（j，‘*’，i，‘=’，i*j，'\t',end='')
 +       print()
 +  print()
 +
@@ -12,10 +12,10 @@ Select python environment
 +
 +  # 九九乘法表，但除去偶数行
 +  x=1 
-+  white  x<10：
++   white  x<10：
 +     if  x%2==1:
 +           for  y in range（1，x+1）：
-+               print（y，‘*’，x，‘=’，x*y，‘\t‘，end=’‘）
++               print（y，‘*’，x，‘=’，x*y，'\t',end='')
 +           print（）
 +           x=x+1
 +  print（）
