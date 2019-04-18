0a32af98a20aaccd8e583f99456994c01bbb6635
wdy1888 <763590877@qq.com>
2019-04-18 22:28:47 +0800

    Create 1001S02E04_control_flow.py

---
+++ exercises/1901050011/1001S02E04_control_flow.py
@@ -0,0 +1,22 @@
+Select python environment
++  # 控制语句练习
+
++  #九九乘法表
++  for i in range（1，10）：
++      for  j  in range（1，i+1）：
++             print（j，‘*’，i，‘=’，i*j，‘\t‘，end='')
++       print()
++  print()
++
++
++
++  # 九九乘法表，但除去偶数行
++  x=1 
++  white  x<10：
++     if  x%2==1:
++           for  y in range（1，x+1）：
++               print（y，‘*’，x，‘=’，x*y，‘\t‘，end=’‘）
++           print（）
++           x=x+1
++  print（）
++
