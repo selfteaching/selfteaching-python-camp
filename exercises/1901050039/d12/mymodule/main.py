5f92a3bfd23a4ade9d6e454de6cf96c1409e3b94
abbywakaha <abbywakaha@gmail.com>
2019-04-24 18:34:25 +0800

    enter

--- exercises/1901050039/d12/mymodule/main.py
+++ exercises/1901050039/d12/mymodule/main.py
@@ -1,11 +1,7 @@
 from wxpy import *
 import requests
 from pyquery import PyQuery
-import numpy as np
-import matplotlib.pyplot as plt
 import stats_word
-plt.rcParams['font.family']=['SimHei']
-
 bot = Bot()
 
 @bot.register()   #回复信息
@@ -14,16 +10,7 @@ def auto_reply(msg):
         r = requests.get(msg.url)
         document = PyQuery(r.text)
         content = document('#js_content').text()
-        result = dict(stats_word.stats_text_cn(content))
-        x = tuple(result.keys())
-        y = tuple(result.values())
-        plt.bar(x,y,width=0.5)
-        plt.xlabel(u"高频词汇")
-        plt.ylabel(u"频次")
-        plt.title("高频词汇统计表")
-        plt.ylim(0,int(y[0]))
-        plt.savefig('my_result.png')
-        plt.show()
-    return ('my_result.png')
+        result = str(stats_word.stats_text_cn(content))
+    return result
 
 embed()
