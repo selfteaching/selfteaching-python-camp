f5eca259c87f3eac7c1e7f4c38f418dfabf9f828
josedweck <45022738+josedweck@users.noreply.github.com>
2019-04-25 21:59:38 +0800

    Update 1001S02E03_calculator.py
    calll

--- exercises/1901050068/1001S02E03_calculator.py
+++ exercises/1901050068/1001S02E03_calculator.py
@@ -2,9 +2,9 @@ def add(x,y):
     return x+y
 def subtract(x,y):
     return x-y
-def multiply(x,y)
+def multiply(x,y):
     return x*y
-def divide(x,y)
+def divide(x,y):
     return x/y
 
 print("PLEASE CHOOSE THE CALCULATION METHOD:")
@@ -21,11 +21,14 @@ number2 = int(input("PLEASE ENTER THE SECOND NUMBER: "))
 if choise == '1':
     print(number1, "+", number2, "=", add(number1,number2))
 elif choise == '2':
-    print(number1, "-", number2, "=", subtract(number1，number2))
+    print(number1, "-", number2, "=", subtract(number1,number2))
 elif choise =='3':
     print(number1, "*", number2, "=", multiply(number1,number2))
 elif choise =='4':
     print(number1, "/", number2, "=", divide(number1,number2))
 else:
     print("false") 
+    
+
+
 
