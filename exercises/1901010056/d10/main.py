584f649f12bdd26ee360a845866679d472a31bfe
pony6666 <48642730+pony6666@users.noreply.github.com>
2019-04-28 12:49:49 +0800

    Update main.py

--- exercises/1901010056/d10/main.py
+++ exercises/1901010056/d10/main.py
@@ -1,8 +1,8 @@
-from mymodule import stats_word            #它是输入stats_word
+from mymodule import stats_word10            #它是输入stats_word
 import re
 import json  
 import jieba
-path=r'D:\selfteaching-python-camp\exercises\1901010056\d10\tang300.json'
+path=r'D:\selfteaching-python-camp\selfteaching-python-camp\exercises\1901010056\d10\tang30010.json'
 with open(path,'r',encoding='UTF-8') as f: #不加'r', encoding='UTF-8'会报UnicodeDecodeError
                                                       #然后是打开，tang300.josn
     read_date = f.read()                              #把这个josn读出来，然后在关闭
