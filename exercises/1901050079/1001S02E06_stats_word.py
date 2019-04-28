e3505263e3068ed6428103f34ce67b9d010dfc08
maxtongxue <49889415+maxtongxue@users.noreply.github.com>
2019-04-28 10:00:47 +0800

    Day06

--- exercises/1901050079/1001S02E06_stats_word.py
+++ exercises/1901050079/1001S02E06_stats_word.py
@@ -1,4 +1,4 @@
-text = '''
+x = '''
 The Zen of Python, by Tim Peters
 Beautiful is better than ugly.
 Explicit is better than implicit.
@@ -20,4 +20,32 @@ If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 '''
-def stats_text_en(text)
+y='''
+君不见，黄河之水天上来，奔流到海不复回。
+君不见，高堂明镜悲白发，朝如青丝暮成雪。
+人生得意须尽欢，莫使金樽空对月。
+天生我材必有用，千金散尽还复来。
+烹羊宰牛且为乐，会须一饮三百杯。
+岑夫子，丹丘生，将进酒，杯莫停。
+与君歌一曲，请君为我倾耳听。
+钟鼓馔玉不足贵，但愿长醉不复醒。
+古来圣贤皆寂寞，惟有饮者留其名。
+陈王昔时宴平乐，斗酒十千恣欢谑。
+主人何为言少钱，径须沽取对君酌。
+五花马，千金裘，呼儿将出换美酒，与尔同销万古愁。
+'''
+def stats_text_en(text):                       #以降序方式统计英文词频
+    a=text.replace('.','').replace(',','').replace('--','').replace('*','').replace('!','')
+    b=a.split()
+    from collections import Counter
+    c=Counter(b)
+    return c
+print(stats_text_en(x))
+
+def stats_text_cn(text):                       #以降序方式统计中文词频
+    d=text.replace('，','').replace('。','').replace('\n','')
+    e=list(d)
+    from collections import Counter
+    f=Counter(e)
+    return f
+print(stats_text_cn(y))
