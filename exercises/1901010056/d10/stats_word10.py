6ea025b24d613d29c4c49315a6f775b787478c3c
pony6666 <48642730+pony6666@users.noreply.github.com>
2019-04-28 09:16:28 +0800

    Update stats_word10.py

--- exercises/1901010056/d10/stats_word10.py
+++ exercises/1901010056/d10/stats_word10.py
@@ -19,9 +19,9 @@ def  stats_text_cn(text,count):
     for i in text: #筛选长度大于等于2的词
         if len(i)>=2:
             text1.append(i)
-    print(collections.Counter(text1).most_common(count))
+    return collections.Counter(text1).most_common(count)
 
-def stats_word(text,count): #定义函数，实现统计汉字和英文单词出现次数
+def stats_word10(text,count): #定义函数，实现统计汉字和英文单词出现次数
     if type(text)!=str: 
         raise ValueError("文本为非字符串") 
     stats_text_en(text,count) 
