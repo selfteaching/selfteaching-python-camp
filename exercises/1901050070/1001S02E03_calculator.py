c3b067f0497adf00331851a699d7f39d41b131e9
suweti <suwt1266@qq.com>
2019-05-01 14:15:29 +0800

    Update 1001S02E03_calculator.py

--- exercises/1901050070/1001S02E03_calculator.py
+++ exercises/1901050070/1001S02E03_calculator.py
@@ -1,12 +1,47 @@
-import os 
-while True: 
-    dynamic = input('输入计算表达式：') 
-if dynamic != 'cls': 
-    try: 
-        result = eval(dynamic) 
-        print('计算结果：'+str(result)) 
-    except: 
-        print('计算表达式输入有误！') 
-else: 
-    command = 'cls' 
-os.system(command)
+# 定义函数
+def add(x,y):
+    """相加"""
+
+    return x + y
+
+def subtract(x,y):
+    """相减"""
+
+    return x - y
+
+def multiply(x,y):
+    """相乘"""
+
+    return x * y
+
+def divide(x,y):
+    """相除"""
+
+    return x/y
+
+# 用户输入
+print("选择运算")
+print("1.相加")
+print("2.相减")
+print("3.相乘")
+print("4.相除")
+
+choice = input("输入你的选择（1/2/3/4):")
+
+num1 = int(input("输入第一个数字："))
+num2 = int(input("输入第二个数字："))
+
+if choice == '1':
+    print(num1,"+",num2,"=",add(num1,num2))
+
+elif choice == '2':
+    print(num1,"-",num2,"=",subtract(num1,num2))
+
+elif choice == '3':
+    print(num1,"*",num2,"=",multiply(num1,num2))
+
+elif choice == '4':
+    print(num1,"/",num2,"=",divide(num1,num2))
+
+else:
+    print("非法输入")
