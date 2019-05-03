79171921f9a8231c660ac1983f1d45cb79738a88
Xiaolai_007 <525387153@qq.com>
2019-05-03 21:05:00 +0800

    在Python里for循环可以更简单一点

--- exercises/1901050070/1001S02E04_control_flow.py
+++ exercises/1901050070/1001S02E04_control_flow.py
@@ -1,10 +1,10 @@
-i = 1
+
 for i in range(1,10):
-    n = 1
+   
     for n in range(1,i+1):
         print(i,"*",n,"=",eval('n*i'),'',end="")
-        n += 1
-    i += 1
+        
+    
     print()
 
 
