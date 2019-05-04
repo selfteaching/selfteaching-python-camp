cc913bc9246cb5b61aec8aa68535c72ac8f27ce8
MGTeller <340362013@qq.com>
2019-05-04 10:10:18 +0800

    day12 homework

---
+++ exercises/1901050024/d12/main.py
@@ -0,0 +1,20 @@
+import requests                 #获取网址
+from mymodule import stats_word #统计单词
+from wxpy import *
+
+bot = Bot()
+
+@bot.register(Friend,SHARING)
+def auto_get_url(msg):
+    response = requests.get(msg.url)
+    from pyquery import PyQuery as py
+    document = py(response.text) 
+    content = document('#js_content').text()
+    text1 = stats_word.stats_word(content,100)
+    text2 = str(text1)
+    bot.file_helper.send(text1)
+    bot.file_helper.send(text2)
+    
+# 堵塞线程，并进入 Python 命令行
+embed()
+
