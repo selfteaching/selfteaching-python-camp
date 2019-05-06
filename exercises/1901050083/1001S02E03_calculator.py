f09a59e4bf5c0054f6d7a68d661a198dcf8a252f
sunanchn <sunanchn@126.com>
2019-05-06 18:03:38 +0800

    day06 真的要玩坏啦.....

--- exercises/1901050083/1001S02E03_calculator.py
+++ exercises/1901050083/1001S02E03_calculator.py
@@ -1,3 +1,18 @@
-a = input("请输入：")
-b = eval(a)
-print("计算结果为：" , a,"=",b)
+print("请依次输入数字a，运算符'+'or'-'or'*'or'/',数字b")
+a=int(input())
+x=input()
+b=int(input())
+if x =="+":
+    c=a+b
+    print(a,"+",b,"=",c)
+elif x =="-":
+    c=a-b
+    print(a,"-",b,"=",c)
+elif x =="*":
+    c=a*b
+    print(a,"*",b,"=",c)
+elif x =="/":
+    c=a/b
+    print(a,"/",b,"=",c)
+else:
+    print("运算符号错误，请检查")
