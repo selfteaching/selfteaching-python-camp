6afebe68b6bbd6ed74802015fd2e29bb589e10cf
musicmiao <1204431131@qq.com>
2019-05-08 19:50:24 +0800

    自学训练营学习1群day9

--- exercises/1901010061/d09/main.py
+++ exercises/1901010061/d09/main.py
@@ -3,6 +3,6 @@ import json
 with open(r'C:\Users\CS-Mu\Desktop\vsc\d09\tang300.json',encoding='UTF-8') as f: 
     read_date = f.read()
 try:
-    print('统计字数最多的前100个字： \n',stats_word.stats_text_cn(read_date,100))
+    print('统计汉字字频最高的前100个字： \n',stats_word.stats_text_cn(read_date,100))
 except ValueError as e:
     print(e) 
