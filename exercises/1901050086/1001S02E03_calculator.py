36c306827d101fb28211d114107f878c06059474
wangzuo <49434691+wangzuo613@users.noreply.github.com>
2019-05-08 13:42:15 +0800

    自学训练营学习7群 DAY3
    Co-Authored-By: srvz <2470962+srvz@users.noreply.github.com>

---
+++ exercises/1901050086/1001S02E03_calculator.py
@@ -0,0 +1,19 @@
+#加减乘除计算器
+# 思路：分三步输入 计算 输出 
+#输入 input()函数
+#计算 + - * /,提供可选择项
+#输出 print()函数
+x = input('x:')
+y = input('y:')
+z = input('请选择+ or - or * or /:')
+if z == '+':
+    print(float(x)+float(y))
+elif z == '-':
+    print(float(x)-float(y))
+elif z == '*':
+    print(float(x)*float(y))
+elif z == '/':
+    print(float(x)/float(y))
+elif z == '+' or '-' or '*' or '/':
+    print('Error')
+#以上代码均为copy,来源：某学员（19100302）
