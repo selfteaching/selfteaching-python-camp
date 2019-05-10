edbefd35f3a3281dc1db612ff090b2c1b2f22653
Galaxy1227 <galaxy1227@163.com>
2019-05-10 16:37:02 +0800

    Update 1001S02E06_stats_word.py

--- exercises/1901050119/1001S02E06_stats_word.py
+++ exercises/1901050119/1001S02E06_stats_word.py
@@ -1,5 +1,5 @@
 
-
+# 统计字符串中每个英文单词出现的次数，返回一个按词频降序排列的数组
 def stats_text_en (text):
     # 剔除字符串中的无意义字符
     s1 = text.replace('*', '', text.count('*'))
@@ -24,8 +24,34 @@ def stats_text_en (text):
     L1 = sorted(aDict.items(), key=lambda x: x[1], reverse=True)
     return(L1)
 
+# 统计字符串中每个中文汉字出现的次数，返回一个按字频降序排列的数组 
+def stats_text_cn(text):
+    # 剔除字符串中的非中文字符
+    s1 = text.replace('，', '', text.count('，'))
+    s2 = s1.replace('“', '', text.count('“'))
+    s3 = s2.replace('”', '', text.count('”'))
+    s4 = s3.replace('——', '', text.count('——'))
+    s5 = s4.replace('。', '', text.count('。'))
+    s6 = s5.replace('……', '', text.count('……'))
+    s7 = s6.replace(' ', '', text.count(' '))
+    s8 = s7.replace('\n', '', text.count('\n'))
+    text1 = s8.replace('？', '', text.count('？'))
 
-text = '''
+    # 将字符串整理为以每个汉字为key，以该汉字个数为value的字典
+    i = 0
+    bDict = {}
+    
+    while(i < len(text1)):
+        cnt = text1.count(text1[i])
+        bDict.setdefault(text1[i], cnt) 
+        i = i + 1
+    
+    # 将字典中的中文汉字按个数从大到小排序
+    L2 = sorted(bDict.items(), key=lambda x: x[1], reverse=True)
+    return(L2)
+
+
+text_en = '''
 The Zen of Python, by Tim Peters
 Beautiful is better than ugly.
 Explicit is better than implicit.
@@ -49,4 +75,15 @@ Namespaces are one honking great idea -- let's do more of those!
 '''
 
 # 打印出排序后的单词数组
-print(stats_text_en(text))
+print(stats_text_en(text_en))
+
+text_cn = '''
+有些概念，在绝大多数人脑子里干脆就是混沌的，比如“价值” —— 什么是价值？
+如何衡量价值？“价值”这个概念，被我私下里称为“哈姆雷特概念”，
+因为莎士比亚说，“一千个人眼里有一千个哈姆雷特”……一旦争论中出现这种概念上的冲突，
+那么基本上争论演变成争吵，甚至只剩下吵，就是永恒的狗血结局。
+'''
+
+# 打印出排序后的汉字数组
+print()
+print(stats_text_cn(text_cn))
