d7248c4035348eb2e24814d7a21d2a1cf8ca4bc0
hyb07410133 <hyb07410133@sina.com>
2019-05-19 06:45:57 +0800

    Update d3_exercise_calculator.py

--- exercises/1901020008/d3_exercise_calculator.py
+++ exercises/1901020008/d3_exercise_calculator.py
@@ -1,2 +1,12 @@
-b=45/9+15
-print(b)
+def add(x,y):
+    print("x is {} and y is {}".format(x,y))
+    return x + y
+def sub(x,y):
+    print("x is {}and y is{}".format(x,y))
+    return x-y
+def multi(x,y):
+    print("x is {} and y is {}".format(x,y))
+    return x*y
+def div(x,y):
+    print("x is {} and y is {}".format(x,y))
+    return x / y
