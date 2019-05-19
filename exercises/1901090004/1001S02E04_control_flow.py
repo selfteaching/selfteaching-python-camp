75f7bfb58e3649f233374fa7d6aa4e40951d42f7
Evolvingwufan <49020116+Evolvingwufan@users.noreply.github.com>
2019-05-19 23:53:10 +0800

    Create 1001S02E04_control_flow.py

---
+++ exercises/1901090004/1001S02E04_control_flow.py
@@ -0,0 +1,8 @@
+for row in range (1,10):
+    for col in range (1,10):
+        if row >= col:
+            print(format(row*col,
+        else:
+            print（format("",""):
+    print()
+            
