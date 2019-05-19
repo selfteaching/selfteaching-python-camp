21db5536a6f6efe68cea667265a5bee720fa60bd
Jungle002 <824966768@qq.com>
2019-05-19 12:56:55 +0800

    Update 1001S02E04_control_flow.py

--- exercises/1901090008/1001S02E04_control_flow.py
+++ exercises/1901090008/1001S02E04_control_flow.py
@@ -1,16 +1,20 @@
-print("九九乘法表")
-for i in range(1,10):
-    for j in range(1,i+1):
-        print(j,'x',i,'=',i*j,end='\t')
-        if i == j:
-            print()
+for i in range(1,10): # 九九乘法表
+    for j in range(1,10):
+        print(1,"*",j,"=",i*j,end='\t')
+        if j == i:
+            print("")
+            break
 
 
-print("\n去除偶数行")
-for a in range(1,10):
-    for b in range(1,a+1):
-        while a % 2 !=0:
-            print(b,'x',a,'=',a*b,end='\t')
-            if a == b:
-                print()
+a,b = 0,0 # 去除偶数行
+while (a < 9):
+    a +=1
+    if a % 2 == 0:
+        a += 1
+    while (b < 9):
+        b += 1
+        print(a,"*",b,"=",a*b,end='\t')
+        if a == b:
+            b = 0
+            print('')
             break
