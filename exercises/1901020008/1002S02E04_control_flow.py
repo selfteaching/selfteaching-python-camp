180dc39daeeb3e2aa7e3fd1945b4471594d9c46c
hyb07410133 <hyb07410133@sina.com>
2019-05-19 16:44:14 +0800

    Update 1002S02E04_control_flow.py

--- exercises/1901020008/1002S02E04_control_flow.py
+++ exercises/1901020008/1002S02E04_control_flow.py
@@ -1,19 +1,5 @@
-f = 1
-while f <= 9:
-     h = 1
-     while h < f + 1:
-          if f%2==0:
-               break
-
-          print(f, "*", h, '=', f*h, sep='', end='\t')
-
-          h += 1
-     print()
-     f += 1
-
-
 for i in range(1,10):
-    # print(i,end = " ")
+  
     for j in range(1,i+1):
         print('%s*%s=%s' %(i,j,i*j),end = ' ')
     print()
