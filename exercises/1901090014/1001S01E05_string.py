79c7b202a3578b1a93fb8568749dc067779e7c99
Xu <45456059+Xujalg@users.noreply.github.com>
2019-05-21 09:03:06 +0800

    Update 1001S01E05_string.py

--- exercises/1901090014/1001S01E05_string.py
+++ exercises/1901090014/1001S01E05_string.py
@@ -21,17 +21,36 @@ If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 '''
 #  better替换 worse
-print(text.replace('better','worse'))
+#print(text.replace('better','worse'))
+
 #  去除ea
-print(text.replace('ea',''))
+
+symbol = ",.!-*"
+for str in symbol:
+    text= text.replace(str,'')
+print(text)
+
+#split切片
+text2=text.split()
+text3=[]#新建一个空的列表
+for i in text2:
+    if i.find('ea')<0:
+        text3.append(i)
+print(text3)
  # 小写字母转换成大写字母
-print(text.upper()) 
-# 大写字母转换成小写字母    
-print(text.lower())  
+b = []
+for n in text:
+   if "a"<= n <= "z":
+      b.append(n.upper())
+   elif"A" <= n <= "Z" :
+      b.append(n.lower())
+   else:
+      b.append(n)
+#print("".join(b))
 #升序排列
 s=list(text)
 s.sort()
-print (s)
+#print (s)
 
 
 
