6f0103d90fbe57ffbdce403c874d261edcd3de4f
Youngjin <YoungJin333@users.noreply.github.com>
2019-05-21 20:03:28 +0800

    删除settings.json

--- 19100203/Young4/test001.py
+++ 19100203/Young4/test001.py
@@ -1,6 +1,10 @@
+'''
+代码试验区，以下为试验成功代码：
+d7 模块的使用
+
 import re
 def stats_text_en(text):  #定义函数
-    text_1 = re.sub(r'[\W\u4e00-\u9fa5]',' ',text) #不是字符的，都转换为空格。主要去除标点符号
+    text_1 = re.sub(r'[\W\u4e00-\u9fa5]',' ',text) #去除中文、非字符，将其转换为空格。
     text_1 = text_1.lower() #转换为小写
     textlist_1 = text_1.split()#将段落转换为列表
     dict1 = {} 
@@ -15,6 +19,9 @@ def stats_text_en(text):  #定义函数
 
 text = '''
 abc,frd,hugggv,我是一串字母，想把我分开？
+
 '''
 
 print(stats_text_en(text))
+
+'''
