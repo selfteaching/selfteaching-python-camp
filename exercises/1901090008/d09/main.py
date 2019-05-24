e8e2a2856b55d9d8e9ba0f41ee3e379d0f598f7f
Jungle002 <824966768@qq.com>
2019-05-24 17:14:28 +0800

    Update main.py

--- exercises/1901090008/d09/main.py
+++ exercises/1901090008/d09/main.py
@@ -6,7 +6,7 @@ print(__file__, __name__)
 fp =path.join(path.dirname(path.abspath(__file__)), 'tang300.json')  #找到唐诗三百首的路径
 with open(fp, 'rb') as f:                                            #读取本地文件，以以 binary（二进制） mode 打开文件
     read_data =f.read()
-    f.closed
+    
     b = read_data.decode('utf8')                                     #将utf8解码为Unicode
     c3 =s.stats_text_cn(b,100) 
     print(c3.most_common(100))
