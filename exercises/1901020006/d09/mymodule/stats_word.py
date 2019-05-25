f7fc861c77638a7b6784d9461473a36e0331e25d
jun <137541297@qq.com>
2019-05-25 20:40:07 +0800

    d9 作业补交
    有点蒙,再摸索

--- exercises/1901020006/d09/mymodule/stats_word.py
+++ exercises/1901020006/d09/mymodule/stats_word.py
@@ -1,12 +1,8 @@
-from collections import Counter #提供了哈希对象的计算功能
+from collections import Counter 
+#提供了哈希对象的计算功能
 
 # 封装统计英文单词词频的函数
-def stats_text_en(text):
-    if type(text)!=str:
-        raise ValueError("文本为非字符串")
-
-
-
+def stats_text_en(text,count):
     elements = text.split()
     words = []
     symbols = ',.*-!'
@@ -16,57 +12,24 @@ def stats_text_en(text):
         # 用 str 类型的 isascii 方法判断是否是英文单词
         if len(element) and element.isascii:
             words.append(element)
-    counter = {}
-    word_set = set(words)
-    
-    for word in word_set:
-        counter[word] = words.count(word)
-    return sorted(counter.items(),key=lambda x:x[1],reverse=True)        
+    return Counter(words).most_common(count)       
+
 
 # 统计参数中每个中文汉字出现的次数
-def stats_text_cn(text):
-    if type(text)!=str:
-        raise ValueError("文本为非字符串")
+def stats_text_cn(text,count):
     cn_characters = []
     for character in text:
+        #unicode 中 中文 字符的范围
         if'u4e00' <= character <= '\u9fff':
             cn_characters.append(character)
-    counter = {}
-    cn_character_set = set(cn_characters)
-    for character in cn_character_set:
-        counter[character] = character.count(character)
-    return sorted(counter.items(),key=lambda x:x[1],reverse=True)
-
-en_text = '''
-The Zen of Python, by Tim Peters
-by Tim Peters,by Tim ,by 
-'''
-
-cn_text = '''
-
-优美胜于丑陋,
-胜于丑陋,胜于丑,
-胜于,胜
-人
-好
-大
-人
-'''
-    
-    #搜索 __name__ ==__main__
-    # 一般情况下在文件内 测试 代码的事后以下面的形式进行
-if __name__ =='__main__':
-    en_result = stats_text_en(en_text)
-    cn_result = stats_text_cn(cn_text)
-    print('统计参数中每个英文单词出现的次数 ==>\n', en_result)
-    print('统计参数中每个中文单词出现的次数 ==>\n', cn_result)
+    return Counter(cn_characters).most_common(count)   
 
 
-def stats_text(text):
+def stats_text(text,count):
     if type(text)!=str:
-        raise ValueError("文本为非字符串")
+        raise ValueError('参数必须是 str 类型,输入类型 %s' % type(text))
     '''
     合并 英文词频 和 中文词频 的结果
     '''
-    return stats_text_en(text) + stats_text_cn(text)
+    return stats_text_en(text,count) + stats_text_cn(text,count)
 
