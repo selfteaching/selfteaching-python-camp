23eca9f265f62ff93c95580cf7dcb53b47734367
minidv963 <3636655@qq.com>
2019-05-26 19:27:03 +0800

    DAY11

--- exercises/1901090003/d11/main.py
+++ exercises/1901090003/d11/main.py
@@ -1,12 +1,12 @@
 from mymodule import stats_word
 
-file = open('/Users/Justco/selfteaching-python-camp/exercises/1901090003/d09/tang300.json','rb')
-text1 = file.read()
-text = text1.decode()
+# file = open('/Users/Justco/selfteaching-python-camp/exercises/1901090003/d09/tang300.json','rb')
+# text1 = file.read()
+# text = text1.decode()
 
-print(stats_word.stats_text_cn(text,20))
+# print(stats_word.stats_text_cn(text,20))
 
-file.close()
+# file.close()
 
 import requests
 response = requests.get('https://mp.weixin.qq.com/s/pLmuGoc4bZrMNl7MSoWgiA')
@@ -16,7 +16,14 @@ from pyquery import PyQuery
 document = PyQuery(response.text)
 content = document('#js_content').text()
 print(stats_word.stats_text_cn(content,100))
+# 输入你的账号密码，注意密码是授权码
+# 然后你打开你的qq邮箱
+# 发送成功了，刚才你没有设置服务器smtp.qq.com
+# 现在好了，可以关闭了
 
 import yagmail
-yagmail.SMTP('3636655@qq.com').send('pythoncamp@163.com', '【1901090003】自学训练营 DAY11 minidv963', 'stats_word.stats_text_cn(content,100)')
+user = input('账号')
+password = input('mi ma')
+smp = 'smtp.qq.com'
+yagmail.SMTP(user=user,password=password,host=smp).send('pythoncamp@163.com', '【1901090003】自学训练营 DAY11 minidv963', str(stats_word.stats_text_cn(content,100)))
 
