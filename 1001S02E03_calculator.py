3c7a44aa2a233c6afa9b08f4379ca0cc8533ae4b
junpingwang <21466368+junpingwang@users.noreply.github.com>
2019-05-27 10:52:52 +0800

    Update 1001S02E03_calculator.py
    sorry, it change,please merge

--- 1001S02E03_calculator.py
+++ 1001S02E03_calculator.py
@@ -1,4 +1,4 @@
-ef add(x,y):
+def add(x,y):
     return x + y
 def subtract(x,y):
     return x - y
@@ -6,21 +6,21 @@ def multiply(x,y):
     return x * y
 def divide(x,y):
     return x / y
-
-    print('输入数字开始进行计算')
-    a=int(input(''))
-    print('请选择：=，-，*，/')
-     c=input('')
-    print('请继续输入数字')
-    b=int(input(''))
-    if c=='+':
-       print(add(a,b))
-    elif c=='-':
-       print(subtract(a,b))
-    elif c=='*':
-       print(multiply(a,b))
-    elif c=='/':
-       print(divide(a,b))
-    else:
-       print('输入错误')
+print('输入数字开始进行计算')
+a=int(input(''))
+print('请选择：+，-，*，/')
+c=input('')
+print('请继续输入数字')
+b=int(input(''))
+if c=='+':
+print(add(a,b))
+elif c=='-':
+print(subtract(a,b))
+elif c=='*':
+print(multiply(a,b))
+elif c=='/':
+print(divide(a,b))
+else:
+print('输入错误')
        
+
