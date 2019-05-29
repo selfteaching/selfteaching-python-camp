9cd5ff297790795e400b6d8fcb0d34499dafbffb
hyb07410133 <hyb07410133@sina.com>
2019-05-29 22:25:32 +0800

    Update main.py

--- exercises/1901020008/d09/main.py
+++ exercises/1901020008/d09/main.py
@@ -5,26 +5,26 @@ import re
 import logging
 
 logging.basicConfig(
-format='file:%(filename)s|line:%(lineno)d|message: %(message)s', level=logging.DEBUG)
+    format='file:%(filename)s|line:%(lineno)d|message: %(message)s', level=logging.DEBUG)
 
 
 def load_file():
-    file_path = path.join(path.dirname(path.abspath(__file__)),'../tang300.json')
-    print(__file__, file_path)
+    file_path = path.join(path.dirname(path.abspath(__file__)),'./tang300.json')
+    print('当前文件路径:', __file__,'\n读取文件路径:', file_path)
     with open(file_path,'r', encoding='utf-8') as f:
         return f.read()
 
 def merge_poems(data):
     poems = ''
     for item in data:
-        poems += item.get('comtents', '')
+        poems += item.get('contents', '')
     return poems
 def main():
     try:
        data = load_file()
        logging.info(data[0])
        poems = merge_poems(json.loads(data))
-       logging.info('result ==> %s', stats_word.stats_text_cn(poems,100))
+       logging.info('result ==> %s', stats_word.stats_text_cn(poems, 100))
     except Exception as e:
         logging.exception(e)
 
