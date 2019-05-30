7897529a0681fb6de60f752bd3242dd387c9aea8
cerquake <651090186@qq.com>
2019-05-30 16:33:58 +0800

    homeworkDay9

--- exercises/1901050043/d10/mymodule/stats_word.py
+++ exercises/1901050043/d10/mymodule/stats_word.py
@@ -1,5 +1,6 @@
 import re
 from collections import Counter
+import jieba
 
 def stats_text_en(text,count):
     '''This function aims to count English words.'''
@@ -15,9 +16,14 @@ def stats_text_cn(text,count):
     ''' This function aims to count Chinese words.'''
     
     if type(text)==str:
-        result1 = re.findall(u'[\u4e00-\u9fff]', text)
-        newString = result1
-        return (Counter(newString).most_common(count),'/n')
+        result1 = re.findall(u'[\u4e00-\u9fff]+', text)
+        newString = ' '.join(result1)
+        b=jieba.cut(newString)
+        newString1=[]
+        for i in b:
+            if len(i)>=2:
+                newString1.append(i)
+        return (Counter(newString1).most_common(count),'/n')
     else:
          raise ValueError(type(text))
     
