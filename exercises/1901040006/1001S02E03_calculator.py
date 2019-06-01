0661a2b4a2c33606a142d16d16a27a257bf6f9c6
PowerZCY <zcy777et@gmail.com>
2019-06-01 13:07:34 +0800

    Update 1001S02E03_calculator.py
    去掉第二行int后的空格

--- exercises/1901040006/1001S02E03_calculator.py
+++ exercises/1901040006/1001S02E03_calculator.py
@@ -1,5 +1,5 @@
 def calculator():
-    num_1 = int (input('请输入要计算的第一个数字：'))
+    num_1 = int(input('请输入要计算的第一个数字：'))
     symbol = input('请输入一个运算符，+ - * / ：')
     num_2 = int(input('请输入要计算的第二个数字：'))
     if symbol =='+':
