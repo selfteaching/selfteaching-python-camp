9cfb530cbb19943cabe01b403db8b8605727e3c9
xiaoyi <xiaoyiam@gmail.com>
2019-06-02 12:44:05 +0800

    记录了第三天的作业心得

---
+++ exercises/1901010130/1001S02E03_calculator.py
@@ -0,0 +1,26 @@
+def add_numbers(a,b):
+    a=input("Type the first number: ")
+    b=input("Type the second number: ")
+    c=a+b
+    print("The result is ",c )
+
+def sub_numbers(a,b):
+    a=input("Type the first number: ")
+    b=input("Type the second number: ")
+    c=a-b
+    print("The result is ",c )
+
+
+def mul_numbers(a,b):
+    a=input("Type the first number: ")
+    b=input("Type the second number: ")
+    c=a*b
+    print("The result is ",c )
+
+def div_numbers(a,b):
+    a=input("Type the first number: ")
+    b=input("Type the second number: ")
+    c=a/b
+    print("The result is ",c )
+
+x=input("请输入你要进行的运算：")
