f8471b5042fd09a3dccc2afcbd8e2705cdbfea1c
PowerZCY <zcy777et@gmail.com>
2019-06-05 16:27:33 +0800

    Update 1001S02E03_calculator.py
    解决无法合并问题

--- exercises/1901040006/1001S02E03_calculator.py
+++ exercises/1901040006/1001S02E03_calculator.py
@@ -1,5 +1,5 @@
 def calculator():
-    num_1 = int(input('请输入要计算的第一个数字：'))
+    num_1 = int (input('请输入要计算的第一个数字：'))
     symbol = input('请输入一个运算符，+ - * / ：')
     num_2 = int(input('请输入要计算的第二个数字：'))
     if symbol =='+':
@@ -12,6 +12,5 @@ def calculator():
         result = num_1 / num_2
     print(result)
 
-calculator()
 
 
