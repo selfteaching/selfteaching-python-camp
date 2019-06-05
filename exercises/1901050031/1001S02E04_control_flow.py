705810f796cef70711545c7e6074831338c2d7d3
zhoujinyunzhouzhou <sevenzjy@gmail.com>
2019-06-05 23:24:39 +0800

    Update 1001S02E04_control_flow.py

--- exercises/1901050031/1001S02E04_control_flow.py
+++ exercises/1901050031/1001S02E04_control_flow.py
@@ -1,11 +1,11 @@
-#左下三角格式输出九九乘法表
+print#左下三角格式输出九九乘法表
 for i in range(1,10):
-   for j in range(1,i+1):
+    for j in range(1,i+1):
        print("%d*%d=%2d" % (i,j,i*j),end=" ")
-   print (" ")
-   print(str )
-   i=1
-   while i<10:
+    print (" ")
+    print(str )
+    i=1
+    while i<10:
        if i%2==0:
           print()
        else:
