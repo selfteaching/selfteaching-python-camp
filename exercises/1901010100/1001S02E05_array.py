c16b5c9ab54f4a5b2ac3911680267ff6e5aec2d8
songwei333 <48575949+songwei333@users.noreply.github.com>
2019-06-06 15:38:54 +0800

    day5

---
+++ exercises/1901010100/1001S02E05_array.py
@@ -0,0 +1,22 @@
+array=[0,1,2,3,4,5,6,7,8,9]
+array.reverse()
+array1=[str(i)for i in array]
+array2 = ''.join(array1)
+array3 = array2[2:9]
+array4 = []
+for i in array3:
+    array4.append(str(i))
+array4.reverse()
+array5 = ''.join(array4)
+array6 =[]
+for i in array5:
+    array6.append(int(i))
+print(array6)
+array7 = []
+array8 = []
+array9 = []
+for i in array6:
+    array7.append(bin(i))
+    array8.append(oct(i))
+    array9.append(hex(i))
+print(array7,array8,array9)
