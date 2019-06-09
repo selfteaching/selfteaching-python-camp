ffa3bd1c879c4c32701c499a70b70d707b835ec5
kidswonder <hecheng@bigseeart.cn>
2019-06-09 20:33:49 +0800

    Update 1001S02E04_control_flow.py

--- exercises/1901050022/1001S02E04_control_flow.py
+++ exercises/1901050022/1001S02E04_control_flow.py
@@ -1,28 +1,24 @@
 # Filename : 1001S02E04_control_flow.py
 # author by : kidswonder
 
-print('''
-DAY4 九九乘法表
-''')
-print("使用for...in循环打印乘法表")
+#九九乘数表
 for i in range(1,10):
-    for a in range(1,i+1):
-        print(i,"x",a,"=",i*a," ",end="")
-    print("")
+    for j in range(1,i+1):
+        print('{}*{}={}\t'. format(j, i, i*j), end='')
+    print()
 
-
-
-
-
-    print("九九乘法奇数表")
- i=1
-while i<10:
-    if i%2==0:
-        i=i+1
-        continue
-    a=1
-    while a<i+1:
-         print(i,"*",a,"=",i*a,"",end="")
-         a=a+1
-    print("",end="\n")
-    i=i+1
+#九九乘数表去除偶数行
+i = 0
+while i < 9:
+    j = 1
+    i += 1
+    while j <= i:
+        if i % 2 != 0:
+            if j < i:
+                print(j, '*', i,'=',i*j, end='\t')
+                j += 1
+            else:
+                print(j, '*', i,'=',i*j, end='\n')
+                j += 1
+        else:            
+            j += 1
