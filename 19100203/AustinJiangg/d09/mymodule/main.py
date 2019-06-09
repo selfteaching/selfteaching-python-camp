5e942923848b816e9379dd4dc0f960bfa765f371
Austin Jiang <jianglulu001@gmail.com>
2019-06-09 22:21:05 +0800

    some changes

--- 19100203/AustinJiangg/d09/mymodule/main.py
+++ 19100203/AustinJiangg/d09/mymodule/main.py
@@ -1,5 +1,5 @@
-from stats_word import stats_text_cn
-'''stats_text_cn(1234)
+'''from stats_word import stats_text_cn
+stats_text_cn(1234)
 
 while True:
     try:
@@ -9,6 +9,15 @@ while True:
     except ValueError:
         print("Oops! Argument text must be <class 'str'>  Try again...")
 '''
-
-f = open('tang300.json', 'r')
-stats_text_cn(f)
+from collections import Counter
+file1= open('tang300.txt', 'r')
+text = file1.readlines()
+file1.close()
+dict1 = {}
+for i in text:
+    if i in ['」','「','!','-','’',' ','\n',',','，','.','，','。','”','“','※','…','？','：','！','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']:
+        continue
+    if i not in dict1:        #为首次出现的字创建key
+        dict1[i] = 0
+    dict1[i] += 1
+print(Counter(dict1).most_common(100))
