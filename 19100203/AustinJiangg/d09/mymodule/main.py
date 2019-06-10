de55fce2b6d8ba3497d5704d66067d9913b464a5
Austin Jiang <jianglulu001@gmail.com>
2019-06-11 00:04:33 +0800

    Day 9

--- 19100203/AustinJiangg/d09/mymodule/main.py
+++ 19100203/AustinJiangg/d09/mymodule/main.py
@@ -8,7 +8,7 @@ while True:
         break
     except ValueError:
         print("Oops! Argument text must be <class 'str'>  Try again...")
-'''
+
 from collections import Counter
 file1= open('tang300.txt', 'r')
 text = file1.readlines()
@@ -21,3 +21,32 @@ for i in text:
         dict1[i] = 0
     dict1[i] += 1
 print(Counter(dict1).most_common(100))
+'''
+
+from os import path
+from collections import Counter
+import json
+
+
+file_path = path.join(path.dirname(path.abspath(__file__)), './tang300.json')
+with open(file_path, 'r', encoding='utf-8') as f:
+    load_dict = json.load(f)
+    list1 = []
+    for i in load_dict:
+        a = list(i.items())
+        for j in a:
+            for h in j:
+                if type(h) is int:
+                    continue
+                else:
+                    list1.append(h)
+   
+    a_str = ''.join(list1)
+    dict1 = {}
+    for k in a_str:
+        if k in ['\n','」','「','!','-','’',' ','/n',',','，','.','，','。','”','“','※','…','？','：','！','a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']:
+            continue
+        if k not in dict1:        
+            dict1[k] = 0
+        dict1[k] += 1
+    print(Counter(dict1).most_common(100))
