19c5cf263ff8a85987f4effb858295ddb200f289
zhaodikang <572208112@qq.com>
2019-06-12 22:57:06 +0800

    Update 1001S02E05_stats_test.py

--- exercises/1901050069/1001S02E05_stats_test.py
+++ exercises/1901050069/1001S02E05_stats_test.py
@@ -22,3 +22,42 @@ If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 '''
+
+# 1. 使⽤字典（dict）统计字符串样本 text 中各个英⽂单词出现的次数
+
+#先将字符串根据空白字符分割成 list，要调用 str 类型
+elements = simple_text.split()
+
+# 定义一个新的 list 型变量，存储处理过的单词
+words = []
+
+# 挑出需要剔除的非单词符号
+symbols = ',-*_!'
+
+for element in elements:
+    for symbol in symbols:
+        element = element.replace(symbol,'')
+        if len(element):
+            words.append(element)
+print(words)
+
+counter = {}
+word_set = set(words)
+for word in word_set:
+    counter[word] = words.count(word)
+print(counter)
+
+# 按照出现次数从⼤到⼩输出所有的单词及出现的次数
+
+
+
+
+
+
+
+
+
+
+
+
+
