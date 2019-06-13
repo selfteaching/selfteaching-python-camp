33d99b578f9977b2f60b50163a9fddec0a31a4f9
Luojjone <962634479@qq.com>
2019-06-13 15:24:17 +0800

    11

---
+++ exercises/1901010145/1001S02E03_calculator.py
@@ -0,0 +1,13 @@
+print
+x=int(input("x:"))
+y=int(input("y:"))
+z=input("加减乘除")
+if z=="+":
+    print(x+y)
+elif z=="-":
+    print(x-y) 
+elif z=="*":
+    print(x*y)
+elif z=="/":
+    print(x/y)
+    print()
