1a71b84a7ec83907bd0611c68277f62790a326db
vinsonmail <vinsonmail@qq.com>
2019-06-16 08:45:51 +0800

    OK

--- 19100302/aosjiabei/d3_exercise_calculator.py
+++ 19100302/aosjiabei/d3_exercise_calculator.py
@@ -1,10 +1,10 @@
 #支持加减乘除的计算器，支持输入参数和输出结果
 print("hi,i am a caculator,provide two numbers ")
-i=int(input("please,give me the first number:"))
+i=float(input("please,give me the first number:"))
 operator=input("enter the type of calculation， + or - or * or / :")
-j=int(input("please,give me th second number："))
+j=float(input("please,give me th second number："))
 
-if operator=="+"or operator== "-"or operator=="*"or operator=="/":
+if operator=="+"or "-"or"*"or"/":
     if operator == "+":
         print(i+j)
     if operator == "-":
