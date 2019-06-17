1122195d3c5ed6756486948da8bfc688dac88c23
liqiang031143 <48619298+liqiang031143@users.noreply.github.com>
2019-06-17 11:52:02 +0800

    自学编程训练营1 李强 (#3888)
    * Create 100102E01_helloworld.txt.txt
    
    * Create README.md.txt
    
    * 修改文件名
    
    * Create 1001S02E02_hello_python.py
    
    * 提交第三天和第四天作业内容
    
    * 九九乘法口诀表
    
    * 第五天作业更新，开心
    
    * 第六天作业更新
    
    * day7更新
    
    day7
    
    * 第七天老师批改后的更新
    
    * 第七关再订正
    
    * 1
    
    * 第七日作业最后修订版
    
    * 第七天再修订，感觉自己训练还是太少，不怕麻烦，继续
    
    * 第八天作业
    
    * 第九天作业
    
    * 第十天

--- exercises/1901010114/day10/main.py
+++ exercises/1901010114/day10/main.py
@@ -7,8 +7,10 @@ file_path = os.path.join(dirname,'tang300.json')
 with open(file_path,'r',encoding = 'UTF-8') as a:
     text = a.read()
 
+ 
+ 
+ 
 print(stats_word.stats_text_cn(text,20))
-
 
  
  
