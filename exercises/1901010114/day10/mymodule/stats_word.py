2f64a414d914ba548d29a7e086f1774b6878efdf
liqiang031143 <48619298+liqiang031143@users.noreply.github.com>
2019-06-19 21:19:45 +0800

    day11

--- exercises/1901010114/day10/mymodule/stats_word.py
+++ exercises/1901010114/day10/mymodule/stats_word.py
@@ -3,10 +3,10 @@ from collections import Counter
 import jieba
 # 统计英文
 
-def stats_text_en(text,count):
-    if not isinstance(text,str):
+def stats_text_en(text,count):   #定义一个函数stats_en
+    if not isinstance(text,str):  #一个if语句用来判断text是否是字符串，否则的话生成一个异常
         raise ValueError('参数必须是str类型')
-    word_list = re.findall(r'\b[a-z]+\'?[a-z]+',text.lower())
+    word_list = re.findall(r'\b[a-z]+\'?[a-z]+',text.lower())  #代码是参考了别人的，请看一下如何把一片文档中的所有英文单词单独提炼出来，请google
     return Counter(word_list).most_common(count)
 
 # 统计中文
