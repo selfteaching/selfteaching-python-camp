7880429f0325b92d7a05ff094cde1035ca29f4da
starry18 <loudipengzhixiang@163.com>
2019-06-20 23:28:43 +0800

    Update 1001S02E03_calculator.py

--- exercises/1901090039/1001S02E03_calculator.py
+++ exercises/1901090039/1001S02E03_calculator.py
@@ -1,17 +1,14 @@
 print('calculation')
-
-#user input numbers & select operation
 x = int(input("x:"))
 y = int(input("y:"))
-z = int(input("please choose, + - */:"))
+z = int(input("please choose, +-*/:"))
+if z =="+":
+    print(x+y)
+elif z =="-":
+    print(x-y)
+elif z =="*":
+    print(x*y)
+else:
+    print(x/y)
+print("thank you")
 
-if z =="+"
-print(x+y)
-elif z =="-"
-print(x-y)
-elif z =="*"
-print(x*y)
-else z =="/"
-print(x/y)
-else
-print("wrong input, please kindly try it again!")
