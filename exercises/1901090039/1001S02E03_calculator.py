eb97f5319d89eaaf6ad2b4688b07cab46056b28e
starry18 <loudipengzhixiang@163.com>
2019-06-20 22:57:54 +0800

    Create 1001S02E03_calculator.py

---
+++ exercises/1901090039/1001S02E03_calculator.py
@@ -0,0 +1,17 @@
+print('calculation')
+
+#user input numbers & select operation
+x = int(input("x:"))
+y = int(input("y:"))
+z = int(input("please choose, + - */:"))
+
+if z =="+"
+print(x+y)
+elif z =="-"
+print(x-y)
+elif z =="*"
+print(x*y)
+else z =="/"
+print(x/y)
+else
+print("wrong input, please try it again!")
