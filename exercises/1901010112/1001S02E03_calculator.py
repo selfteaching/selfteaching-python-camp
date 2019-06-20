bb9240216fc306b24d53cea9daee7a3ea7e5765b
zhangchaoxian <backwindow.boy@gmail.com>
2019-06-20 21:21:02 +0900

    Update 1001S02E03_calculator.py

--- exercises/1901010112/1001S02E03_calculator.py
+++ exercises/1901010112/1001S02E03_calculator.py
@@ -1,14 +1,14 @@
-#wlecome to Calculator
-#1.接收用户输入的运算符号和数字
+##wlecome to Calculator
+##1.接收用户输入的运算符号和数字
 symbol = input ('请输入+，-，*，/中的运算符号')
 num1 = input('请输入第一个数字')
 num2 = input('请输入第二个数字')
 
-#2.把用户输入的数字（字符串）转换成整形
+##2.把用户输入的数字（字符串）转换成整形
 a = int(num1)
 b = int(num2)
 
-#3.计算
+##3.计算
 
 if symbol == '+':
     print (a,'+',b,'=', a+b)
