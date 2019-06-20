f470c4a60a79affe460749b4ac1a5f7f821ea754
jiaxiaobin <50037769+jiaxiaobin@users.noreply.github.com>
2019-06-20 22:46:35 +0800

    Update main.py

--- exercises/1901050092/d10/main.py
+++ exercises/1901050092/d10/main.py
@@ -1,31 +1,72 @@
-from mymodule import stats_word #从mymodule中导入stats_word
 
-from collections import Counter
+from mymodule import stats_word
 
-import re #调取re函数
+from os import path
 
-import os #调取os函数
+import json
 
-import jieba
+import re
 
-with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), 'tang300.json'), encoding = 'UTF-8') as f:
+import logging
 
-    #绝对路径调取唐诗三百首
 
-    read_date = f.read()
 
-    t1=re.findall(r'[\u4e00-\u9fa5]',read_date)#提取中文字符
+logging.basicConfig(
 
-    seg_list1 = jieba.cut(''.join(t1), cut_all=False)#将t1转换字符串再进行jieba
+    format='file:%(filename)s|line:%(lineno)d|message: %(message)s', level=logging.DEBUG)
 
-    cn1 = []#定义一个空列表
 
-    for i in seg_list1:#遍历老列表
 
-        if len(i)>=2:#将字符串长度大于等于2的字符筛选出来
+def load_file():
 
-            cn1.append(i)#生成新列表
 
-    mydict = Counter(cn1).most_common(20)#统计出现频次前100的字符
 
-print (mydict)
+    file_path = path.join(path.dirname(path.abspath(__file__)), 'tang300.json')
+
+    print('当前文件路径：', __file__, '\n读取文件路径：', file_path)
+
+
+
+    with open(file_path, 'r', encoding='utf-8') as f:
+
+        return f.read()
+
+
+
+
+
+def merge_poems(data):
+
+    poems = ''
+
+    for item in data:
+
+        poems += item.get('contents', '')
+
+    return poems
+
+
+
+def main():        #
+
+    try:
+
+        data = load_file()
+
+        logging.info(data[0])    #打印检查代码
+
+        poems = merge_poems(json.loads(data))  #转化josn格式
+
+        logging.info('reasult ==> %s', stats_word.stats_text_cn(poems, 100))
+
+    except Exception as e:
+
+        logging.exception(e)        #捕获异常
+
+
+
+
+
+if __name__ == "__main__":
+
+    main()
