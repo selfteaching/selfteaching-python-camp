0b4a5edd8243b842d6b5a2b2b9dafe7aea6ddba8
bubu8yuanmin <miumiu0912@163.com>
2019-06-23 00:38:41 +0800

    day3 version2
    added "+,-,/"computation

--- exercises/1901090036/1001S02E03_calculator.py
+++ exercises/1901090036/1001S02E03_calculator.py
@@ -1,5 +1,11 @@
 def multiplication(a,b):
     return a*b
+def add(a,b):
+    return a+b
+def substract(a,b):
+    return a-b
+def divide(a,b):
+    return a/b
 print("please input the first number:")
 a=int(input(''))
 print("please choose:+,-,*,/,")
@@ -8,5 +14,11 @@ print("please input the second number:")
 b=int(input(''))
 if c=="*":
     print(multiplication(a,b))
+elif c=="+":
+    print(add(a,b))
+elif c=="-":
+    print(substract(a,b))
+elif c=="/":
+    print(divide(a,b))
 else:
     print("wrong message")
