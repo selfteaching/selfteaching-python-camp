170d5912964586f93d08bee313d4037d1b242816
TongHua1688 <49399616+TongHua1688@users.noreply.github.com>
2019-06-24 10:13:16 +0800

    Create 1001S02E03_calculator.py

---
+++ exercises/1901090035/1001S02E03_calculator.py
@@ -0,0 +1,17 @@
+
+#程序是在issue区，按照别人完成的作业，照着上面写的。
+def add(x,y):
+    print("x is {} and y is {}".format(x,y))
+    return x + y
+
+def sub(x,y):
+    print("x is {} and y is {}".format(x,y))
+    return x - y
+
+def multi(x,y):
+    print("x is {} and y is {}".format(x,y))
+    return x * y
+
+def div(x,y):
+    print("x is {} and y is {}".format(x,y))
+    return x / y
