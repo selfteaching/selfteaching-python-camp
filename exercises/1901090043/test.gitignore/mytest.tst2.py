628de513fb18543b52b4389444d37b4bc1f63006
keqi-jj <184094775@qq.com>
2019-06-26 16:00:19 +0800

    day08修改

--- exercises/1901090043/test.gitignore/mytest.tst2.py
+++ exercises/1901090043/test.gitignore/mytest.tst2.py
@@ -1,17 +1,4 @@
-def input_password():
-    # 提示用户输入密码
-    pwd = input("请输入密码：")
-    # 判读密码长度大于等于8，返回用户输入的密码
-    if len(pwd) >= 8:
-        return pwd
-    # 如果小于8，则抛出异常
-    print("主动抛出异常")
-    # 创建异常对象
-    ex = Exception("密码长度不够")
-    # 抛出异常对象
-    raise ex
-# 提示用户输入密码
-try:
-    print(input_password())
-except Exception as result:
-    print(result)
+text = 'afdd'
+print(type(text))
+if type(text) == str:
+    print('true')
