6c511e638789c7d6c40427072926feba6cccdedd
TYcugb <31087216+TYcugb@users.noreply.github.com>
2019-06-27 16:25:56 +0800

    Update 1001S02E06_stats_word.py

--- exercises/1901090053/1001S02E06_stats_word.py
+++ exercises/1901090053/1001S02E06_stats_word.py
@@ -37,8 +37,9 @@ import re
 
 def stats_text_en(text):
     ''' count the number of every English word in a text '''
-    text = re.sub(r'[,.!--*]', ' ', text)
-    text_list = text.split()
+    text = text.lower()
+    import re
+    text_list = re.findall(r'\w+', text)
     text_set = set(text_list)
     d = {x:text_list.count(x) for x in text_set}
     d_list = [v for v in d.values()]
@@ -47,11 +48,12 @@ def stats_text_en(text):
 
 stats_text_en(en_text)
 
+
 def stats_text_cn(text):
     ''' count the number of every Chinese character in
     a text '''
-    text = re.sub(r'[、, 。\n]', '', text)
-    text_list = list(text)
+    import re
+    text_list = re.findall(r'([\u4e00-\u9fff])',text)
     text_set = set(text_list)
     d = {x:text_list.count(x) for x in text_set}
     d_list = [v for v in d.values()]
