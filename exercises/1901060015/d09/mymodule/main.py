8e43f0bc2cebba021df07ee2684c6954cf27ec5d
veryspecialdog <13141360658@163.com>
2019-06-28 22:50:57 +0800

    d9

--- exercises/1901060015/d09/mymodule/main.py
+++ exercises/1901060015/d09/mymodule/main.py
@@ -1,15 +1,11 @@
 import json
-
-
-
-with open('tang300.json') as f:
-    t=f
+with open('tang300.json',mode='r+',encoding='utf-8') as text:
+    text=text.read()
     
-
 import stats_word
 try:
     
-    print(stats_word.stats_text_cn(t))
+    print(stats_word.stats_text_cn(text))
     
 except:
     print("omg! please try again!!!")
