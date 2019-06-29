c96b4897b7fd54aeb41536c54a78d426771bb9ad
keqi-jj <184094775@qq.com>
2019-06-29 22:34:56 +0800

    day10 final

--- exercises/1901090043/d10/main.py
+++ exercises/1901090043/d10/main.py
@@ -9,7 +9,7 @@ file_object.close()
 sort_list = {}
 try:
     #对列表中中英文字符，进行统计降序排列
-    sort_list = stats_word.stats_text_cn(file_content,100)
+    sort_list = stats_word.stats_text_cn(file_content,20)
 
 except ValueError as result:
     print(result)
