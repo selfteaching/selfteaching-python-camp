0784cae1d69208bf790f56c2321e440d59173df9
TYcugb <31087216+TYcugb@users.noreply.github.com>
2019-06-30 17:44:00 +0800

    Update stats_word.py

--- exercises/1901090053/d09/mymodule/stats_word.py
+++ exercises/1901090053/d09/mymodule/stats_word.py
@@ -1,4 +1,5 @@
-
+import re
+import collections as col
 
 def stats_text_en(text, count):
     ''' count the number of every English word in a text '''
@@ -8,13 +9,11 @@ def stats_text_en(text, count):
         raise ValueError('request input data type of parameter count is int')
     
     text = text.lower()
-    import re
     filtrate = re.findall(r'[^\u4E00-\u9FA5]',text)
     en_text = str()
     for item in filtrate:
         en_text = en_text + item
     en_text = re.findall(r'\w+', en_text)    
-    import collections as col 
     d = col.Counter(en_text)
     d = d.most_common(count)
     d = dict(d)
@@ -30,9 +29,7 @@ def stats_text_cn(text, count):
     if type(count) is not int:
         raise ValueError('request input data type of parameter count is int')    
       
-    import re
     text_list = re.findall(r'([\u4e00-\u9fff])',text)
-    import collections as col 
     d = col.Counter(text_list)
     d = d.most_common(count)
     d = dict(d)
@@ -47,7 +44,6 @@ def stats_text(text, count):
     if type(count) is not int:
         raise ValueError('request input data type of parameter count is int')    
     
-    import collections as col
     t = {**stats_text_en(text, count), **stats_text_cn(text, count)}
     d = col.Counter(t)
     d = d.most_common(count)
