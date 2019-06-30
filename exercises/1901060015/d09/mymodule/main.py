b36e394ca3356ed51301ec02e191d79fe67570cc
veryspecialdog <13141360658@163.com>
2019-07-01 00:32:36 +0800

    day9

--- exercises/1901060015/d09/mymodule/main.py
+++ exercises/1901060015/d09/mymodule/main.py
@@ -1,7 +1,47 @@
+from mymodule import stats_word
+from os import path
 import json
+import re
+import logging
 
-with open('tang300.json','r',encoding='UTF-8') as f:
-    print(f.read())
+logging.basicConfig(
+    format='file:%(filename)s|line:%(lineno)d|message:%(message)s',level=logging.DEBUG)
+
+def load_file():
+    flie_path = path.join(path.dirname(path.abspath(__file__)),'tang300.json')
+    
+    with open(flie_path,'r',encoding='utf-8') as f:
+        return f.read()
+
+def merge_poems(data):
+    poems =''
+    for item in data:
+        poems +=item.get('contents','')
+    return poems
+
+def main():
+    try:
+    
+        data=load_file()
+        logging.info(data[0])
+        poems=merge_poems(json.loads(data))
+        logging.info('result ==> %S',stats_word.stats_text_cn(poems,100))
+    except Exception as e:
+        logging.exception(e)
+
+if __name__ == "__main__":
+    main()
+    
+
+
+
+
+
+    
+
+    
+    
+    
     
     
 
@@ -24,7 +64,7 @@ with open('tang300.json','r',encoding='UTF-8') as f:
 
 
     
-()
+
     
 
 
