cdeb0cea0161d70b9c68b61d4ba4fe6fe965c0e3
Mi-penguin <52156586+Mi-penguin@users.noreply.github.com>
2019-07-01 15:32:40 +0800

    Create 1001S02E03_calculator.py
    计算器

---
+++ exercises/1901090068/1001S02E03_calculator.py
@@ -0,0 +1,19 @@
+def jia(x, y):
+  return x + y
+  
+def jian(x, y):
+  return x - y
+
+def cheng(x, y):
+    return x * y
+
+def chu(x, y):
+    return x / y
+
+
+print(jia(1,2))
+
+
+
+以为很难…… 想了半天要怎么做
+后来发现简单的让人无语，总感觉自己做错了
