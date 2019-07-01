9e0bea17746740041db7731bd284afe4f2305ecd
keqi-jj <184094775@qq.com>
2019-07-01 13:23:22 +0800

    day11

--- exercises/1901090043/test.gitignore/mytest.tst2.py
+++ exercises/1901090043/test.gitignore/mytest.tst2.py
@@ -1,2 +1,13 @@
-str = '中'
-print(len(str))
+import getpass
+import yagmail
+#sender = input('woaxiangxiang@foxmail.com')
+#password = getpass.getpass('qqmimaasus17715') 
+#recipients = input('184094775@qq.com')
+
+yag = yagmail.SMTP(user = "184094775@qq.com", password = "wdbqzdfthegibhcb", host='smtp.qq.com')
+
+# 邮箱正文
+contents = ['send you picture！']
+
+# 发送邮件
+yag.send('pythoncamp@163.com', '打招呼！',contents)
