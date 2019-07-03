ae2c6be64b6ec9f7990dd63585c232139c7428e7
AmazingJulie <8071696@qq.com>
2019-07-03 11:35:59 +0800

    重新提交Day6作业

--- exercises/1901090023/1001S02E06_stats_word.py
+++ exercises/1901090023/1001S02E06_stats_word.py
@@ -5,7 +5,14 @@ you have found the community that has passion for the same goal;
 you have found a couple of open-minded and trustworthy friends.'''     
 
 # 定义函数
-def stats_text_en(text:str) -> list:                   # 定义函数，形式参数名为text，并且给函数名添加注释说明
+def stats_text_en(text):                   # 定义函数，形式参数名为text
+    """ 函数名：stats_text_en               # 文档字符串用来为函数添加注释说明
+
+    参数名：text
+    统计参数中英文单词词频
+    最后返回一个按词频降序排列的数组
+    """
+    
     text1 = text.replace(';', '').replace('.', '')     # 去掉字符串中的特殊符号
     list1 = text1.split()                              # 字符串转换为列表
    
@@ -18,9 +25,6 @@ def stats_text_en(text:str) -> list:                   # 定义函数，形式�
 # 执行函数
 print("英文单词及出现次数降序排列")
 print(stats_text_en(text))                              
-
-# 函数注释说明
-print("Annotations:", stats_text_en.__annotations__)
 print('_'*200)
 
 
@@ -32,21 +36,22 @@ text= '''
 找到了敞亮明朗靠谱的二三好友。
 '''
 
-def stats_text_cn(text:str) -> list:         # 定义函数，用来统计中文汉字字频，并且给函数名添加注释说明
-    text1 = text.replace('。', '')
-    text2 = text1.replace('、', '')
-    text3 = text2.replace('', ' ')           # 中文汉字字符间增加空格                      【非常重要】
+def stats_text_cn(text:str) -> dict:         # 定义函数，用来统计中文汉字字频，并且给函数名添加注释说明
+    """ 函数名：stats_text_cn                 # 文档字符串用来为函数添加注释说明
 
-    list1 = text3.split()                    # 将字符串转换为列表
+    参数名：text
+    统计参数中中文汉字词频
+    最后返回一个按词频降序排列的数组
+    """
+
+    text = text.replace('。','').replace('、','').replace('', ' ')    # 前两个去掉特殊符号，最后一个是把字符串拆成一个个汉字 【非常重要】
+    list1 = text.split()                    # 将字符串转换为列表
 
     from collections import Counter          # collections模块下的Counter计算器
     c = Counter(list1)                       # Counter对象（记住，是dict的子类）的key是待计数元素，value是对应的计数
-    return (c)                               # python 函数返回值 return，函数中一定要有return返回值才是完整的函数。                    【非常重要】
+    return (c)                               # python 函数返回值 return，函数中一定要有return返回值才是完整的函数。         【非常重要】
                                              # 如果没有python 定义函数返回值，那么会得到一个结果是None对象，而None表示没有任何值。
 
 print("中文汉字及出现次数降序排列")
 print(stats_text_cn(text))
-    
-# 函数注释说明
-print("Annotations:", stats_text_cn.__annotations__)
 print('_'*200)
