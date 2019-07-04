114b666eb0e2f6b0eb15943404984b2cd321080a
shaoguos <shgchai@163.com>
2019-07-04 13:23:10 +0800

    按照作业中要求的采用while来实现.
    Signed-off-by: shaoguos <shgchai@163.com>

--- exercises/1901100019/1001S02E04_control_flow.py
+++ exercises/1901100019/1001S02E04_control_flow.py
@@ -7,15 +7,17 @@ def multiplication_tables():
                 break
 
 def multiplication_tables_without_even():
-    for a in range(1, 10):
-        if a % 2 == 0:
-            continue
-
-        for b in range(1, 10):
-            print(a, '*', b, '=', a*b, end='\t')
-            if a == b:
-                print()
-                break
+    a = 1
+    while a < 10:
+        if a % 2 != 0:
+            b = 1
+            while b < 10:
+                print(a, '*', b, '=', a*b, end='\t')
+                if a == b:
+                    print()
+                    break
+                b += 1
+        a += 1
 
 if __name__ == '__main__':
     print("Multiplication tables:")
