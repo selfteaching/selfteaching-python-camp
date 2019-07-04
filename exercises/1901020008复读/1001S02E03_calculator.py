9ea3e187f1ff1e4b67f863df487ce8c51cf2f51d
丁浩峰 <dinghaofeng008@gmail.com>
2019-07-04 17:52:57 +0800

    自学训练营学习1群 day3 1901020008

---
+++ exercises/1901020008复读/1001S02E03_calculator.py
@@ -0,0 +1,14 @@
+x = int(input("Please enter an integer: "))
+y = int(input("Please enter an integer: "))
+operator = input("Please enter one of operators:" )
+if operator == "+":
+  print(x+y)
+elif operator == "+": 
+  print( x + y)
+elif operator == "-":
+  print( x - y)
+elif operator == "*":
+  print(x * y)
+elif operator == "/":
+  print(x/y) 
+
