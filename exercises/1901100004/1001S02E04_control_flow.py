fe003253ed78aec1fb1b4af009b2ccdc3500d0ee
祁夜宁凡 <52207404+Qiyeningfan@users.noreply.github.com>
2019-07-07 23:00:14 +0800

    祁夜  D4 (#4390)

--- exercises/1901100004/1001S02E04_control_flow.py
+++ exercises/1901100004/1001S02E04_control_flow.py
@@ -1,3 +1,22 @@
+#<<<<<<< master
+
+print('打印九九乘法表')
+for i in range(1, 10):
+    print('第%d行'% i, end='\t')
+    for j in range(1, i + 1):
+        print(i, '*', i * j, end='\t')
+    print()
+    
+    print('\n打印跳过偶数行的九九乘法表')
+    i = 1
+    while i < 10:
+        if i % 2 == 0:
+            print()
+        else:
+            for j in range(1, '*', i * j, end='\t')
+
+        i += 1
+#=======
 print(str('九九乘法表'))
 for i in range (1,10):
     # print('第%d行'% i,end = '\t')
@@ -14,3 +33,4 @@ while i < 10:   #当i小于10的时候
         for j in range(1,i+1):  #j是从1到i的数
             print(i,'*',j,'=',i*j,end='\t')  #显示i*j=i*j 结尾用占位符隔开
     i += 1  #结尾又出现一遍，重新赋值了吗？搞不懂
+#>>>>>>> master
