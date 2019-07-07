359e18918b8ed3392623b28cc35265a53228920b
Union1993 <1963721578@qq.com>
2019-07-07 22:11:41 +0800

    day10 党聪作业

--- exercises/1901050107/d09/main.py
+++ exercises/1901050107/d09/main.py
@@ -1,14 +1,18 @@
 
+# -*- coding: utf-8 -*-
+# 读取本地唐诗三百首文件中的内容，并统计其中汉字的词频，输出词频最高的前100个词 
+from collections import Counter 
+# from  mymodule import stats_word
 path = "exercises/1901050107/d09/tang300.json"
-import os
-from mymodule import stats_word
+
 with open(path) as f:
-    data = f.read()
- 
-from collections import Counter
+    data = f.read()        
 chars = []
-cnt = Counter()
+# cnt = Counter()
 for item in data:
+    if isinstance(data,str):
+        ValueError("data not a string ")
+
     if '\u4e00' <= item <= '\u9fff':
         chars.append(item)
 print(Counter(chars).most_common(100))   
