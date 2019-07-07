4cbcab0560cd386f5a9d1ac8e1c4f09a8e933d0c
youyepeng <995475839@qq.com>
2019-07-08 00:13:32 +0800

    Update 1001s02E06_stats_word.py

--- exercises/1901010115/1001s02E06_stats_word.py
+++ exercises/1901010115/1001s02E06_stats_word.py
@@ -24,18 +24,18 @@ If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 '''
 
-def stats_text_en(t):
-    t1 = t.lower()
-    t_list = t1.split()
-    dict1 = {}
-    for i in t_list:
-        ii = i.strip(' ,.*!-')
-        if ii not in dict1:
-            dict1[ii] = 1
-        else:
-            dict1[ii] = dict1[ii] + 1
-    dict2 = sorted(dict1.items(),key = lambda x:x[1],reverse = True)
-    return dict2
+def stats_text_en(t): #定义函数，参数为t
+    t = t.lower() #文本转小写
+    t_list = t.split() #文本转列表
+    t_dict = {} #新建一个空字典
+    for i in t_list: #指定i遍历上述列表
+        ii = i.strip(' ,.*!-') #清洗i中元素
+        if ii not in t_dict: #如果单词不在新建字典中
+            t_dict[ii] = 1 #则字典添加单词
+        else: #如果单词已在新建字典中
+            t_dict[ii] = t_dict[ii] + 1 #则该单词对应的计数增加1
+    t_dict_sorted = sorted(t_dict.items(),key = lambda x:x[1],reverse = True) #items()函数用于把字典转为元组；key=lambda x:x[1]用于设定键按值大小排列；reverse=True用于设定键按降序排列
+    return t_dict_sorted #返回函数结果
 print('text1:')
 print(stats_text_en(text1))
 print('\n')
@@ -56,20 +56,20 @@ text3 = '''
 我曾经在文章中提到过能力的重要性。有人问，能力有那么多，到底培养哪一个才好呢？如果我用一个两维矩阵来分析的话，可以看到，结构化思维能力，是既能够被培养同时价值度又高的能力。
 '''
 
-def stats_text_cn(t):
-    t_list = []
-    t_dict = {}
-    exclude_str = '\n ，。！？/”“'
-    for char in t:
-        t_list.append(char)
-    for char in t_list:
-        if char not in exclude_str:
-            if char.strip() not in t_dict:
-                t_dict[char] = 1
-            else:
-                t_dict[char] = t_dict[char] + 1
-    t_dict2 = sorted(t_dict.items(),key = lambda x:x[1],reverse = True)
-    return t_dict2
+def stats_text_cn(t): #定义函数，参数为t
+    t_list = [] #新建一个空列表
+    t_dict = {} #新建一个空字典
+    exclude_str = '\n ，。！？/”“' #新建一个字符串,备用（将要消除的中文符号）
+    for char in t: #遍历文本中的字节
+        t_list.append(char) #把拆解的所有字节（包括单个汉字和符号）放入新建的列表中
+    for char in t_list: #遍历列表中的元素
+        if char not in exclude_str: #如果该字节为汉字，不是符号
+            if char.strip() not in t_dict: #又如果（strip(）用于消除空格）汉字不在新建的字典中
+                t_dict[char] = 1 #则字典添加汉字
+            else: #如果汉字已在字典中
+                t_dict[char] = t_dict[char] + 1 #则对字典中该单词对应的计数增加1
+    t_dict_sorted = sorted(t_dict.items(),key = lambda x:x[1],reverse = True)#items()函数用于把字典转为元组；key=lambda x:x[1]用于设定键按值大小排列；reverse=True用于设定键按降序排列
+    return t_dict_sorted #返回函数结果
 print('text2:')
 print(stats_text_cn(text2))
 print('\n')
