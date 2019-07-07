858f10473098c6453ac045fd6f6b46cefe3573cd
veryspecialdog <13141360658@163.com>
2019-07-07 22:36:32 +0800

    DAY12

--- exercises/1901060015/d12/mymodule/prac.py
+++ exercises/1901060015/d12/mymodule/prac.py
@@ -7,9 +7,10 @@ myself= bot.self
 #向文件传数助手发送消息
 
 bot.file_helper.send('hello from wxpy!')
-my_friend = bot.friends().search('yan',sex =FEMALE,city ="上海")[0]
-
+my_friend = bot.friends().search('yan',sex =FEMALE)
 my_friend.send('hello')
+
+
 
 
 
