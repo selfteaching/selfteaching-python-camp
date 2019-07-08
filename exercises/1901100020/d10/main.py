e703165724aa6a732e221cad52f0a5318485832a
liangqs2019 <52305452+liangqs2019@users.noreply.github.com>
2019-07-08 22:48:59 +0800

    自学营009期01班 ，学号：1901100020，Day10 work
    电脑里加了个中文分词库，代码多了一行代码

---
+++ exercises/1901100020/d10/main.py
@@ -0,0 +1,10 @@
+from mymodule import stats_word
+import json
+
+with open('tang300.json','r',encoding='UTF-8') as f:
+    text = f.read()
+f.closed
+try:
+    stats_word.stats_text_cn(text,100)
+except ValueError:
+    print('Error: input is not string.')
