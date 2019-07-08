e821123c8ad4b403bc1892abb698381dcee58356
veryspecialdog <13141360658@163.com>
2019-07-08 23:49:52 +0800

    day12

--- exercises/1901060015/d12/mymodule/prac1.py
+++ exercises/1901060015/d12/mymodule/prac1.py
@@ -1,4 +1,8 @@
 from wxpy import *
+import requests
+from pyquery import PyQuery
+import stats_word
+
 bot = Bot(cache_path= True)
 
 #机器人账号自身
@@ -10,6 +14,19 @@ msg = bot.messages
 @bot.register(my_friend,SHARING)
 def get_msgurl(msg):
     return Message.url
+
+url=get_msgurl(msg)
+
+r = requests.get("url")
+#提取正文
+d=PyQuery(r.text)
+c=d('#js_content').text().replace("，","").replace("。","").replace("\n","")
+
+c1 = stats_word.stats_text_cn(c)
+#转换为字符串
+c2 =str(c1)
+
+
 
 
 
