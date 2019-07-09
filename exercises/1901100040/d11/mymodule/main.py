2d3760a4f9888eef18c081541244324dd7b3efd5
Four-Leaf-Clover <39918462@qq.com>
2019-07-09 22:48:10 +0800

    Update main.py

--- exercises/1901100040/d11/mymodule/main.py
+++ exercises/1901100040/d11/mymodule/main.py
@@ -21,7 +21,7 @@ print(b)
 #调用stats_word.py文件中的stats_text_cn函数实现：把提取到的文本分词降序排序
 
 yag=yagmail.SMTP()
-yag.send(recipients,'pythoncamp@163.com', '自学营009期01班 DAY11 Four-Leaf-Clover', b) 
+yag.send('pythoncamp@163.com', '自学营009期01班 DAY11 Four-Leaf-Clover', b) 
 
 
 
