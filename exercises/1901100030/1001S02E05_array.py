e08e1cbb17837a07b3c1681e3af210bbba662f77
lovemg11 <48599312+lovemg11@users.noreply.github.com>
2019-07-09 15:35:11 +0800

    自学训练营 13群 1901100030 day5 (#4445)
    * 1901100030day1
    
    * Create 1001S02E02_hello_python.py
    
    * Update 1001S02E02_hello_python.py
    
    * 1901100030 day3
    
    * Update 1001S02E02_hello_python.py
    
    * Create 1001S02E04_control_flow.py
    
    * 1901100030 day5

---
+++ exercises/1901100030/1001S02E05_array.py
@@ -0,0 +1,34 @@
+# day5 字符串练习
+# 2019年7月9日
+# 陈浩 学号 1901100030
+
+#对列表进行翻转
+sample_list = [0,1,2,3,4,5,6,7,8,9]
+#print(sample_list)
+#reversed_list = sample_list.reverse()
+sample_list.reverse()
+reversed_list = sample_list
+print(reversed_list)
+
+#拼接字符串
+join_str=""
+for i in reversed_list:
+    join_str = join_str + str(i)
+print(join_str)
+
+#切片取出3到8字符
+sliced_str = join_str[2:8]
+print(sliced_str)
+
+# 翻转字符串
+reversed_str = sliced_str[::-1]
+print(reversed_str)
+
+# 格式转换
+int_value = int(reversed_str)
+
+print("转换为int类型：", int_value)
+
+print("转换为二进制：", bin(int_value))
+print("转换为八进制：", oct(int_value))
+print("转换为十六进制：", hex(int_value))
