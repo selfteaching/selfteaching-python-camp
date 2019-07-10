12848315dae2d2e44fe5f97380aca2918bc678ca
Four-Leaf-Clover <39918462@qq.com>
2019-07-10 23:17:04 +0800

    DAY12

--- exercises/1901100040/d11/mymodule/main.py
+++ exercises/1901100040/d11/mymodule/main.py
@@ -1,27 +1,33 @@
 import requests
 import stats_word
-from pyquery import PyQuery
+import pyquery
 import getpass
 import yagmail
+import logging
 
-r = requests.get("https://mp.weixin.qq.com/s/pLmuGoc4bZrMNl7MSoWgiA")
-#从指定网站中请求信息
+logging.basicConfig(format='file:%(filename)s[line:%(lineno)d]message:%(message)s',level=logging.DEBUG)
 
-document=PyQuery(r.text)
-content = document('#js_content').text()
-#提取微信公众号正⽂
-
-sender = input("输入发件人邮箱：")
-password = getpass.getpass("输入发件人邮箱密码:")
-recipients = input("输入收件人邮箱:")
-
-a=stats_word.stats_text_cn(content,100)
-b=str(a)
-print(b)
-#调用stats_word.py文件中的stats_text_cn函数实现：把提取到的文本分词降序排序
-
-yag=yagmail.SMTP()
-yag.send('pythoncamp@163.com', '自学营009期01班 DAY11 Four-Leaf-Clover', b) 
+#提取公众号正文
+def get_article():
+    r = requests.get("https://mp.weixin.qq.com/s/pLmuGoc4bZrMNl7MSoWgiA")
+    document = pyquery.PyQuery(r.text)
+    return document('#js_content').text()
 
 
-
+def main():
+    try:
+        article = str(get_article)
+        result=stats_word.stats_text_cn(article,100)
+        #调用stats_word.py文件中的stats_text_cn函数实现：把提取到的文本分词降序排序
+        logging.info('%s %s',type(result),str(result))
+        sender = input("输入发件人邮箱：")
+        password = getpass.getpass("输入发件人邮箱密码:")
+        recipients = input("输入收件人邮箱:")
+        yag=yagmail.SMTP(sender,password,'stmp.qq.com')
+        yag.send(recipients,'pythoncamp@163.com', '自学营009期01班 DAY11 Four-Leaf-Clover', str(result)) 
+        logging.info("已发送，请注意查收。")
+    except Exception as e:
+        logging.exception(e)
+        
+if __name__=="__main__": 
+    main()
