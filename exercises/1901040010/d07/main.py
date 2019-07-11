08f875e799677276cf3c6bc01160f5a05cfaf26b
moxiyun <cicimok@qq.com>
2019-07-11 16:26:07 +0800

    Day07

--- exercises/1901040010/d07/main.py
+++ exercises/1901040010/d07/main.py
@@ -1,9 +1,9 @@
+from mymodule import stats_word
 
-from stats_word import stats_text
-
-text= '''
+text1 = '''
 This is a wonderful day. It is a sunny hot day.
 昨天下雨了，今天又下雨了，明天还会下雨吗？
 '''
 
-print(stats_text(text)) 
+result = stats_text(text1)
+print('统计结果==>',result)
