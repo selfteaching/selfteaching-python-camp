765d48e359ac11f75d37aa9001c5842deb4f85c7
Wanzijun66 <845563869@qq.com>
2019-07-12 11:38:21 +0800

    Create state_word.py

---
+++ exercises/1901090066/d10/mymodule/state_word.py
@@ -0,0 +1,37 @@
+from collections import Counter
+import jieba
+
+def stats_text_en(tx1,count):
+    # 将符号变为空格，将字符串分割
+    if not isinstance(tx1,str):#如果不是字符串，则抛出异常
+        raise ValueError("输入的不是字符串类型!")
+    text0=[]
+    txt= tx1.replace('\n', ' ').replace('.', ' ').replace('：', ' ').replace('--', ' ').replace('.',' ').replace('*',' ').replace('「',' ').replace('」',' ')
+    txt1=txt.split()
+    for i in txt1:
+        if i >'\u9fff' or i<'\u4e00':
+            text0.append(i)
+    count=int(count)
+    return Counter(text0).most_common(count)
+
+
+def stats_text_cn(text,count):
+    # 将text中符号变为空格，然后放到list中
+    if not isinstance(text,str):#如果不是字符串，则抛出异常
+         raise ValueError("输入的不是字符串类型!")
+    count=int(count) 
+    words=jieba.cut(text,cut_all=False)   
+    cn_characters=[]
+    for i in words:
+        if '\u4e00'<=i<='\u9fff':
+            if len(i)>=2:
+                 cn_characters.append(i)
+    return Counter(cn_characters).most_common(count)
+
+def stats_text(txx,count): #定义函数，分别调⽤stats_text_en , stats_text_cn ，输出合并词频统计结果
+    if not isinstance(txx,str):#如果不是字符串，则抛出异常
+         raise ValueError("输入的不是字符串类型!")
+    result = stats_text_en(txx,count) + stats_text_cn(txx,count)
+    return result 
+
+
