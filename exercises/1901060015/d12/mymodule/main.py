eaba6c731ab5434715641240fac2cb36009c9bfd
veryspecialdog <13141360658@163.com>
2019-07-12 23:08:26 +0800

    day13

--- exercises/1901060015/d12/mymodule/main.py
+++ exercises/1901060015/d12/mymodule/main.py
@@ -2,7 +2,7 @@ import logging
 import requests
 import pyquery
 from wxpy import *
-from mymodule import stats_word
+import stats_word
 
 # 安装依赖包 wxpy
 # pip install -i https://pypi.tuna.tsinghua.edu.cn/simple wxpy
@@ -20,12 +20,12 @@ def main():
     bot = Bot()
     friends = bot.friends()
 
-    @bot.register(friends, SHARING)
+    @bot.register(friends,SHARING)
     def handler(msg):
         try:
             logging.info('sharing url = %s', msg.url)
             article = get_article(msg.url)
-            result = stats_word.stats_text_cn(article, 100)
+            result = stats_word.stats_text_cn(article)
             msg.reply(str(result))
         except Exception as e:
             logging.exception(e)
