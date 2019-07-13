bcbfe72b374f1cd69b25c96cbe2a19b405ea5931
Wanzijun66 <845563869@qq.com>
2019-07-13 16:25:39 +0800

    Delete main.py

--- exercises/1901090066/d10/main.py
+++
@@ -1,9 +0,0 @@
-from mymodule import state_word
-fo=open("//Users//zhangyuxiaowanzi//Documents//English//selfteaching-python-camp//exercises/1901090066//d10//tang300.json","r")
-text=fo.read()
-fo.close
-try:
-    print('统计出现最多的前20词：',state_word.stats_text_cn(text,20))
-except ValueError :
-    print('输入异常') 
-
