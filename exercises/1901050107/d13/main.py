7f95fe1684e1c144056bb69ebff1136d50999750
党聪 <17710462701@163.com>
2019-07-13 13:51:02 +0800

    day13 自学营009期01班 1901050107 (#4564)
    * Create 1001S02E03_calculator.py
    
    * Create 1001S02E02_hello_python
    
    *  day4 乘法表
    
    * day5 作业
    
    * day5 第二次提交
    
    * day6 党聪
    
    * Revert "day6 党聪"
    
    This reverts commit a626c4bf69bb37fec2f8e52baaabfc543e16f4c8.
    
    * day6 作业
    
    * day7 党聪
    
    * day8 作业党聪
    
    * day9 党聪作业
    
    * day10 党聪作业
    
    * day11 作业，009期01班，1901050107，党聪
    
    * Update main.py
    
    day12 自学营009期01班  1901050107 党聪
    
    * day13 自学营009期01班  1901050107  
    
    本地github出了问题，一时上传不了，直接在网页端传入
    
    * Create d13
    
    * Delete d13
    
    * Create stats_word.py
    
    * Add files via upload
    
    * Delete main.py
    
    * Delete tang300.json
    
    * Update README.md

--- exercises/1901050107/d13/main.py
+++ exercises/1901050107/d13/main.py
@@ -1,5 +1,3 @@
-
-
 # encoding=utf-8
 from wxpy import *
 import jieba
@@ -8,9 +6,19 @@ import requests
 from pyquery import PyQuery
 from collections import Counter
 
-from mymodule import stats_word
+#from mymodule import stats_word
 import getpass
 import logging
+
+import matplotlib.pyplot as plt
+import matplotlib.image as imagedeal
+import numpy as np
+from pylab import *
+
+
+from matplotlib.font_manager import FontProperties
+# 提取字体对象
+font = FontProperties(fname='/Library/Fonts/Songti.ttc')
 
 # 提取公众号文章里的正文内容并统计词频前100的词汇
 
@@ -27,14 +35,15 @@ def get_article_hotwords(path,count):
         words.append(item[0])
     words = ','.join(words)
     return words
-'''
-sender:发送者，不能为空
-author_key：发送者邮箱的授权码
-host：邮箱服务器类别
-subject：邮件标题
-message：邮件正文
-recepients = None：接受者
-'''
+
+
+# sender:发送者，不能为空
+# author_key：发送者邮箱的授权码
+# host：邮箱服务器类别
+# subject：邮件标题
+# message：邮件正文
+# recepients = None：接受者
+
 def send_mail(sender,author_key ,host, subject,message,recipients = None):
     try: 
         # print(sender,author_key ,host, subject,message,recipients)
@@ -54,6 +63,28 @@ subject = "自学营009期01班 day12 党聪 "
 # '自学营009期01班 JiJun-1993'
 recipients = ["pythoncamp@163.com",sender]
 
+image_path = "/Users/mac/Desktop/selfteaching-python-camp/exercises/1901050107/d13/temp.png"
+def graph_data(item_list):
+# Fixing random state for reproducibility
+    np.random.seed(19680801)
+
+    plt.rcdefaults()
+    fig, ax = plt.subplots()
+    y_pos = np.arange(len(item_list))
+    performance = 3 + 10 * np.random.rand(len(item_list))
+    error = np.random.rand(len(item_list))
+
+    ax.barh(y_pos, performance, xerr=error, align='center')
+    ax.set_yticks(y_pos)
+    ax.set_yticklabels(item_list,fontproperties = font)
+    ax.invert_yaxis()  # labels read top-to-bottom
+    ax.set_xlabel('出现次数',fontproperties = font)
+    ax.set_title("你发我的文章中的关键词统计",fontproperties = font)
+    plt.savefig(image_path)
+    return image_path
+
+
+
 # 初始化机器人，扫码登陆
 bot = Bot()
 
@@ -61,9 +92,14 @@ bot = Bot()
 def print_others(msg):
     if msg.type == "Sharing":
     # 获取文章中关键词组成的字符串
-        context = get_article_hotwords(msg.url,100)
-        send_mail(sender,author_key ,host, subject,context,recipients)
-        msg.reply(context)
+        context = get_article_hotwords(msg.url,20)
+        # 把字符串对性转化为元组
+        context = tuple(context.split(','))
+        result = graph_data(context)
+        msg.reply_image(result)
+        # send_mail(sender,author_key ,host, subject,context,recipients)
+        print("回复成功")
     else:
         print(msg)
 embed()    
+
