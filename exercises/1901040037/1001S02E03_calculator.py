44d3df6c22f6fb2e3b497c74113138fa1e2cb37c
Peiyueliu <36148780+Peiyueliu@users.noreply.github.com>
2019-07-15 22:04:50 +0800

    自学训练营学习3群 (#4617)
    * Create 1001S02E03_calculator.py
    
    * day3
    
    file
    
    * Create 1001S02E04_control_flow.py

---
+++ exercises/1901040037/1001S02E03_calculator.py
@@ -0,0 +1,22 @@
+num1 = int(input("Enter a number:"))
+op = input('Enter Operator')
+num2 = int(input("Enter another number:"))
+
+if op == "+":
+	print(num1 + num2)
+
+elif op == '-':
+	print(num1 - num2)
+
+elif op == "*":
+	print(num1 * num2)
+
+elif op == "/":
+	print(num1 / num2)
+
+else:
+	print("invalid operator")
+<<<<<<< HEAD
+	
+=======
+>>>>>>> c184ccdc2c33a47756508a149add32f7e3c3fc8b
