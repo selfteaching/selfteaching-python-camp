899ca87f93eb707f1cbdee02d74102715da8fffa
veryspecialdog <13141360658@163.com>
2019-07-18 01:47:51 +0800

    day13

--- exercises/1901060015/d13/mymodule/main.py
+++ exercises/1901060015/d13/mymodule/main.py
@@ -3,9 +3,15 @@ import requests
 import pyquery
 from wxpy import *
 import stats_word
-
+import matplotlib.pyplot as plt
+import numpy as np
 # 安装依赖包 wxpy
 # pip install -i https://pypi.tuna.tsinghua.edu.cn/simple wxpy
+
+#获取当前工作目录
+cwd = path.abspath(path.dirname(__file__))
+#设置中文字体
+plt.rcParams['font.sans-serif'] = 'SimHei'
 
 logging.basicConfig(format='file:%(filename)s|line:%(lineno)d|message: %(message)s', level=logging.DEBUG)
 
@@ -14,6 +20,24 @@ def get_article(url):
     r = requests.get(url)
     document = pyquery.PyQuery(r.text)
     return document('#js_content').text()
+
+def make_P(data,image_path):
+    labels = [v[0] for v in data]
+    widths = [v[1] for v in data]
+    ypos =range(len(data))
+    fig,ax = plt.subplots()
+    ax.barh(ypos,widths)
+    ax.set_yticks(ypos)
+    ax.set_yticklabels(labels)
+    ax.invert_yaxis()
+    ax.set_ylabel('关键字')
+    ax.set_xlabel('词频')
+    ax.set_tile('词频统计')
+    fig.savefig(image_path,bbox_inches-'tight')
+
+
+
+
 
 
 def main():
@@ -26,7 +50,10 @@ def main():
             logging.info('sharing url = %s', msg.url)
             article = get_article(msg.url)
             result = stats_word.stats_text_cn(article)
-            msg.reply(str(result))
+            image_path = path.join(cwd,'stats.png')
+            make_P(result,image_path)
+
+            msg.reply(image_path)
         except Exception as e:
             logging.exception(e)
     embed()
