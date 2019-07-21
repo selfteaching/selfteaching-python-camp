0d186b4e794c86c24400f494465c189d6e831456
jinghuanlee <foosun8@gmail.com>
2019-07-21 21:30:03 +0900

    day5
    day5

--- exercises/1901100049/1001S02E05_string.py
+++ exercises/1901100049/1001S02E05_string.py
@@ -1,25 +1,25 @@
-sample_text = '''
-The zen of pyhon, by Tim peters
+sample_text = ''' 
+The Zen of Python, by Tim Peters
 
 
-Beautiful is better than ugly.
+Beautiful is better than ugly. 
 Explicit is better than implicit.
-simple is better than complex.
-Complex is better than complicated.
-Flat is better than nested.
+Simple is better than complex. 
+Complex is better than complicated. 
+Flat is better than nested. 
 Sparse is better than dense.
-Readanility counts.
+Readability counts.
 Special cases aren't special enough to break the rules.
-Although practicality beats purity.
+Although practicality beats purity. 
 Errors should never pass silently.
-Unless explicitly silenced.
+Unless explicitly silenced. 
 In the face of ambxiguity, refuse the temptation to guess.
-There should be one-- and preferably only one --obvious way to doit.
-Although that way may not be obvious at first unless you're Dutch.
+There should be one-- and preferably only one --obvious way to do it. 
+Although that way may not be obvious at first unless you're Dutch. 
 Now is better than never. 
 Although never is often better than *right* now. 
-If the implementation is hard to explain, it's a bad idea. 
-If the implementation is easy to explain, it may be a good idea. 
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 '''
 
@@ -32,7 +32,7 @@ print('将字符串样本里的 better 全部替换成 worse ==>', text)
 # 2. 将单词中包含 ea 的单词剔除
 
 # 先将字符串根据 空白字符 分割成 list, 要调用 str 类型
-words - text.split()
+words = text.split()
 # 定义一个 list 类型的变量用来存放过滤完的单词
 filtered = []
 # 用 for ... in 循环遍历一遍 words 里的元素然后判断单词是否包含 ea
