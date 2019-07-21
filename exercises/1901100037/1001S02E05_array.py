33289ed227ca427e380b3b0373cebd47d7d179fd
srvz <2470962+srvz@users.noreply.github.com>
2019-07-21 18:36:34 +0800

    Update 1001S02E05_array.py

--- exercises/1901100037/1001S02E05_array.py
+++ exercises/1901100037/1001S02E05_array.py
@@ -2,11 +2,8 @@ sample_list = [0,1,2,3,4,5,6,7,8,9]
 reversed_list = sample_list[::-1]
 print('列表翻转 ==>',reversed_list)
 
-#<<<<<<< master
+
 joined_str = ''.join([str(i) for i in reversed_list])
-#=======
-joined_str = ''.join([str9i for i in reversed_list])
-#>>>>>>> master
 print('翻转后的数组拼接成字符串 ==>',joined_str)
 
 sliced_str = joined_str[2:8]
