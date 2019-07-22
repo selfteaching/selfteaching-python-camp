324acb581ed7b38a4b32dbb9e38e2fae4752566d
卢卿 <adam.lu@idreamsky.com>
2019-07-22 12:07:57 +0800

    使用 os.path.join 拼接路径

--- exercises/1901090022/d10/main.py
+++ exercises/1901090022/d10/main.py
@@ -4,7 +4,8 @@ import json
 import os
 
 json_dir = os.path.dirname(__file__)
-json_file = open(json_dir + '/1001S02E09.json')
+json_file_path = os.path.join(json_dir, '1001S02E09.json')
+json_file = open(json_file_path)
 jason_data = json.load(json_file)
 
 text = ""
