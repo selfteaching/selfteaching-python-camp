7dbd541ef0736b3a46791d93a516f05b15187cd1
Meroy <506589981@qq.com>
2019-07-23 20:27:58 +0800

    Update main.py

--- exercises/1901100088/d09/main.py
+++ exercises/1901100088/d09/main.py
@@ -1,7 +1,8 @@
 from mymodule import stats_word
 import os
 
-with open(os.getcwd() + "/d09/tang300.json" ,'r') as f:
+with open( os.getcwd() + "/d09/tang300.json" ,'r') as f: #Vscode当前文件位置定位19011000088文件夹
+#with open("tang300.json" ,'r') as f:  pycharm中可以通过文件名称打开文件，其当前文件位置定位为d09文件夹
     text = f.read()
 
 if __name__ == "__main__":
@@ -12,3 +13,4 @@ if __name__ == "__main__":
     except AttributeError:
         print('list object has no attribute lower')
 
+       
