8f19765b009b3b70f87e90937342a21375833d37
Liupeiyue <peckyliu11@gmail.com>
2019-07-24 21:29:52 +0800

    day 8【学号：1901040037】 自学3营地
    day 8 作业

--- exercises/1901040037/1001S02E05_string.py
+++ exercises/1901040037/1001S02E05_string.py
@@ -21,44 +21,24 @@ If the implementation is easy to explain, it may be a good idea.
 Namespaces are one honking great idea -- let's do more of those!
 '''
 
-# 将字符串串样本text⾥里里的better全部替换成worse
-
-
-print('better' in text)
+# 1、将字符串串样本text⾥里里的better全部替换成worse
 text = text.replace('better','worse')
-print(text)
+print('将字符串里的better全部替换为worse：',text)
 
-print('1=======================')
+# 从第 1 步的结果⾥里里，将单词中包含 ea 的单词剔除
+words = text.split()  # 切成一个列表,如果写text.split（' '）的话，换行符号会保留
+print(words)
 
-# 从第 2 步的结果⾥里里，将单词中包含 ea 的单词剔除
-text = text.split()  # 切成一个列表,如果写text.split（' '）的话，换行符号会保留
-print(text)
-
-print('2=======================')
-
-
-
-list1 = []
-for i in text:  # 遍历这个列表,把含有 ea 的东西剔除掉
-    if 'ea' not in i:
-        list1.append(i)
-
-print(list1)
-
-print('3=======================')
+filtered = [] #用来存放单词
+for word in words:  # 遍历这个列表,把含有 ea 的东西剔除掉
+    if word.find('ea')< 0:
+        filtered.append(word)
+print('将单词中包含ea的单词剔除',filtered)
 
 # 将第3步的结果⾥的字⺟进⾏大小写翻转
-
-text2 = []
-for i in list1:
-    i.strip()
-    text2.append(str(i).swapcase())
-
-print(text2)
-print('4=======================')
+swapcased = [i.swapcase() for i in filtered]
+print(swapcased)
 
 # 将第4步的结果⾥里里所有单词按a...z升序排列列，并输出结果
-text2.sort()
-print(text2)
 
-print('5=======================')
+print(sorted(swapcased))
