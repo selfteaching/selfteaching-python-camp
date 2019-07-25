ebecb57da7119dabc1d8bb07aa11884f7a732a97
Zmoon73 <zyn@gmail.com>
2019-07-25 16:30:29 +0800

    Update stats_word.py

--- exercises/1901100079/08/mymodule/stats_word.py
+++ exercises/1901100079/08/mymodule/stats_word.py
@@ -22,7 +22,7 @@ def stats_text_en(text):
           
 def stats_text_cn(text):           
     if not isinstance(text,str):
-        raise ValueError(('无效输入！输入类型：%s'%type(text))
+        raise ValueError('无效输入！输入类型：%s'%type(text))
     a=[]
     for b in text:
         if '\u4e00'<=b<='\u9fff':
@@ -36,5 +36,5 @@ def stats_text_cn(text):
         
 def stats_text(text):# 重新定义一个函数把上述两个函数封装为一个函数
     if not isinstance(text,str):
-        raise ValueError(('无效输入！输入类型：%s'%type(text))
+        raise ValueError('无效输入！输入类型：%s'%type(text))
     return stats_text_en(text)+stats_text_cn(text)# 因为两个函数的返回值都为list，所以可以用+合并在一起'''
