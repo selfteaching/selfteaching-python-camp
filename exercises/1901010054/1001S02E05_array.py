0d277dc5e441c6fd8f86e1e6d2b970ee58c298e4
Evan <li_2281838166@gmail.com>
2019-07-27 22:26:35 +0800

    Update 1001S02E05_array.py

--- exercises/1901010054/1001S02E05_array.py
+++ exercises/1901010054/1001S02E05_array.py
@@ -1,19 +1,3 @@
-Skip to content
- 
-Search or jump to…
-
-Pull requests
-Issues
-Marketplace
-Explore
- 
-@Evanopenlife 
-Code  Issues 368  Pull requests 1  Projects 14  Wiki  Security  Pulse  Community
-selfteaching-python-camp/exercises/1901010052/1001S02E05_array.py
-@Luckylisa379 Luckylisa379 Create 1001S02E05_array.py
-a7c7388 on 4 May
-20 lines (15 sloc)  284 Bytes
-    
 num1=[0,1,2,3,4,5,6,7,8,9]
 num2=list(reversed(num1)) #翻转
 print(num1)
@@ -34,15 +18,3 @@ a=int(result)
 print(bin(a))
 print(oct(a))
 print(hex(a))
-© 2019 GitHub, Inc.
-Terms
-Privacy
-Security
-Status
-Help
-Contact GitHub
-Pricing
-API
-Training
-Blog
-About
