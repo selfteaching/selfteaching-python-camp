8c6e2a3082c236c1fa2e014b233cef6ddf4bb413
Zezhou-Sun <894101858@qq.com>
2019-07-29 21:42:06 +0800

    day07

--- exercises/1901010161/1001S02E06_stats_word.py
+++ exercises/1901010161/1001S02E06_stats_word.py
@@ -9,7 +9,7 @@ def stats_text_en(text):  # 定义英语文本统计函数
         wordcount[i] = str.count(i)
     wordcount = sorted(wordcount.items(), key=lambda x: x[1], reverse=True)
     print(wordcount)
-    return
+    return wordcount      # 原来这里只写了return，系统虽然没报错，但输出时结果最后面有个none，没明白错误出在哪里，经老师提醒改正后果然没有none了
 
 
 text = '''
@@ -58,7 +58,6 @@ Namespaces are one honking great idea -- let's do more of those!
 print(stats_text_en(text))
 
 
-
 def stats_text_cn(text):     # 定义中文文本统计函数
     p = re.compile(r'[\u4e00-\u9fa5]')    # 中文基本汉字（20902字）的编码范围是：\u4e00到\u9fa5
     res = re.findall(p, text)   # 获取所有中文字符
@@ -67,7 +66,7 @@ def stats_text_cn(text):     # 定义中文文本统计函数
         wordcount[i] = res.count(i)     # 词频统计词典设置
     wordcount = sorted(wordcount.items(), key=lambda x: x[1], reverse=True)
     print(wordcount)  # 输出每个中文汉字频数统计结果
-    return
+    return wordcount
 
 
 print(stats_text_cn(text))
