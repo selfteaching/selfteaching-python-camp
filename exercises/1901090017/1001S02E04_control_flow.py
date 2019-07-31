2b664b2681c294295cffd7aaab58690298eddc8a
eaebymomo <157300861@qq.com>
2019-07-31 22:04:10 +0800

    Update 1001S02E04_control_flow.py

--- exercises/1901090017/1001S02E04_control_flow.py
+++ exercises/1901090017/1001S02E04_control_flow.py
@@ -7,7 +7,12 @@ i=1
 while i<10:
     j=1
     while j<i+1:
-        print(i,'*',j,'=',i*j,end='\t',sep='')
+        if i%2==0:
+            break
+        else:
+            print(i,'*',j,'=',i*j,end='\t',sep='')
         j += 1
-    print()  
+    if i%2!=0:
+        print()  
     i+=1
+
