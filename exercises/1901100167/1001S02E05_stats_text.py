dfc2f00d43db170b8a5116a6b2058ece5fbc0e1a
comevback <48838127+comevback@users.noreply.github.com>
2019-07-31 23:52:53 +0800

    Update 1001S02E05_stats_text.py

--- exercises/1901100167/1001S02E05_stats_text.py
+++ exercises/1901100167/1001S02E05_stats_text.py
@@ -19,6 +19,8 @@ If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea. 
 Namespaces are one honking great idea -- let's do more of those .
 '''
+map=str.maketrans('*-.','   ')
+text=text.translate(map)
 a = text.split()
 dic={ }
 for x in a:
