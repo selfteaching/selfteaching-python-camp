595520bc2603832e76e1157fa948051d938acc8f
eaebymomo <157300861@qq.com>
2019-07-31 10:38:40 +0800

    Create 1001S02E04_control_flow.py

---
+++ exercises/1901090017/1001S02E04_control_flow.py
@@ -0,0 +1,13 @@
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(i,'*',j,'=',i*j,end='\t',sep='')
+    print()       
+          
+i=1 
+while i<10:
+    j=1
+    while j<i+1:
+        print(i,'*',j,'=',i*j,end='\t',sep='')
+        j += 1
+    print()  
+    i+=1
