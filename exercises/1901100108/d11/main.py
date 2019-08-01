c64dd2642f32293129626153374e99f5f829ce9d
Mathophy <1564898312@qq.com>
2019-08-02 00:22:50 +0800

    Update main.py

--- exercises/1901100108/d11/main.py
+++ exercises/1901100108/d11/main.py
@@ -16,20 +16,16 @@ def get_article():
 def main():
     try:
         article = get_article()
-        result = stats_word.stats_text_cn(article,10)
+        result = stats_word.stats_text_cn(article,100)
         logging.info('%s %s', type(result), str(result))
         sender = input('请输入发件人邮箱：')
         password = getpass.getpass('请输入发件人邮箱密码：')
         recipients = input('请输入收件人邮箱：')
-        yag = yagmail.SMTP(sender, password, 'smt.qq.com')
-        yag.send(recipients, '在自学python入门的WXS:', str(result))
+        yag = yagmail.SMTP(sender, password, 'SMTP.qq.com')
+        yag.send(recipients, '【1901100108】⾃学训练营学习16群 DAY11 Mathophy', str(result))
         logging.info('已发送，请注意查收。')
     except Exception as e:
         logging.exception(e)
-
-
-
-
 
 if __name__ == "__main__":
     main()
