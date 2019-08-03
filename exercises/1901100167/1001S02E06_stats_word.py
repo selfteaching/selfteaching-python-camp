40854b590f04d93c97c5d857c89230718e2a5cd9
comevback <48838127+comevback@users.noreply.github.com>
2019-08-03 14:31:57 +0800

    Update 1001S02E06_stats_word.py

--- exercises/1901100167/1001S02E06_stats_word.py
+++ exercises/1901100167/1001S02E06_stats_word.py
@@ -19,28 +19,34 @@ If the implementation is hard to explain, it's a bad idea.
 If the implementation is easy to explain, it may be a good idea. 
 Namespaces are one honking great idea -- let's do more of those .
 '''
+
+text2='''
+深啡网烦无法是而是艾弗森额额，。234 qewffesaf
+'''
 def stats_text_en(st1):
-    map=str.maketrans('*-.','   ')
-    st1=st1.translate(map)
-    a=st1.split()
-    dic={}
-    for x in a:
-        dic.setdefault(x,0)
-        dic[x]+=1
-    for k in sorted(dic,key=dic.__getitem__,reverse=True):
-        print(k,dic[k])
+        map=str.maketrans('*-.','   ')
+        st1=st1.translate(map)
+        a=st1.split()
+        dic={}
+        b=[]
+        for x in a:
+                dic.setdefault(x,0)
+                dic[x]+=1
+        b=sorted(dic.items(),key=lambda x:x[1],reverse=True)
+        return b
 
 print(stats_text_en(text))
 
 def stats_text_cn(st2):
         a=list(st2)
         dic={}
+        b=[]
         for x in a:
                 if u'\u4e00'<=x<=u'\u9fa5':
                         dic.setdefault(x,0)
                         dic[x]+=1
-        for k in sorted(dic,key=dic.__getitem__,reverse=True):
-                print (k,dic[k])
+        b=sorted(dic.items(),key=lambda x:x[1],reverse=True)
+        return b
 
 print(stats_text_cn(text2))
 
