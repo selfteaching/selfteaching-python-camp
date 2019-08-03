7c3c98d5ff8ecf53d1c25ac9b60b864f102695fb
yangbisheng <2606424906@qq.com>
2019-08-03 15:25:16 +0800

    Create 1001S02E03_calculator.py

---
+++ exercises/1901070018/1001S02E03_calculator.py
@@ -0,0 +1,17 @@
+def add(a,b):
+    return a+b 
+
+def substructin(a, b):
+    return a - b
+    
+def multiplication(a, b):
+    return a * b
+
+def division(a, b):
+    return a / b
+
+
+print(add(2, 4))
+print(substructin(4, 4))
+print(multiplication(5, 5))
+print(division(3, 3))
