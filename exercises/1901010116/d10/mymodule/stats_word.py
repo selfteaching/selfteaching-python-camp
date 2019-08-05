0e73c5a13a0e5b0a5ec207be99e35fac98a8cf69
Tiannaxu <51069179+Tiannaxu@users.noreply.github.com>
2019-08-05 14:54:55 +0800

    【1901010116】自学训练营学习1群 DAY11 (#5214)
    * Create 1001S02E01_helloworld.txt
    
    * Create README.md
    
    * Create 1001S02E02_hello_python.py
    
    * Create 1001S02E03_calculator.py
    
    * Create 1001S02E04_control_flow.py
    
    * Create 1001S02E05_array.py
    
    * Create 1001S02E05_stats_text.py
    
    * Create 1001S02E05_string.py
    
    * Create 1001S02E06_stats_word.py
    
    * Create main.py
    
    * Create stats_word.py
    
    * Create main.py
    
    * Create stats_word.py
    
    * Create main.py
    
    * Create stats_word.py
    
    * Create tang300.json
    
    * Create tang300.json
    
    * Create stats_word.py
    
    * Create main.py
    
    * Update main.py
    
    * Create tang300.json
    
    * Create stats_word.py
    
    * Create main.py

---
+++ exercises/1901010116/d10/mymodule/stats_word.py
@@ -0,0 +1,50 @@
+#封装统计英文单词词频的函数
+from collections import Counter
+import string
+import re
+import jieba
+
+def stats_text_en(text,count = None):
+    if not isinstance(text,str): #检查参数类型是否为字符串
+        raise ValueError('Oops! The first parameter was no valid string.')
+    elif not isinstance(count,int) and count != None: #检擦输入的第二个参数是否为整数
+        raise ValueError('Oops! The second parameter was no valid integer.')
+    else:
+        text = re.findall(r'\w+',text.lower(),re.A) #筛选英文单词，剔除其他文字
+
+        #统计单词出现次数  
+        f = Counter(text).most_common(count)
+        return f
+
+#封装统计中文汉字字频的函数
+def stats_text_cn(text,count = None):
+    if not isinstance(text,str): #检查参数类型是否为字符串
+        raise ValueError('Oops! The first parameter was no valid string.')
+    elif not isinstance(count,int) and count != None: #检擦输入的第二个参数是否为整数
+        raise ValueError('Oops! The second parameter was no valid integer.')
+    else:
+        text = re.sub(r'[^\u4E00-\u9FA5]',' ',text) #只留下汉字，剔除其他字符
+        text = jieba.cut(text,cut_all = False)
+        text_list = []
+        for i in text:
+            if len(i) > 1:
+                text_list.append(i)
+
+        #统计汉字出现次数
+        f = Counter(text_list).most_common(count)
+        return f
+
+#分别调⽤stats_text_en , stats_text_cn ，输出合并词频统计结果
+def stats_text(text,count = None):
+    """
+    统计英文单词及中文汉字出现的频次
+    
+    参数为字符串
+    返回一个数列
+    """
+    if not isinstance(text,str): #检查参数类型是否为字符串
+        raise ValueError('Oops! The first parameter was no valid string.')
+    elif not isinstance(count,int) and count != None: #检擦输入的第二个参数是否为整数
+        raise ValueError('Oops! The second parameter was no valid integer.')     
+    else:
+        return stats_text_en(text,count) + stats_text_cn(text,count)
