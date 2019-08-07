fa63064a775dc2bd69f598ac6a333456900441bf
comevback <48838127+comevback@users.noreply.github.com>
2019-08-07 16:01:49 +0800

    改

--- exercises/1901100167/d09/main.py
+++ exercises/1901100167/d09/main.py
@@ -5,8 +5,8 @@ from collections import Counter
 sys.path.append('e:\\self-teaching\\selfteaching-python-camp\\exercises\\1901100167\\d09')
 file_path=os.path.join(os.path.dirname(os.path.abspath(__file__)),'tang300.json')
 print(file_path)
-f=open(file_path,'r',encoding='utf-8')
-a=f.read()
+with open(file_path,'r',encoding='utf-8') as f:
+    a=f.read()
 from mymodule import stats_word
 try:
     print(stats_word.stats_text_cn(a))
