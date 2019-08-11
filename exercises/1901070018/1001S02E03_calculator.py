f70eb7908c0500844e1937812a2f597fc5ee7cfd
yangbisheng <2606424906@qq.com>
2019-08-11 16:09:15 +0800

    Update 1001S02E03_calculator.py

--- exercises/1901070018/1001S02E03_calculator.py
+++ exercises/1901070018/1001S02E03_calculator.py
@@ -1,17 +1,25 @@
-def add(a,b):
-    return a+b 
+def add(a, b):
+    return a + b
+
 
 def substructin(a, b):
     return a - b
-    
+
+
 def multiplication(a, b):
     return a * b
+
 
 def division(a, b):
     return a / b
 
 
-print(add(2, 4))
-print(substructin(4, 4))
-print(multiplication(5, 5))
-print(division(3, 3))
+first_value = input('请输入数值：')
+second_value = input('请输入数值：')
+list1 = [int(first_value),int(second_value)]
+
+
+print("{} + {}".format(list1[0], list1[1]), add(list1[0],list1[1]))
+print('{} - {}'.format(list1[0], list1[1]), substructin(list1[0], list1[1]))
+print('{} * {}'.format(list1[0], list1[1]), multiplication(list1[0], list1[1]))
+print('{} / {}'.format(list1[0], list1[1]), division(list1[0], list1[1]))
