5195425e7ef7aa07e0f7a511581fc16dfc9b9a97
yangbisheng <2606424906@qq.com>
2019-08-13 20:42:59 +0800

    【1901070018】自学训练营18群DAY9

--- exercises/1901070018/day9/mymodule/stats_word.py
+++ exercises/1901070018/day9/mymodule/stats_word.py
@@ -1,41 +1,45 @@
 from collections import Counter
+import re
+
+
 def stats_text_en(text):
     '''统计英文参数中的词频并且并且降序排列'''
     count = int(input('请输入一个数子，用以限制输出英文词频的个数：'))
     if type(text)is not str:
         raise ValueError("This value is\' string.")
-    char = ',.-:;!@#$%^&*()-+=<>?/\\`~|[]{}，。？、·~！@#￥%……&*（）——+-={}【】、|；：“”‘’'
-    for i in char:
-        text = text.replace(i, '')
+    cop = re.compile("[^\u4e00-\u9fa5^a-z^A-Z]")  # 匹配不是中文、大小写 的其他字符
+    text = cop.sub(' ', text)  # 将text中匹配到的字符替换成空字符
     list1 = text.split()
-    dic = {}
     list2 = []
     for i in list1:
-        for j in i:
-            if (65<=ord(j)<=90)or(97<=ord(j)<=122):
-                list2.append(i)
-
-    for i in list2:
-        num = list2.count(i)
-        dic[i] = num
-    list3 = sorted(dic.items(), key=lambda d: d[1], reverse=True)
-    new_li = Counter(list3).most_common(count)
+        for uchar in i:
+            if (uchar >= u'\u0041' and uchar <= u'\u005A') or (uchar >= u'\u0061' and uchar <= u'\u007A'):
+                pass
+            else:
+                break
+        else:
+            list2.append(i)
+    new_li = Counter(list2).most_common(count)
     return new_li
+
+
 
 def stats_text_cn(text):
     '''统计中文参数中的词频并且并且降序排列'''
     count = int(input('请输入一个数子，用以限制输出中文词频的个数：'))
     if type(text)is not str:
         raise ValueError("This value is\' string.")
+
     list1 = []
     dic = {}
     for i in text:
         if '\u4e00' <= i <= '\u9fff':
             list1.append(i)
-            dic[i] = list1.count(i)
-    list2 = sorted(dic.items(), key=lambda d: d[1], reverse=True)
-    new_li = Counter(list2).most_common(count)
+
+    new_li = Counter(list1).most_common(count)
     return new_li
+
+
 
 def stats_text(text):
     '''分别统计中文与英文词频并且合并输出'''
