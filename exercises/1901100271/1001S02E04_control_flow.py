e6835be4b1c1bdb39cff5697a290ab38cd9ce337
noodleCYF <53895938+noodleCYF@users.noreply.github.com>
2019-08-16 20:51:38 +0800

    Update 1001S02E04_control_flow.py

--- exercises/1901100271/1001S02E04_control_flow.py
+++ exercises/1901100271/1001S02E04_control_flow.py
@@ -4,7 +4,8 @@ for i in range (1,10):
         if i == j:
             print ('\n')
             i = i + 1
-            break
+            
+            break #break语句在这里是跳出for j的循环
         else:
             j = j + 1
 pass
@@ -13,13 +14,10 @@ print ("next work\n")
 i = 1
 while (i < 10):
     j = 1
-    if i % 2 == 0:
-        break
-    else:
+    print (i,"*",j, "=", i*j, end='\t')
+    while (j < i):
+        j += 1
         print (i,"*",j, "=", i*j, end='\t')
-        while (j < i):
-            j += 1
-            print (i,"*",j, "=", i*j, end='\t')
             
     print ('\n')
     i += 2
