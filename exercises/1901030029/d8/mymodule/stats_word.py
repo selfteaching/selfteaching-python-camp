dcb21951fba6f4ca3f3822607e133186833acbba
hailina <49583466+hailina@users.noreply.github.com>
2019-08-16 11:48:00 +0800

    自学训练营5群d8 (#5464)
    * day7
    
    * d7
    
    * Update stats_word.py
    
    * Update stats_word.py
    
    * Update stats_word.py
    
    * Update stats_word.py
    
    * Update main.py
    
    * d7
    
    * Update stats_word.py
    
    * d7
    
    * Update stats_word.py
    
    * d8
    
    * Update stats_word.py
    
    * 1901030029自学训练营5群d8
    
    * Update stats_word.py
    
    * 自学训练营5群d8

--- exercises/1901030029/d8/mymodule/stats_word.py
+++ exercises/1901030029/d8/mymodule/stats_word.py
@@ -20,13 +20,15 @@ Namespaces are one honking great idea -- let's do more of those!
 去除英文单词中的标点符号
 '''
 import re
-fuhao=",.!-*，。&"                         #去除英文单词中的标点符号
+fuhao=",.!-*，。&"                         #去除英文单词中
 for a in fuhao:
     text=text.replace(a,'')
-print()
+print(text)
 
 
 def stats_text_en(text):                   #定义一个以字符串text为参数的函数
+    if not isinstance (text,str):
+        raise ValueError ('字符串必须是str类型，输入类型  %s' %type(text))
     result=re.sub("[^A-Za-z]"," ",text.strip())#统计单词次数
     dic={}                                 #使用字典统计text中的每个英文单词出现的次数
     for x in result.split():
@@ -43,11 +45,13 @@ print(sorted(frequency.items(),key=lambda x:x[1],reverse=True))
 
  #创建一个名为stas_text_cn的函数 
 def stats_text_cn(text):
-     dictionary={}                         #引用一个新字典
-     for i in text:
-         if u'\u4e00' <= i <=u'\u9fa5':    #提取中文汉字 \u是unincode的编码，u4e00是十进制表达式
+    if not isinstance(text,str):
+        raise ValueError('字符串必须是str类型，输入类型  %s' %type(text))
+    dictionary={}                         #引用一个新字典
+    for i in text:
+        if u'\u4e00' <= i <=u'\u9fa5':    #提取中文汉字 \u是unincode的编码，u4e00是十进制表达式
              dictionary[i]=text.count(i)
-     return dictionary
+    return dictionary
 frequency = stats_text_cn(text)
 print('**************************')
 print("按照出现次数从大到小输出所有的汉字以及出现的次数")
@@ -56,6 +60,8 @@ print(sorted(frequency.items(),key=lambda x: x[1],reverse=True))
 
 def stats_text(text):
     '''分别调⽤stats_text_en , stats_text_cn ，输出合并词频统计结果'''
+    if not isinstance(text,str):
+        raise ValueError('字符串必须是str类型，输入类型  %s' %type(text))
 
     return  stats_text_en(text).update(stats_text_cn(text))
     # print(result)
