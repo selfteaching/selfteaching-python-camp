2da1c8ef604892cf25dff5053661b079c54cb92d
scarlettzha <scarlettzha@gmail.com>
2019-08-16 16:53:02 +0800

    Update 1001S02E05_array.py

--- exercises/1901100243/1001S02E05_array.py
+++ exercises/1901100243/1001S02E05_array.py
@@ -1,30 +1,26 @@
-#将数组[0,1,2,3,4,5,6,7,8,9]翻转,翻转后的数组拼接成字符串
-a=[0,1,2,3,4,5,6,7,8,9]
+a = [0,1,2,3,4,5,6,7,8,9]
+print(list(reversed(a)))
+#print输出 a = [9, 8, 7, 6, 5, 4, 3, 2, 1, 0]
 
-a.reverse()
-print(a)
 
-a1=[str(i) for i in a]
+a1 = ''.join(str(i) for i in a)
 print(a1)
-a2=" ".join(a1)
+#print输出 a1 = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+
+a2 = a1[3:9]
 print(a2)
+#print输出 a2 = ['3', '4', '5', '6', '7', '8']
 
-#用字符串串切⽚片的⽅方式取出第三到第⼋八个字符(包含第三和第⼋八个字符)
-a3=a[3:9]
+a3 = list(reversed(a2))
 print(a3)
+#print输出 a3 = ['8', '7', '6', '5', '4', '3']
 
-#将获得的字符串串进⾏行行翻转
-a4 = list(reversed(a3))
+a3 = map(eval,a3)
+a4 = list(a3)
 print(a4)
 
-#将结果转换为int类型
-
-
-#分别转换成⼆二进制，⼋八进制，⼗十六进制 8. 最后输出三种进制的结果"""
-
-a6=[bin(i) for i in a4]
-a7=[oct(i) for i in a4]
-a8=[hex(i) for i in a4]
-print(a6)
-print(a7)
-print(a8)
+a5 = int(a2)
+print(a5)
+print(bin(a5))
+print(oct(a5))
+print(hex(a5))
