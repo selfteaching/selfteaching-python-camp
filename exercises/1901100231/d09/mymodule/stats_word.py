bdec8e6d08de01979859efcd8c30ca62337328e2
CHJ219 <53363031+CHJ219@users.noreply.github.com>
2019-08-20 05:29:36 +0800

    Revert "Revert "day 10""
    This reverts commit a94e4740426cfe609b77d71f6b4e3f607f481c29.

--- exercises/1901100231/d09/mymodule/stats_word.py
+++ exercises/1901100231/d09/mymodule/stats_word.py
@@ -1,5 +1,6 @@
 import re#运用正则表达式来去除符号和非目标文字
 import collections#运用collections模块进行词频统计
+import jieba
 def exam(x):#定义参数类型检查函数，若非字符串类型则抛出ValueError类型错误并提示错误类型
   if not isinstance(x,str): 
     raise ValueError("您输入的不是字符，请重新尝试，输入字符类型的参数。")
@@ -26,7 +27,7 @@ def stats_text_cn(x,count) :#定义汉字字频统计函数
    count = int(count)#设定int类型变量count以限制输出元素个数
    x = re.sub(u"[^\u4e00-\u9fa5]"," ",x)#去除英文和其他非中文的特殊符号
    x = x . replace("\n"," ")#去除换行符
-   x = x . replace(""," ")#由于汉字输入字和字之间无间隔会导致所有汉字文本共同算作列表中的一个元素，所以人为添加空格将汉字分隔开以便于统计。其余的套用英文文本处理的程序
+   x = jieba.cut(x,cut_all=False)
    copy = x.split(' ')#将文本转换为列表
    copy = [i for i in copy if i != ""]#去除之前人为添加的空格，避免影响统计结果
    a = {}#创建新字典
@@ -39,4 +40,4 @@ def stats_text_cn(x,count) :#定义汉字字频统计函数
    return(b)#返回值用于合并统计结果
 def stats_text(x,count) :#定义混合词频统计函数（引用的函数中已有参数类型检查步骤，故不再添加参数类型检查步骤）
   a = stats_text_cn(x,count)+stats_text_en(x,count)#合并两词频统计结果
-  print(a)#输出合并词频统计结果
+  return(a)#返回统计值
