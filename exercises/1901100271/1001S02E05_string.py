2b18ad1a9f179365af1644b3c16272240937cd0c
noodleCYF <53895938+noodleCYF@users.noreply.github.com>
2019-08-20 17:53:31 +0800

    Update 1001S02E05_string.py

--- exercises/1901100271/1001S02E05_string.py
+++ exercises/1901100271/1001S02E05_string.py
@@ -5,6 +5,7 @@ The zen of Python, by Tim Peters
 Beautiful is better than ugly.
 Explicit is better than implicit.
 Simple is better than complex.
+Complex is better than complicated.
 Flat is better than nested.
 Sparse is better than dense.
 Readability counts.
