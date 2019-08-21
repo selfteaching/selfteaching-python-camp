a7a2592820fe3d457290826c9e4be7c11c32e1d1
noodle <53895938+noodleCYF@users.noreply.github.com>
2019-08-21 09:14:45 +0800

    【1901100271】自学训练营学习19群 Day5 (#5580)
    * Create 1001S02E01_helloworld.txt
    
    * Create README.md
    
    * Create 1001S02E01_hello_python.py
    
    * Create 1001S02E03_calculator.py
    
    * Create 1001S02E04_control_flow.py
    
    * Update 1001S02E04_control_flow.py
    
    * Create 1001S02E05_string.py
    
    * Update 1001S02E05_string.py
    
    * Update 1001S02E05_string.py
    
    * Update 1001S02E05_string.py
    
    * Update 1001S02E05_string.py
    
    * Create 1001S02E05_stats_text.py
    
    * update
    
    * Update 1001S02E05_array.py

--- exercises/1901100271/1001S02E04_control_flow.py
+++ exercises/1901100271/1001S02E04_control_flow.py
@@ -3,8 +3,11 @@ for i in range (1,10):
         print (i,"*",j, "=", i*j, end='\t')
         if i == j:
             print ('\n')
-            i = i + 1
+            i = i + 1            
+            break #break语句在这里是跳出for j的循环
+
             break
+
         else:
             j = j + 1
 pass
@@ -13,6 +16,12 @@ print ("next work\n")
 i = 1
 while (i < 10):
     j = 1
+
+    print (i,"*",j, "=", i*j, end='\t')
+    while (j < i):
+        j += 1
+        print (i,"*",j, "=", i*j, end='\t')
+
     if i % 2 == 0:
         break
     else:
@@ -20,7 +29,7 @@ while (i < 10):
         while (j < i):
             j += 1
             print (i,"*",j, "=", i*j, end='\t')
-            
+
     print ('\n')
     i += 2
 
