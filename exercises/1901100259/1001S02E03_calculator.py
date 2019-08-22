a40d2fcee766fee0d4b96f9b3dd91bc8a8652d46
longfei-tu <53812206+longfei-tu@users.noreply.github.com>
2019-08-22 16:41:23 +0800

    Revert "Create 1001S02E03_calculator.py"
    This reverts commit 526bc670cb971675dc1b17cfe9a06458f15acf30.

--- exercises/1901100259/1001S02E03_calculator.py
+++ exercises/1901100259/1001S02E03_calculator.py
@@ -1,10 +1,33 @@
-a,b=[4,3]
-A=a+b
-B=a-b
-C=a*b
-D=a/b
-E=a//b
-F=a%b
-G=a**b
-W=[A,B,C,D,E,F,G]
-print(W)
+operator=input("输出第一个字符(+、-、*、/、//、%、**)：")
+number_1=input("输入第1个数字：")
+number_2=input("输入第2个数字：")
+a=int(number_1)
+b=int(number_2)
+#print(operator,type(operator),)
+#print(number_1,type(number_1),type(a))
+#print(number_2,type(number_2),type(b))
+#print(a*b)
+           
+if operator =="+":
+    print(a,"+",b,"=",a+b)
+
+elif operator =="-":
+    print(a,"-",b,"=",a-b)
+
+elif operator =="*": 
+    print(a,"*",b,"=",a*b)
+
+elif operator =="/":
+    print(a,"/",b,"=",a/b)
+
+elif operator =="//":
+    print(a,"//",b,"=",a//b)
+
+elif operator =="%":
+    print(a,"%",b,"=",a%b)
+
+elif operator =="**":
+    print(a,"**",b,"=",a**b)
+
+else:
+     print("error")   
