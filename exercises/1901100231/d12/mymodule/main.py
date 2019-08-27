f377ac65fdee5aa80aa25f90aac6cfa16f95a5d5
CHJ219 <53363031+CHJ219@users.noreply.github.com>
2019-08-27 10:30:47 +0800

    Update main.py

--- exercises/1901100231/d12/mymodule/main.py
+++ exercises/1901100231/d12/mymodule/main.py
@@ -8,10 +8,10 @@ def get_it(msg) :#定义获取网页内容的函数
     document = PyQuery(response.text)
     return document('#js_content').text()
 bot = Bot()#登录账号
-friend = bot.friends#获取所有好友
+friend = bot.friends()#获取所有好友
 @bot.register(friend,SHARING)#检测所有好友发来消息中的sharing类型的消息
 def statistics(msg):#以捕获的网页连接为参数，获取其中内容的词频统计结果
-    content = get_it(msg)#获取网页内容
+    content = get_it(msg.url)#获取网页内容
     result = stats_word.stats_text_cn(content,100)#对网页内容进行词频统计
     msg.reply(str(result))
 embed()
