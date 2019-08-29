dd0f3d02f20c61efd1376d6e92f1d0b27e113ea3
Ningziyun <49535971+Ningziyun@users.noreply.github.com>
2019-08-29 15:11:56 +0800

    d11
    day11

--- exercises/1901100240/d11/mymodule/main.py
+++ exercises/1901100240/d11/mymodule/main.py
@@ -1,25 +1,28 @@
+# Import the 微信文章 from the webside
 import requests
-from pyquery import PyQuery
-import stats_word
-import getpass
-import yagmail
-
 response=requests.get('https://mp.weixin.qq.com/s/pLmuGoc4bZrMNl7MSoWgiA')
+
+# Transer the 微信文章 to string type
+from pyquery import PyQuery
 document = PyQuery(response.text) 
 content = document('#js_content').text()
+
+# Make the satistic of the frequency of the word and output as a string
+import stats_word
 stats = stats_word.stats_text_cn(content,100)
-string=str('')
+string=str('')      # Create a string to store the result
 for unit in stats:
+    # Control the spacing by adding two more space it the word is less than two
     if len(unit[0]) <= 2:
         string += "'" + unit[0] + "'的频率是:    " + str(unit[1]) + "\n"
     elif len(unit[0]) == 3:
         string += "'" + unit[0] + "'的频率是:  " + str(unit[1]) + "\n"
 
-sender = 'yanning931231@sina.com'
-password = '7c50817e1ffff142'
-recipients = '604332298@qq.com'
-print(type(sender))
-yag = yagmail.SMTP(sender,password)
-print(type(password))
-yag.send(to=recipients,subject='[你好]',contents=string)
-print('done')
+# Send the result to the email
+import yagmail
+import getpass
+sender = input('输⼊入发件⼈人邮箱:')
+password = getpass.getpass('输⼊入发件⼈人邮箱密码(可复制粘贴):')       # The password typed will be invisible but will still work
+recipients = input('输⼊入收件⼈人邮箱:')
+yag=yagmail.SMTP(sender,password,host='smtp.sina.com')
+yag.send(to=recipients,subject='自学训练营学习19群+Ningziyun',contents=string)
