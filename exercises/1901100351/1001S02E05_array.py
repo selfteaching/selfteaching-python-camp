6415f6100c3b16d8789483d070af4701b8ec5b9b
Gong Rong Ting <rtgong@163.com>
2019-08-31 20:50:05 +0800

    第5次作业：学习数据容器
    完成3项任务，即字符串处理，字符串中的统计，数组操作

---
+++ exercises/1901100351/1001S02E05_array.py
@@ -0,0 +1,15 @@
+array=[0,1,2,3,4,5,6,7,8,9]     #输入数组
+string=''                       #初始化字符串
+array=sorted(array,reverse=True)#翻转字符串，reverse=true为降序
+print(array)
+
+for i in range(0,len(array)):   #循环查找
+    string=string+str(array[i]) #拼接字符串
+print(array)
+
+string=string[2:8]              #切片取出元素
+string=string[::-1]             #字符串翻转
+string=int(string)              #将字符串转为整数
+print(string)
+
+print(bin(string),'\n',oct(string),'\n',hex(string))    #二进制、八进制、十六进制 分别为bin\oct\hex
---
+++ exercises/1901100351/1001S02E05_stats_text.py
@@ -0,0 +1,41 @@
+text = '''
+the Zen of Python, by Tim Peters
+
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+'''
+
+t1=text.replace(',',' ').replace('.',' ').replace('--',' ').replace('!',' ').replace('*',' ').replace('!',' ')
+# 去掉文本中的中文字符、英文字符、字号、连接符
+text=text.split()
+
+# 统计各个单词出现的次数
+textDict={}#创建名称为textDict的字典
+for word in text:#在text中循环查找word
+    wordCount=text.count(word)#在text中查找word的次数定义为wordCount
+    wordDict={word:wordCount}#创建wordCount字典,并定义word与word出现次数为一一对应的（键，值）
+    textDict.update(wordDict)#将wordDict字典的“键值”更新到textDict字典中
+
+# 按照出现次数从大到小排序
+sortT1=sorted(textDict.items(),key=lambda x:x[1],reverse=True)
+# sorted()表示排序，items()表示以列表返回可遍历的(键, 值) 元组数组
+# 因为字典中基本元素是（键，值），即一个元组，所以要用key=lambda排序
+# lambda是固定写法的隐函数，x:x[1]表示以元组中的第2个元素为对象，reverse=True表示按降序排列
+print(sortT1) 
