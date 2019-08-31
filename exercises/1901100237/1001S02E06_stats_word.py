51975819e7bebd45101fecf3f63bd45de15edca0
liyiming12 <53564065+liyiming12@users.noreply.github.com>
2019-08-31 12:19:14 +0800

    Update 1001S02E06_stats_word.py

--- exercises/1901100237/1001S02E06_stats_word.py
+++ exercises/1901100237/1001S02E06_stats_word.py
@@ -11,8 +11,8 @@ def stats_text_en(text):
         z = set(aa)
     for v in z:
         d[v] = aa.count(v)
-    print('输出结果 ==>', sorted(d.items(), key=lambda x: x[1], reverse=True))
-    return
+    # print('输出结果 ==>', sorted(d.items(), key=lambda x: x[1], reverse=True))
+    return sorted(d.items(), key=lambda x: x[1], reverse=True)
     #将文本text中英文单词出现次数统计并按词频降序输出
 from html.parser import HTMLParser
 def stats_text_cn(text):
@@ -25,6 +25,50 @@ def stats_text_cn(text):
         z = set(aa)
     for v in z:
         d[v] = aa.count(v)
-    print('输出结果 ==>', sorted(d.items(), key=lambda x: x[1], reverse=True))
-    return
-    #将文本text中汉字出现次数统计并按词频降序输出
+    # print('输出结果 ==>', sorted(d.items(), key=lambda x: x[1], reverse=True))
+    return sorted(d.items(), key=lambda x: x[1], reverse=True)
+    
+text_en = """
+    The Zen of Python, by Tim Peters
+    Beautiful is better than ugly.
+    Explicit is better than implicit.
+    Simple is better than complex.
+    Complex is better than complicated.
+    Flat is better than nested.
+    Sparse is better than dense.
+    Readability counts.
+    Special cases aren't special enough to break the rules.
+    Although practicality beats purity.
+    Errors should never pass silently.
+    Unless explicitly silenced.
+    In the face of ambxiguity, refuse the temptation to guess.
+    There should be one-- and preferably only one --obvious way to do
+    it.
+    Although that way may not be obvious at first unless you're Dutch.
+    Now is better than never.
+    Although never is often better than *right* now.
+    If the implementation is hard to explain, it's a bad idea.
+    If the implementation is easy to explain, it may be a good idea.
+    Namespaces are one honking great idea -- let's do more of those!
+    """
+
+text_cn = '''
+    python之禅 by Tim Perers
+    优美胜于丑陋
+    明了胜于晦涩
+    简洁胜于复杂
+    复杂胜于凌乱
+    扁平胜于嵌套
+    间隔胜于紧凑
+    可读性很重要
+    即使假借特例的实用性之名，也不可违背这些规则
+    不要包容所有错误，除非你确定需要这样做
+    当存在多种可能，不要尝试去猜测
+    而是尽量找一种，最好是唯一一种明显的解决方案
+    显然这并不容易，因为你不是 Python 之父
+    做也许好过不做，但不假思索就动手还不如不做
+    。。。
+    '''
+
+print('输出结果cn ==>',stats_text_cn(text_cn))
+print('输出结果en ==>',stats_text_cn(text_en))
