4e19deb39bbed04a9756b44a873c2f024c505e9e
superjmm <673224116@qq.com>
2019-09-01 21:00:01 +0800

    1901100296 自学训练营学习20群 DAY4
    同时完善了DAY3的程序

--- exercises/1901100296/1001S02E03_calculator.py
+++ exercises/1901100296/1001S02E03_calculator.py
@@ -1,9 +1,35 @@
+'''
+计算器
+'''
+'''
+方法一：
+输入公式如：9+8
+回车后出来结果
+'''
+'''
 x=0
 while x<1:
     try:
-        value=input("请输入计算：")
+        value=input("请输入运算公式，如：9+8：")
         print("计算结果是：",eval(value))
     except (BaseException):
         x=2
         print("输入有误！")
-    
+'''  
+'''
+方法二：
+按运算顺序输入
+'''
+n1=input('第一个数字：')
+s=input("选择运算符(+、-、*、/)：")
+n2=input('第二个数字：')
+if s=='+':
+    print('运算结果：',n1,s,n2,'=',int(n1)+int(n2))
+elif s=='-':
+    print('运算结果：',n1,s,n2,'=',int(n1)-int(n2))
+elif s=='*':
+    print('运算结果：',n1,s,n2,'=',int(n1)*int(n2))
+elif s=='/':
+    print('运算结果：',n1,s,n2,'=',int(n1)/int(n2))
+else:
+    print("输入有误！")
