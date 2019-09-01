3bcebe970215463d9fccdddb1d696be80307d087
skyli007 <li37662275@gmail.com>
2019-09-01 18:53:42 +0800

    add judged
    在输入运算符处增加条件判断

--- exercises/1901100368/1001S02E03_calculator.py
+++ exercises/1901100368/1001S02E03_calculator.py
@@ -1,16 +1,18 @@
 #使⽤用 Python 编写一个支持 加、减、乘、除功能 的计算器，⽀持输⼊参数，⽀持输出结果
-
-print("这是一个仅支持两个参数的计算器\n，请输入第一个参数！\n")
+print("这是一个仅支持两个参数的计算器\n 请输入第一个参数！\n")
 
 parment_a = input()
 
-print("请选择输入你的运算符：1为+,2为-,3为*,4为/")
+print("请选择输入你的运算符：1为+,2为-,3为*,4为/ \n")
 
 code_input = input()
+
+code_index = 0
 
 print("请输入第二个参数！\n")
 
 parment_b = input()
+
 
 
 if int(code_input) == 1:
@@ -19,7 +21,12 @@ elif int(code_input) == 2:
     result = int(parment_a) - int(parment_b)
 elif int(code_input) == 3:
     result = int(parment_a) * int(parment_b)
-else:
+elif int(code_input) == 4:
     result = int(parment_a) / int(parment_b)
+else:
+    code_index = 1
 
-print("结果是：" + str(result))
+if code_index == 0:
+    print("结果是：" + str(result))
+else :
+    print("运算符输入参数必须为1，2，3，4中的任意一位! \n")
