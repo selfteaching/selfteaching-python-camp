4dff7a4897a565e539290af7c6472bf110450f08
Bear127 <48996947+Bear127@users.noreply.github.com>
2019-09-02 14:52:45 +0800

    自学训练营学习2群Day3 (#5822)
    * Create 1001S02E02_hello_python.py.py
    
    * Create 1001S02E03_calculator.py

---
+++ exercises/1901010072/1001S02E03_calculator.py
@@ -0,0 +1,21 @@
+def add(a: object, b: object) -> object:
+    """
+
+    :rtype: object
+    """
+    return a + b
+
+a=3
+b=4
+print(add(a,b))
+
+def subtract(c, d):
+    return c - d
+
+
+def multiply(e, f):
+    return e * f
+
+
+def divide(g, h):
+    return g / h
