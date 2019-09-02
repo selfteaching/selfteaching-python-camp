25b6b853a035fe59b17eb6f79e2cb6b11fe42576
Kyra-zhang <yu2495@qq.com>
2019-09-02 19:49:57 +0800

    Update 1001S02E03_calculator.py

--- exercises/1901050073/1001S02E03_calculator.py
+++ exercises/1901050073/1001S02E03_calculator.py
@@ -1,41 +1,41 @@
 # 定义函数
 def add(x,y):
-    "”“相加”“”
+    """加"""
     return x + y
 
 def subtract(x,y):
-    """相减"""
+    """减"""
     return x - y
 
 def multiply(x,y):
-    """相乘"""
+    """乘"""
     return x * y
 
 def divide(x,y):
-    """相除"""
+    """除"""
     return x / y
 
 # 用户输入
-peint("选择运算:")
-print("A、相加")
-print("B、相减")
-print("C、相乘")
-print("D、相除")
+print("选择运算:")
+print("1.加")
+print("2.减")
+print("3.乘")3
+print("4.除")
 
-choice = input("请选择（A/B/C/D):")
-num1 = int(input("输入第一个数字"))
-num2 = int(input("输入第二个数字"))
+choice = input("choice（1/2/3/4):")
+num1 = int(input("num1:"))
+num2 = int(input("num2:"))
 
-if choice == 'A':
+if choice == '1':
     print(num1,"+",num2,"=",add(num1,num2))
 
-elif choice == 'B':
+elif choice == '2':
     print(num1,"-",num2,"=",subtract(num1,num2))
 
-elif choice == 'C':
+elif choice == '3':
     print(num1,"*",num2,"=",multiply(num1,num2))
 
-elif choice == 'D':
-    print(num1,"/",num2,"="divide)(num1,num2))
+elif choice == '4':
+    print(num1,"/",num2,"=",divide(num1,num2))
 else:
-    print("无法计算")
+    print(输入错误)
