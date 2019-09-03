27b661dbfdb3df639e3bb0d11c76a1cad1beab9a
superjmm <673224116@qq.com>
2019-09-04 07:53:32 +0800

    Update 1001S02E04_control_flow.py
    完善了DAY4的程序

--- exercises/1901100296/1001S02E04_control_flow.py
+++ exercises/1901100296/1001S02E04_control_flow.py
@@ -1,21 +1,24 @@
 '''
 九九乘法表
 '''
-'''
-n=1
-while n<10:
-    for i in range(1,n+1):
-        print(n,"*",i,"=",n*i,end='\t')
-    n=n+1
-    print('')
-'''
+print("九九乘法表:")
+
+for i in range(1,10):
+    for j in range(1,i+1):
+        print(i,"*",j,"=",i*j,end='\t')
+    print()
+    i+=1
+
 '''
 九九乘法表，将偶数行去掉
 '''
-n=1
-while n<10:
-    for i in range(1,n+1):
-        if  n%2!=0:
-            print(n,"*",i,"=",n*i,end='\t')
-    n=n+1
-    print('')
+print("删除偶数行的九九乘法表:")
+i=1
+while i<10:
+    for j in range(1,i+1):
+        if i%2!=0:
+            print(i,"*",j,"=",i*j,end='\t') 
+    if i%2!=0:
+        print() 
+    i=i+1
+
