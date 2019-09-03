21aa31d03c2e2c39c8b3b9574b7391a83880b5b3
zhaodikang <572208112@qq.com>
2019-09-03 16:24:21 +0800

    Update main.py

--- exercises/1901050069/D09/main.py
+++ exercises/1901050069/D09/main.py
@@ -7,22 +7,39 @@ import logging
 logging.basicConfig(format = 'file:%(filename)s|line:%(lineno)d|message:%(message)s',level = logging.DEBUG)
 
 def load_file():
-    
-def test_traceback():
+    '''
+    1.
+    下面是常用的获取文件路径的方式，要确保 tang300.json 跟当前文件在同一个文件夹下，这两种方式等价
+    file_path = path.join(path.dirname(path.abspath(__file__)),'./tang300.json')
+    '''
+    file_path = path.join(path.dirname(path.abspath(__file__)),'tang300.json')
+    print('当前文件路径:',__file__,'\n读取文件路径',file_path)
+    '''
+    2.
+    这种方式表示 tang300.json 在当前文件的上一级目录
+    file_path = path.join(path.dirname(path.adspath(__file__)),'../tang300.json')
+    print(__file__, file_path)
+    3.
+    这种方式表示 tang300.json 在当前文件的上一级目录的 data 文件夹下
+    file_path = path.join(path.dirname(path.adspath(__file__)),'../data/tang300.json')
+    print(__file__,file_path)
+    '''
+    with open(file_path, 'r',encoding='utf-8') as f:
+        return f.read()
+
+def merge_poems(data):
+    poems = ''
+    for item in data:
+        poems += item.get('contents','')
+    return poems
+def main():
     try:
-        stats_word.stats_text(1)
+        data = load_file()
+        logging.info(data[0])
+        poems = merge_poems(json.loads(data))
+        logging.info('result ==> %s', stats_word.stats_text_cn(poems,300))
     except Exception as e:
-        print('test_traceback =>',e)
-        print(traceback.format_exc())
-        
-def test_logger():
-    try:
-        stats_word.stats_text(1)
-    except Exception as e:
-        print('test_traceback =>' , e)
-        logger.exception(e)
+        logging.exception(e)
 
 if __name__ == "__main__":
-    stats_word.stats_text(1)
-    test_traceback()
-    test_logger
+    main()
