85fda72e41851e55a281a7ce6fb3d89f0df4295f
Bear127 <48996947+Bear127@users.noreply.github.com>
2019-09-07 00:07:48 +0800

    Update 1001S02E04_control_flow.py

--- exercises/1901010072/1001S02E04_control_flow.py
+++ exercises/1901010072/1001S02E04_control_flow.py
@@ -1,23 +1,15 @@
-# for in循环
- for a in range(1,10):
-   for b in range(1,10):
-        print(a,"X",b,"=",a*b,"\t",end="")
-        if a == b:
-           print("")
-           break
+print('打印九九乘法表')
+for i in range（1,10):
+  for j in range (1,i+1):
+    print(i,'*',j,'=',i*j,end='\t')
+  print()
 
-i=0
-j=0
-while i<9:
-    i+=1
-    if condition: i % 2 == 0:
-      pass
-    while j<9:
-        j+=1
-        print(j,"x",i,"=",i*j,"\t",end="")
-        if i==j:
-            j=0
-            print("")
-            break
-    else: print("")
-            break
+print('打印跳过偶数行的九九乘法表')
+i = 1
+while i<10:
+  if i%2 ==0:
+    print()
+  else:
+    for j in range(1,i+1):
+      print(i,'*',j,'=',i*j,end='\t')
+  i+=1
