e9b759bb7637f174553cd86d0208b10903fcf0d4
Wang yulin <aaron.wangyulin@gmail.com>
2019-09-07 11:27:43 +0800

    Counter

--- exercises/1901100078/d09/main.py
+++ exercises/1901100078/d09/main.py
@@ -1,4 +1,6 @@
-text = 123
+with open ('tang300.json') as f:
+    text = f.read()
+
 
 from mymodule import stats_word
 import traceback
@@ -6,16 +8,16 @@ import logging
 logger = logging.getLogger(__name__)
 
 try:
-    print('中英文字频====>', stats_word.stats_text(text))
+    print('中文字频====>', stats_word.stats_text_cn(text, 100))
 except ValueError as err:
     print("print out the error: ", err)
     print(traceback.format_exc())
 
-def test_logger():
+'''def test_logger():
     try:
         stats_word.stats_text(1)
     except Exception as e:
         logger.exception(e)
 
 if __name__ == '__main__':
-    test_logger()
+    test_logger()'''
