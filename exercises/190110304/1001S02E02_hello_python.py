35a79e60b39fe017ed20d35716763362dea0daa1
xing-cheng <54171952+xing-cheng@users.noreply.github.com>
2019-09-19 19:08:41 +0800

    Update 1001S02E02_hello_python.py

--- exercises/190110304/1001S02E02_hello_python.py
+++ exercises/190110304/1001S02E02_hello_python.py
@@ -1,1 +1,11 @@
-print("hello world")
+i = 1
+while i < 10:
+       j = 1
+       while j < i + 1:
+       	      if i%2 == 0:
+                    break
+              print(j,  '*', i, '=', i * j, sep='', end='\t')
+              j += 1
+
+       print()
+       i += 1
