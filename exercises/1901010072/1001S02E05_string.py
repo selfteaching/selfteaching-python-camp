32384e4cccad277700cd742eaeb4d2f09a9f494a
Bear127 <48996947+Bear127@users.noreply.github.com>
2019-09-28 22:14:18 +0800

    Create 1001S02E05_string.py

---
+++ exercises/1901010072/1001S02E05_string.py
@@ -0,0 +1,43 @@
+text = '''
+The Zen of Python, by Tim Peters
+Beautiful is better than ugly.
+Explicit is better than implicit.
+Simple is better than complex.
+Complex is better than complicated.
+Flat is better than nested.
+Sparse is better than dense.
+Readability counts.
+Special cases aren't special enough to break the rules.
+Although practicality beats purity.
+Errors should never pass silently.
+Unless explicitly silenced.
+In the face of ambxiguity, refuse the temptation to guess.
+There should be one-- and preferably only one --obvious way to do
+it.
+Although that way may not be obvious at first unless you're Dutch.
+Now is better than never.
+Although never is often better than *right* now.
+If the implementation is hard to explain, it's a bad idea.
+If the implementation is easy to explain, it may be a good idea.
+Namespaces are one honking great idea -- let's do more of those!
+'''
+#第一步将字符串样本text里的better全部替换成worse
+s=text
+s.replace('better', 'worse', )
+print(s.replace('better', 'worse', ))
+
+#第二步将单词中包含ea的单词剔除
+s.split()
+L = list(s)
+for C in L:
+    if 'ea' in 'C',
+    del L[C]
+    print(L_list)
+else:
+    print(L_list)
+
+#第三步将字⺟进行⼤小写翻转
+s.swapcase()
+
+#第四步将单词升序排列
+L_list.sort()
