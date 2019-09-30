1c2b66c86696218cc2601c2ce1ca9024e463e91c
xuefeng365 <120158568@qq.com>
2019-09-30 23:51:15 +0800

    Day 10

--- exercises/1901100277/Day 09/main.py
+++ exercises/1901100277/Day 09/main.py
@@ -1,5 +1,5 @@
-from mymodule import stats_word
-import json
+from mymodule import stats_word  
+import json   # 读取本地文件模块
 
 
 # string = 123456789
@@ -14,7 +14,7 @@ import json
 with open(r'E:\xuefeng\selfteaching-python-camp\exercises\1901100277\Day 09\mymodule\tang300.json', 'r',encoding='utf-8') as f:
     #  https://www.liaoxuefeng.com/wiki/1016959663602400/1017607179232640  廖雪峰:文件的读写
 
-    cn_text = f.read()  # 读取文件全部内容
+    cn_text = f.read()  # 读取文件全部内容,如果文件比较大,会浪费很多系统资源,更优的选择是 读取部分文件.模块有介绍
     
 
 print(stats_word.stats_text_cn(cn_text))
--- exercises/1901100277/Day 09/mymodule/stats_word.py
+++ exercises/1901100277/Day 09/mymodule/stats_word.py
@@ -69,7 +69,8 @@ def stats_text(string):
 
     return stats_text_en(en_text) + stats_text_cn(cn_text)
 
-
+en_text = ""
+cn_text = ""
 
 
 
