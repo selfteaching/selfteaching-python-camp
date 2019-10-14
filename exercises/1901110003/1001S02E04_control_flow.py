d05ac5bc6bb4cd2bda7abcf147f9a6d6ba3cd742
Sally-luo <54762102+Sally-luo@users.noreply.github.com>
2019-10-14 10:21:55 +0800

    【21】⾃自学训练营 DAY4  (#6065)
    * Day 1 task
    
    * Create 1001S02E02_hello_python.py
    
    Day two task.
    
    * Create 1001S02E03_calculator.py
    
    design a calculator
    
    * Day04

--- exercises/1901110003/1001S02E04_control_flow.py
+++ exercises/1901110003/1001S02E04_control_flow.py
@@ -1,11 +1,6 @@
 #DAY4 控制流程 2019-08-04
 #Task1：使⽤用for...in循环打印九九乘法表
-for i in range (1,10):
-    for j in range (1,10):
-        print(i,"X",j,"=",i*j,"\t",end="")
-        if i == j:
-            print("")
-            break
+
 #Task2:使用while循环打印九九乘法表并⽤条件判断把偶数⾏去除掉
 i = 1
 while i < 10:  
