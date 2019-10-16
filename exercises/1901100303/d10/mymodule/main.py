019385b5785966e6be34f1c294d31968ee7f91af
python-yuanweiyu <180085799@qq.com>
2019-10-16 21:03:03 +0800

    [1901100303]day11

--- exercises/1901100303/d10/mymodule/main.py
+++ exercises/1901100303/d10/mymodule/main.py
@@ -1,2 +1,48 @@
-import stats_word
-stats_word.stats_text()
+import sys
+import os
+curPath = os.path.abspath(os.path.dirname(__file__))
+rootPath = os.path.split(curPath)[0]
+sys.path.append(rootPath)
+
+
+from mymodule import stats_word
+from os import path
+import json
+import re
+import logging
+
+
+
+logging.basicConfig(
+    format='file:%(filename)s|line:%(lineno)d|message:%(message)s',level=logging.DEBUG)
+
+def load_file():
+    '''
+    1.
+    下面是常用的获取文件路径的方式，要确保tang300.json 跟当前文件在同一文件夹下，这两种方式等价
+    file_path = path.join(path.dirname(path.abspath(__file__)),'./tang300.json')
+    '''
+    file_path = path.join(path.dirname(path.abspath(__file__)),'tang300.json')
+    print('当前文件路径：',__file__,'\n读取文件路径：',file_path)
+
+    with open(file_path,'r',encoding='utf-8') as f:
+        return f.read()
+
+
+def merge_poems(data):
+    poems = ''
+    for item in data:
+        poems += item.get('contents','')
+    return poems
+
+def main():
+    try:
+        data = load_file()  
+        logging.info(data[0])
+        poems = merge_poems(json.loads(data))
+        logging.info('result ==> %s',stats_word.stats_text_cn(poems,100))
+    except Exception as e:
+        logging.exception(e)
+
+#if __name__ == "__main()__":
+main()
