c25c1481b9d9044a5febec5d0849ae6576ace9ec
Bear127 <48996947+Bear127@users.noreply.github.com>
2019-10-16 21:47:27 +0800

    Update main.py.py

--- exercises/1901010072/d07/main.py.py
+++ exercises/1901010072/d07/main.py.py
@@ -1,4 +1,6 @@
-text = '''
+from mymodule import stats_word
+
+sample_text = '''
 愚公移⼭
 太行，王屋⼆山的北面，住了一個九十歲的⽼翁，名叫愚公。⼆山佔地廣闊，擋住去路路，使他
 和家⼈往來極為不便。
@@ -20,53 +22,26 @@ text = '''
 力神揹走⼆山。
 
 How The Foolish Old Man Moved Mountains
-Yugong was a ninety-year-old man who lived at the north of two high
-mountains, Mount Taixing and Mount Wangwu.
-Stretching over a wide expanse of land, the mountains blocked
-yugong’s way making it inconvenient for him and his family to get
-around.
-One day yugong gathered his family together and said,”Let’s do our
-best to level these two mountains. We shall open a road that leads
-to Yuzhou. What do you think?”
+Yugong was a ninety-year-old man who lived at the north of two high mountains, Mount Taixing and Mount Wangwu.
+Stretching over a wide expanse of land, the mountains blocked yugong’s way making it inconvenient for him and his family to get around.
+One day yugong gathered his family together and said,”Let’s do our best to level these two mountains. We shall open a road that leads to Yuzhou. What do you think?”
 All but his wife agreed with him.
-“You don’t have the strength to cut even a small mound,” muttered
-his wife. “How on earth do you suppose you can level Mount Taixin
+“You don’t have the strength to cut even a small mound,” muttered his wife. “How on earth do you suppose you can level Mount Taixin
 and Mount Wanwu? Moreover, where will all the earth and rubble go?”
 “Dump them into the Sea of Bohai!” said everyone.
-So Yugong, his sons, and his grandsons started to break up rocks and
-remove the earth. They transported the earth and rubble to the Sea
-of Bohai.
-Now Yugong’s neighbour was a widow who had an only child eight years
-old. Evening the young boy offered his help eagerly.
-Summer went by and winter came. It took Yugong and his crew a full
-year to travel back and forth once.
-On the bank of the Yellow River dwelled an old man much respected
-for his wisdom. When he saw their back-breaking labour, he ridiculed
-Yugong saying,”Aren’t you foolish, my friend? You are very old now,
-and with whatever remains of your waning strength, you won’t be able
-to remove even a corner of the mountain.”
-Yugong uttered a sigh and said,”A biased person like you will never
-understand. You can’t even compare with the widow’s little boy!”
-“Even if I were dead, there will still be my children, my
-grandchildren, my great grandchildren, my great great grandchildren.
-They descendants will go on forever. But these mountains will not
-grow any taler. We shall level them one day!” he declared with
-confidence.
+So Yugong, his sons, and his grandsons started to break up rocks and remove the earth. They transported the earth and rubble to the Sea of Bohai.
+Now Yugong’s neighbour was a widow who had an only child eight years old. Evening the young boy offered his help eagerly.
+Summer went by and winter came. It took Yugong and his crew a full year to travel back and forth once.
+On the bank of the Yellow River dwelled an old man much respected for his wisdom. When he saw their back-breaking labour, he ridiculed Yugong saying,”Aren’t you foolish, my friend? You are very old now,
+and with whatever remains of your waning strength, you won’t be able to remove even a corner of the mountain.”
+Yugong uttered a sigh and said,”A biased person like you will never understand. You can’t even compare with the widow’s little boy!”
+“Even if I were dead, there will still be my children, my grandchildren, my great grandchildren, my great great grandchildren.
+They descendants will go on forever. But these mountains will not grow any taler. We shall level them one day!” he declared with confidence.
 The wise old man was totally silenced.
-When the guardian gods of the mountains saw how determined Yugong
-and his crew were, they were struck with fear and reported the
-incident to the Emperor of Heavens.
-Filled with admiration for Yugong, the Emperor of Heavens ordered
-two mighty gods to carry the mountains away.
+When the guardian gods of the mountains saw how determined Yugong and his crew were, they were struck with fear and reported the incident to the Emperor of Heavens.
+Filled with admiration for Yugong, the Emperor of Heavens ordered two mighty gods to carry the mountains away.
 '''
 
-import stats_word
+result = stats_word.stats_text(sample_text)
 
-text = f.read()
-stats_word.stats_text(text)
-def stats_text(text):
-    if __name__=='__main__':
-        en_result=stats_text_en(text)
-        cn_result=stats_text_cn(text)
-        print('统计参数中各中英文单词出现的次数 ==>\n',en_result,cn_result)
- 
+print('统计结果==>',result)
