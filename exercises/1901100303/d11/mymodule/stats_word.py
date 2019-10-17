842372cedad141ef46448df4ef35161a144bee7f
python-yuanweiyu <54163416+python-yuanweiyu@users.noreply.github.com>
2019-10-17 08:50:16 +0800

    [1901100303]自学训练营学习20群 day11 (#6081)
    * my exercises
    
    exercises-yuanweiyu
    
    * Create 1001S02E02_hello_python.py
    
    * Update 1001S02E01_helloworld.txt
    
    * Create 1001S02E02_hello_python.py
    
    * Create 1001S02E02_hello_python.py
    
    * Delete 1001S02E02_hello_python.py
    
    * Delete 1001S02E02_hello_python.py
    
    * Create 1001S02E03_calculator.py
    
    * day4_exercise
    
    * Update 1001S02E04_control_flow.py
    
    * Update 1001S02E04_control_flow.py
    
    * Delete 0827.py
    
    * [1901100303] da5
    
    * day5-进制转换
    
    * day6-返回降序的统计词频数组函数
    
    * day6 -汉字按字频降序输出
    
    * study -test
    
    * day7
    
    * Create main.py
    
    * day8 try and except
    
    * day8
    
    * [1901100303]day10
    
    jieba分词练习
    
    * Delete test-file.txt
    
    * Delete PKG-INFO
    
    * [1901100303]day10作业修改
    
    * [1901100303]day11

--- exercises/1901100303/d11/mymodule/stats_word.py
+++ exercises/1901100303/d11/mymodule/stats_word.py
@@ -1,4 +1,3 @@
-
 from collections import Counter
 import jieba
 
@@ -35,7 +34,6 @@ def stas_text(text,count):
     if not isinstance(text,str):
         raise ValueError('参数必须是 str 类型，输入类型 %s' % type(text))
     return stats_text_cn(text,count) + stats_text_en(text,count)
-
 
 
 
