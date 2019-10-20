328bd2202ff9503f6fce980af23e371ac20f39b1
Gong Rong Ting <53923246+rtgong@users.noreply.github.com>
2019-10-20 20:47:52 +0800

    【1901100351】自学群20 DAY14 (#6091)
    * 创建了两个文件夹
    
    创建了两个文件夹
    
    * Day 02
    
    print 'hello world'
    
    * Day 02
    
    print 'hello world'
    
    * 1901100351【自学训练营20】day03
    
    I had wrot two calculators with python language.
    
    * Create 1001S02E04_control_flow.py
    
    01. 学习if、for…in、while、range等语法；
    02. 尝试理解control flow
    
    * 对control flow练习的修改
    
    根据教学视频，对control flow练习进行了优化；
    
    * 第5次作业：学习数据容器
    
    完成3项任务，即字符串处理，字符串中的统计，数组操作
    
    * 补充部分命令的注释
    
    复习之前学习的内容，修改若干书写，补充部分注释，相当于重读自己完成的代码
    
    * 第6次作业，学习自定义函数
    
    学习、练习自定义函数
    
    * DAY7: 学习函数调用
    
    在DAY6自定义函数的基础上，学习函数调用
    
    * 更新DAY5、DAY6作业
    
    听完视频课后，对作业作了一些修改和完善
    
    * Day 8 作业：异常处理
    
    考虑输入中的错误，找出并提示
    
    * 第8次作业修改
    
    根据视频资料，对第8次作业进行了修改
    
    * 第9次作业：学习使用标准库
    
    在第8次作业的基础上，学习使用标准库中的函数
    
    * 参照教学视频，对第9次作业进行了修改
    
    感觉视频中很多没有讲清楚，全部理解还需要一个过程
    
    * 完成自学训练营第10次作业
    
    学习使用第三方库，jieba分词
    
    * 对第10次作业进行少量修改
    
    根据教学视频，对第10次作业进行了少量修改，进一步理解具体环境下的应用
    
    * 第11次作业：实战训练
    
    在之前的基础学习利用第三方库解放具体问题
    
    * DAY 12 学习使用wxpy
    
    在阅读与搜索后，基本上完成代码，但运行时总是显示登陆报错。
    
    * 第12次作业的修改
    
    对照教学视频，重新完成第12次作业
    
    * 初步完成第13次作业
    
    引入第三方库，尝试将数据转化成图表
    
    * 修改完善第13次作业+自学总结
    
    对照教学视频对第13次作业进行修改，但还运行中还是卡住了：
    > AttributeError:module 'ntpath' has no attribute 'jion'

--- exercises/1901100351/d13/main.py
+++ exercises/1901100351/d13/main.py
@@ -1,6 +1,9 @@
-# this is d12 exercise for using wxpy
-# date: 2019.10.6
+
+# this is d13 exercise for using wxpy
+# date: 2019.10.19
 # author by: rtgong
+
+from os import path
 
 import logging
 import requests
@@ -11,6 +14,12 @@ import numpy as np
 from mymodule import stats_word
 from wxpy import *
 
+
+cwd = path.abspath(path.dirname(__file__))  # 获取当前工作目录
+plt.rcParams['font.sans-serif'] = 'SimHei'  # 设置中文字体
+
+
+
 logging.basicConfig(format='file:%(filename)s|line:%(lineno)s|message:%(message)s',level=logging.DEBUG)
 
 def get_article(url):
@@ -18,33 +27,44 @@ def get_article(url):
     document = pyquery.PyQuery(r.text)
     return document('#js_content').text()
 
+def generate_image(data,image_path):
+    labels = [v[0] for v in data]
+    widths = [v[1] for v in data]
+    ypos = range(len(data))
+    fig, ax = plt.subplots()
+    ax.barh(ypos,widths)
+    ax.set_yticks(ypos)
+    ax.set_yticklabels(labels)
+    ax.invert_yaxis()
+    ax.set_ylabels('关键字')
+    ax.set_xlabel('词频')
+    ax.set_title('The top 10 words in the article')
+    fig.savefig(image_path, bbox_inches = 'tight')
+
 def main():
     bot = Bot()
     friends = bot.friends()
     
     @bot.register(friends,SHARING)
-    def handler(chart):
+
+    def handler(msg):
         try:
-            logging.info('sharing url = %s', chart.url)
-            article = get_article(chart.url)
+            logging.info('sharing url = %s', msg.url)
+            article = get_article(msg.url)
             result = stats_word.stats_text_cn(article,10)
-            plt.rcdefaults()
-            fig, ax = plt.subplots()
-            words = [result]
-            y_pos = np.arange(len(words))
-            number = 3 + 10 * np.random.rand(len(words))
-            error = np.random.rand(len(words))
+            image_path = path.jion(cwd, 'stats.png')
+            generate_image(result, image_path)
+            msg.reply_image(image_path)
 
-            ax.barh(y_pos,number, xerr=error, align='center')
-            ax.set_yticks(y_pos)
-            ax.invert_yaxis()
-            ax.set_xlabel('Number')
-            ax.set_title('The top 10 words in the article')
-
-            chart.reply(plt.show(result))
         except Exception as e:
             logging.exception(e)
     embed
 
+def test():
+    article = get_article('https://mp.weixin.qq.com/s/plmuGoc4b2rMN17MSoWgiA')
+    result = stats_word.stats_text_cn(article,10)
+    image_path = path.jion(cwd, 'stats.png')
+    generate_image(result, image_path)
+
 if __name__=="__main__":
-    main()
+    test()
