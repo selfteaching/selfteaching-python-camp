49947eb99b36599a75e52d577b68a2bec0ec761b
lixifeng0926 <48723827+lixifeng0926@users.noreply.github.com>
2019-10-21 10:35:17 +0800

    Create 1001S02E04_control_flow.py
    第四天作业

---
+++ exercises/1901100295/1001S02E04_control_flow.py
@@ -0,0 +1,19 @@
+#使用for……in循环打印九九乘法表
+for i in range (1,10):
+    for j in range (1,i+1):
+        print("{}*{} = {:<4}".format(i,j,i*j),end = " ")
+    print("")
+
+
+#使用while循环打印九九乘法表并用条件判断把偶数行去除掉
+    i = 1
+while i < 10:
+    if i % 2 == 0:
+        i += 1
+        continue
+    j = 1
+    while j < i+1:
+        print(i,"*",j,"=",i*j," ",end="")
+        j += 1
+    print("",end="\n")
+    i += 1
