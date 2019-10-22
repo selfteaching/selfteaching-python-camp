d109e4ad0aecc8a5cb2db38195d28ddfcc61fd34
python-yuanweiyu <180085799@qq.com>
2019-10-22 16:31:37 +0800

    [1901100303]day13

--- exercises/1901100303/d11/mymodule/main.py
+++ exercises/1901100303/d11/mymodule/main.py
@@ -25,12 +25,24 @@ document = PyQuery(r.text)
 content = document('#js_content').text()
 
 wx_list = stats_word.stats_text_cn(content,100)
-wx_str =''
+ll_word = []
+ll_num  = []
+for ll in wx_list:
+    ll_word.append(ll[0])
+    ll_num.append(ll[1])
+    
+print(ll_word)
+print(ll_num)
+        
+        
+        
 
-for word in wx_list:
-    wx_str = wx_str + str(word)
+#wx_str =''
 
-yagmail.SMTP('sender','password').send('pythoncamp@163.com','[1901100303]自学训练营学习20群 day11 yuanweiyu',wx_str)
+#for word in wx_list:
+#   wx_str = wx_str + str(word)
+
+#yagmail.SMTP('sender','password').send('pythoncamp@163.com','[1901100303]自学训练营学习20群 day11 yuanweiyu',wx_str)
 
 
 
