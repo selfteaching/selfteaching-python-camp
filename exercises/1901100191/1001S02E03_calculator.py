65a17cc300ec1b71374fd42d5bb70baa55b5d887
xiaooyu <8507081@qq.com>
2019-11-11 19:59:05 +0800

    day3_1901100191

---
+++ exercises/1901100191/1001S02E03_calculator.py
@@ -0,0 +1,21 @@
+operator = input('请输入运算符（+、-、*、/):')
+first_number = input('请输入第一个数字：')
+second_number = input('请输入第二个数字：')
+
+a = int(first_number)
+b = int(second_number)
+
+print('operator:', operator, type(operator))
+print('first_number:', first_number, type(first_number))
+print('second_number:', second_number, type(second_number))
+
+if operator == '+':
+    print(a, '+', b, '=', a + b)
+elif operator == '-':
+    print(a, '-', b, '=', a - b)
+elif operator == '*':
+    print(a, '*', b, '=', a * b)
+elif operator == '/':
+    print(a, '/', b, '=', a / b)
+else:
+    print('无效运算符')
