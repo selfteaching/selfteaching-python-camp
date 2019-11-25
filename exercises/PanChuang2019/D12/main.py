79f366f0e16026c3f41649dfa55e34a53d65d91a
PanChuang2019 <Bill@xcsource,com>
2019-11-25 22:02:15 +0800

    Day 12 work

--- exercises/PanChuang2019/D12/main.py
+++ exercises/PanChuang2019/D12/main.py
@@ -1,34 +1,33 @@
-import yagmail
+import logging
 import requests
 import pyquery
-import getpass
-import logging
+from wxpy import *
 from mymodule import stats_word
 
 
 logging.basicConfig(
     format='file:%(filename)s[line:%(lineno)d]message:%(message)s',level=logging.DEBUG)
 
-def get_article():
-    r = requests.get('https://mp.weixin.qq.com/s/pLmuGoc4bZrMNl7MSoWgiA')
+def get_article(url):
+    r = requests.get(url)
     document = pyquery.PyQuery(r.text)
     return document('#js_content').text()
 
 
 def main():
-    try:
-        article = get_article()
-        result = stats_word.stats_text_cn(article,100)
-        logging.info('%s %s',type(result),str(result))
-        sender = input('请输入发件人邮箱地址：')
-        password = getpass.getpass('请输入发件人邮箱密码：')
-        recipents = input('请输入收件人邮箱地址：')
-        yag = yagmail.SMTP(sender,password,'smtp.qq.com')
-        yag.send(recipents,'自学训练营学习14群 Day11 PanChuang2019',str(result))
-        logging.info("已发送，请注意查收。")
-    except Exception as e:
-        logging.exception(e)
+    bot = Bot()
+    friends = bot.friends()
 
+    @bot.register(friends,SHARING)
+    def handler(msg):
+        try:
+            logging.info('sharing url= %s',msg.url)
+            article = get_article(msg.url)
+            result = stats_word.stats_text_cn(article,100)
+            msg.reply(str(result))
+        except Exception as e:
+            logging.exception(e)
+    embed()
 
 if __name__=="__main__":
     main()
