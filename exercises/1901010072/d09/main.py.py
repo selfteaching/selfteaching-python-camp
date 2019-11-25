c741a4e02a5667d1c60ffc570b693d1b99838ef2
Bear127 <48996947+Bear127@users.noreply.github.com>
2019-11-25 23:17:46 +0800

    Update main.py.py

--- exercises/1901010072/d09/main.py.py
+++ exercises/1901010072/d09/main.py.py
@@ -1,9 +1,48 @@
 from mymodule import stats_word
+from os import path
 import json
-#路径前+r，避免转义。
-with open(r'C:\Users\CS-Mu\Documents\selfteaching-python-camp\exercises\1901010072\d09\tang300.json',encoding='UTF-8') as f: 
-    read_date = f.read()
-try:
-    print('统计汉字字频最高的前100个字： \n',stats_word.stats_text_cn(read_date,100))
-except ValueError as e:
-    print(e)
+import re
+import logging
+
+logging.basicConfig(format='file:%(filename)s|line:%(lineno)d|message:%(message)s',level=logging.DEBUG)
+
+def load_file():
+    '''
+    1.
+    下面是常用的获取文件路径的方式，要确保tang300.json跟当前文件在同一个文件夹下，这两种方式等价
+    file_path=path.join(path.dirname(path.abspath(_file_)),'./tang300.json')
+    '''
+    file_path=path.join(path.dirname(path.abspath(__file__)),'tang300.json')
+    print('当前文件路径:',__file__,'\n读取文件路径:',file_path)
+    '''
+    2.
+    这种方式表示tang300.json在当前文件的上一级目录
+    file_path=path.join(path.dirname(path.abspath(__file)),'../tang300.json')
+    print(__file__,file_path)
+
+    3.
+    这种方式表示tang300.json在当前文件的上一级目录的data文件夹下
+    file_path=path.join(path.dirname(path.abspath(__file__)),'../data/tang300.json')
+    print(__file__,file_path)
+    '''
+    with open(file_path,'r',encoding='utf-8')as f:
+        return f.read()
+
+def merge_poems(data):
+    poems=''
+    for item in data:
+        poems += item.get('contents','')
+    return poems
+
+def main():
+    try:
+        data=load_file()
+        logging.info(data[0])
+        poems=merge_poems(json.loads(data))
+        logging.info('result==>%s',stats_word.stats_text_cn(poems,100))
+    except Exception as e:
+        logging.exception(e)
+
+    if __name__=="__main__":
+        main()
+        
