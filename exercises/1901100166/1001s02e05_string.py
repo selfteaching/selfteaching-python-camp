ca7499884215f30a777f577d62e48d731b025cee
oneisonly <53245383+oneisonly@users.noreply.github.com>
2019-11-25 16:03:45 +0800

    【1901100166】自学训练营学习18群 d6 (#6202)
    * day2
    
    * day3
    
    辛苦老师了，第一个是视频里讲的，打印正常，第二个是我做了个简单的，但打印不出来
    
    * day3
    
    修改了一下
    
    * d4
    
    两种方法
    
    * d5
    
    * d6
    
    * day6

--- exercises/1901100166/1001s02e05_string.py
+++ exercises/1901100166/1001s02e05_string.py
@@ -25,25 +25,24 @@ Namespaces are one honking great idea -- let's do more of those!
 
 # 调用 str 类型 的 replace 方法进行替换
 b= a.replace('better','worse')
-#print('将字符串样本里的 better 全部替换成 worse ==>', b)
+print('将字符串样本里的 better 全部替换成 worse ==>', b)
 
 #2.
 c=a.split()   #调用str类型，将字符串 根据 空白字符 分割成list
 guolv=[]       #定义list类型的变量存放过滤后的单词
 for d in c:       #句型判断是否含ea
-    if d.find('ea')<2:  # str类型的find方法 不含参数则返回-1，含则返回第一次出现的索引
+    if d.find('ea')<0:  # str类型的find方法 不含参数则返回-1，含则返回第一次出现的索引
         guolv.append(d)
-print('剔除ea',guolv)
+print('剔除ea==>',guolv)
 
 #3.大小写翻转
 E=[e.swapcase() for e in guolv]
-#print('333333',E)
+print('333333',E)
 
 #4.单词升序排列
-#print('升序==>',sorted(E))  #sorted默认升序
+print('升序==>',sorted(E))  #sorted默认升序
 
 #print('降序==>',sorted(E, reverse=True))
-
 
 
 
