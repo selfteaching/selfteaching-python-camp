45f0b9c665190682bfbf4097af7fa82a150e9a71
DainDain <124748338@qq.com>
2019-11-27 17:52:18 +0800

    day8 homework

---
+++ exercises/1901100229/d08/main.py
@@ -0,0 +1,20 @@
+﻿text = [2,3,4,5]
+ 
+from mymodule.stats_word import stats_text_cn as cn 
+try:            #添加一个try except捕获一次
+    cn(text)
+except ValueError as Type_Error:
+    print(Type_Error)
+print(cn(a))
+
+
+
+
+
+
+
+
+
+
+
+
