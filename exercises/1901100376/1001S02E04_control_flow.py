69bb1f7c8ed5edcbd323d01d80a41d9402da8f68
GS2017GS <gs121500190@gmail.com>
2019-11-27 22:38:44 +0800

    Update 1001S02E04_control_flow.py

--- exercises/1901100376/1001S02E04_control_flow.py
+++ exercises/1901100376/1001S02E04_control_flow.py
@@ -1,5 +1,14 @@
+print('九九乘法表一:')
 for a in range(1,10):
     for b in range(1,a+1):
         print('%s*%s=%s'%(a,b,a*b),end=' ')
     print()
-            
+
+print( '九九乘法表二:')
+a=1
+while a<10:
+    if a % 2 !=0:
+        for b in range(1,a+1):
+            print('%s*%s=%s'%(a,b,a*b),end=' ')
+        print()
+        a += 2
