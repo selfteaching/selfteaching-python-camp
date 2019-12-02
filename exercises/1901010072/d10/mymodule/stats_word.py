4b706d5d0fd41a1e85018dcd6975cf598e6e7830
Bear127 <48996947+Bear127@users.noreply.github.com>
2019-12-03 00:14:00 +0800

    Update stats_word.py

--- exercises/1901010072/d10/mymodule/stats_word.py
+++ exercises/1901010072/d10/mymodule/stats_word.py
@@ -1,16 +1,27 @@
 from collections import Counter
 import jieba
-seg_list = jieba.cut("text", cut_all=False)
-print("Default Mode: " + "/ ".join(seg_list))
-
+#pip install -i http://pypi.tuna.tsinghua.edu.cn/simple  jieba
+#统计参数中每个英文单词出现的次数
+def stats_text_en(text,count):
+    elements=text.split()
+    words=[]
+    symbols=',.*-!'
+    for element in elements:
+        for symbol in symbols:
+            element=element.replace(symbol,'')
+        #用str类型的isascii方法判断是否是英文单词
+        if len(element) and element.isascii():
+            words.append(element)
+    return Counter(words).most_common(count)
 
 #统计参数中每个中文汉字出现的次数
-def stats_text_cn(text, count):
-    cn_characters=[]
-    for character in text:
-        if '\u4e00'<=character<='\u9fff'and len(character)>=2:
-            cn_characters.append(character)
-    return Counter(cn_characters).most_common(20)
+def stats_text_cn(text,count):
+    words=jieba.cut(text)
+    tmp=[]
+    for i in words:
+        if len(i)>1:
+            tmp.append(i)
+    return Counter(tmp).most_common(count)
 
 def stats_text(text,count):
     '''
