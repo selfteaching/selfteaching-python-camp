1754820a35a0cc84af6450b5777b15127aea1a60
Bear127 <48996947+Bear127@users.noreply.github.com>
2019-12-04 23:55:49 +0800

    Update main.py.py

--- exercises/1901010072/d11/main.py.py
+++ exercises/1901010072/d11/main.py.py
@@ -1,45 +1,34 @@
 from mymodule import stats_word
-from os import path
-import json
-import re
+import yagmail
+import requests
+import pyquery
+import getpass
 import logging
+
+#安装依赖包 requests yagmail pyquery lxml
+#pip install -i https://pypi.tuna.tsinghua.edu.cn/simple  requests yagmail pyquery lxml
 
 logging.basicConfig(format='file:%(filename)s|line:%(lineno)d|message:%(message)s',level=logging.DEBUG)
 
-def load_file():
-    '''
-    1.
-    下面是常用的获取文件路径的方式，要确保tang300.json跟当前文件在同一个文件夹下，这两种方式等价
-    file_path=path.join(path.dirname(path.abspath(_file_)),'./tang300.json')
-    '''
-    file_path=path.join(path.dirname(path.abspath(__file__)),'tang300.json')
-    print('当前文件路径:',__file__,'\n读取文件路径:',file_path)
-    '''
-    2.
-    这种方式表示tang300.json在当前文件的上一级目录
-    file_path=path.join(path.dirname(path.abspath(__file)),'../tang300.json')
-    print(__file__,file_path)
-
-    3.
-    这种方式表示tang300.json在当前文件的上一级目录的data文件夹下
-    file_path=path.join(path.dirname(path.abspath(__file__)),'../data/tang300.json')
-    print(__file__,file_path)
-    '''
-    with open(file_path,'r',encoding='utf-8')as f:
-        return f.read()
-
-def merge_poems(data):
-    poems=''
-    for item in data:
-        poems += item.get('contents','')
-    return poems
+#提取微信公众号文章正文
+def get_article():
+    r=requests.get('https://mp.weixin.qq.com/s/hT45I5TsNv18AAfNsx81PA')
+    document=pyquery.PyQuery(r.text)
+    return document('#js_content').text()
 
 def main():
     try:
-        data=load_file()
-        #logging.info(data)
-        poems=merge_poems(json.loads(data))
-        logging.info('result==>%s',stats_word.stats_text_cn(poems,100))
+        article=get_article()
+        result= stats_word.stats_text_cn(article,10)
+        logging.info('%s %s',type(result),str(result))
+        sender=input('请输入发件人邮箱:')
+        #为了保护密码，输入密码的时候不会显示出来，直接输入，完成后按回车就行
+        password=getpass.getpass('输入发件人邮箱密码:')
+        recipients=input('请输入收件人邮箱:')
+        #如果使用的是QQ邮箱这里就填 smtp.qq.com
+        yag=yagmail.SMTP(sender,password,'smtp.163.com')
+        yag.send(recipients,'自学训练学习2群 190101***',str(result))
+        logging.info("已发送，请注意查收.")
     except Exception as e:
         logging.exception(e)
 
